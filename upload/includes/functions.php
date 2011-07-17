146ac8b1bd8914ba878060e670a903413c94d256
nanosimbiot <nanosimbiot@a8ac35ab-4ca4-ca47-4c2d-a49a94f06293>
2011-07-17 15:21:52 +0000

    r116
     git-svn-id: https://torrentpier2.googlecode.com/svn/trunk@116 a8ac35ab-4ca4-ca47-4c2d-a49a94f06293

--- upload/includes/functions.php
+++ upload/includes/functions.php
@@ -2066,7 +2066,7 @@ function realdate($date, $format = "Ymd")
 function birthday_age($date)
 {
 	if(!$date) return;
-	return delta_time(mktime(0, 0, 0, 6, 0, realdate($date, 'Y')));
+	return bb_date(mktime(11, 0, 0, realdate($date, 'm'), realdate($date, 'd'), realdate($date, 'Y')));
 }
 
 //
