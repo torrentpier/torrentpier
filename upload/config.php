5e81f5413b48dad25d6294e11da337850e6f8ced
torrent.rus.ec@gmail.com <torrent.rus.ec@gmail.com@a8ac35ab-4ca4-ca47-4c2d-a49a94f06293>
2011-08-24 20:37:00 +0000

    r253
    Добавки в конфиг сфинкса (от Elder) git-svn-id: https://torrentpier2.googlecode.com/svn/trunk@253 a8ac35ab-4ca4-ca47-4c2d-a49a94f06293

--- upload/config.php
+++ upload/config.php
@@ -57,8 +57,8 @@ $bb_cfg['css_ver'] = 1;
 
 // Increase number of revision after update
 $bb_cfg['tp_version'] = '2.1 Stable';
-$bb_cfg['tp_release_state'] = 'R252';
-$bb_cfg['tp_release_date'] = '24-08-2011';
+$bb_cfg['tp_release_state'] = 'R253';
+$bb_cfg['tp_release_date'] = '25-08-2011';
 
 $bb_cfg['board_disabled_msg'] = 'форум временно отключен'; // 'forums temporarily disabled'; // show this msg if board has been disabled via ON/OFF trigger
 $bb_cfg['srv_overloaded_msg'] = "Извините, в данный момент сервер перегружен\nПопробуйте повторить запрос через несколько минут";
