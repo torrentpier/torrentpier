bb70968c0acbda4be39eceab9cf56f02f095e942
nanosimbiot <nanosimbiot@a8ac35ab-4ca4-ca47-4c2d-a49a94f06293>
2011-08-24 07:01:27 +0000

    r252
     git-svn-id: https://torrentpier2.googlecode.com/svn/trunk@252 a8ac35ab-4ca4-ca47-4c2d-a49a94f06293

--- upload/config.php
+++ upload/config.php
@@ -57,7 +57,7 @@ $bb_cfg['css_ver'] = 1;
 
 // Increase number of revision after update
 $bb_cfg['tp_version'] = '2.1 Stable';
-$bb_cfg['tp_release_state'] = 'R251';
+$bb_cfg['tp_release_state'] = 'R252';
 $bb_cfg['tp_release_date'] = '24-08-2011';
 
 $bb_cfg['board_disabled_msg'] = 'форум временно отключен'; // 'forums temporarily disabled'; // show this msg if board has been disabled via ON/OFF trigger
