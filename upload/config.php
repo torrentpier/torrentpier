8fb2598954a325f6ca313460213549b17db2e0bd
pherum83 <pherum83@a8ac35ab-4ca4-ca47-4c2d-a49a94f06293>
2011-09-30 17:34:47 +0000

    r295
     git-svn-id: https://torrentpier2.googlecode.com/svn/trunk@295 a8ac35ab-4ca4-ca47-4c2d-a49a94f06293

--- upload/config.php
+++ upload/config.php
@@ -53,7 +53,7 @@ $bb_cfg = $tr_cfg = $page_cfg = array();
 
 // Increase number of revision after update
 $bb_cfg['tp_version'] = '2.1 Stable';
-$bb_cfg['tp_release_state'] = 'R294';
+$bb_cfg['tp_release_state'] = 'R295';
 $bb_cfg['tp_release_date'] = '30-09-2011';
 
 // Database
