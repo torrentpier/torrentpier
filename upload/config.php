7ac2e207f28173fc116d9c36a49725888733b8d3
dimka3210@gmail.com <dimka3210@gmail.com@a8ac35ab-4ca4-ca47-4c2d-a49a94f06293>
2012-05-31 17:38:52 +0000

    r427
    Опять мой косяг. git-svn-id: https://torrentpier2.googlecode.com/svn/trunk@427 a8ac35ab-4ca4-ca47-4c2d-a49a94f06293

--- upload/config.php
+++ upload/config.php
@@ -56,7 +56,7 @@ $domain_name = (!empty($_SERVER['SERVER_NAME'])) ? $_SERVER['SERVER_NAME'] : $do
 
 // Increase number of revision after update
 $bb_cfg['tp_version'] = '2.5 Beta';
-$bb_cfg['tp_release_state'] = 'R426';
+$bb_cfg['tp_release_state'] = 'R427';
 $bb_cfg['tp_release_date'] = '31-05-2012';
 
 // Database
@@ -527,4 +527,4 @@ $bb_cfg['captcha'] = array(
 );
 
 // SEO
-$bb_cfg['seo_link_home_page'] = true
+$bb_cfg['seo_link_home_page'] = true;
