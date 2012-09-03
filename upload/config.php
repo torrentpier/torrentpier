b57acb7c2b1f8ef86dc4665ece79144bbe48ac87
pherum83 <pherum83@a8ac35ab-4ca4-ca47-4c2d-a49a94f06293>
2012-09-03 21:01:48 +0000

    r449
    обновление библиотек git-svn-id: https://torrentpier2.googlecode.com/svn/trunk@449 a8ac35ab-4ca4-ca47-4c2d-a49a94f06293

--- upload/config.php
+++ upload/config.php
@@ -56,8 +56,8 @@ $domain_name = (!empty($_SERVER['SERVER_NAME'])) ? $_SERVER['SERVER_NAME'] : $do
 
 // Increase number of revision after update
 $bb_cfg['tp_version'] = '2.5 Beta';
-$bb_cfg['tp_release_state'] = 'R448';
-$bb_cfg['tp_release_date'] = '03-09-2012';
+$bb_cfg['tp_release_state'] = 'R449';
+$bb_cfg['tp_release_date'] = '04-09-2012';
 
 // Database
 $charset  = 'utf8';
--- upload/misc/js/main.js
+++ upload/misc/js/main.js
@@ -1,11 +1,3 @@
-/**
- * SWFObject v1.5: Flash Player detection and embed - http://blog.deconcept.com/swfobject/
- *
- * SWFObject is (c) 2007 Geoff Stearns and is released under the MIT License:
- * http://www.opensource.org/licenses/mit-license.php
- *
- */
-if(typeof deconcept=="undefined"){var deconcept=new Object();}if(typeof deconcept.util=="undefined"){deconcept.util=new Object();}if(typeof deconcept.SWFObjectUtil=="undefined"){deconcept.SWFObjectUtil=new Object();}deconcept.SWFObject=function(_1,id,w,h,_5,c,_7,_8,_9,_a){if(!document.getElementById){return;}this.DETECT_KEY=_a?_a:"detectflash";this.skipDetect=deconcept.util.getRequestParameter(this.DETECT_KEY);this.params=new Object();this.variables=new Object();this.attributes=new Array();if(_1){this.setAttribute("swf",_1);}if(id){this.setAttribute("id",id);}if(w){this.setAttribute("width",w);}if(h){this.setAttribute("height",h);}if(_5){this.setAttribute("version",new deconcept.PlayerVersion(_5.toString().split(".")));}this.installedVer=deconcept.SWFObjectUtil.getPlayerVersion();if(!window.opera&&document.all&&this.installedVer.major>7){deconcept.SWFObject.doPrepUnload=true;}if(c){this.addParam("bgcolor",c);}var q=_7?_7:"high";this.addParam("quality",q);this.setAttribute("useExpressInstall",false);this.setAttribute("doExpressInstall",false);var _c=(_8)?_8:window.location;this.setAttribute("xiRedirectUrl",_c);this.setAttribute("redirectUrl","");if(_9){this.setAttribute("redirectUrl",_9);}};deconcept.SWFObject.prototype={useExpressInstall:function(_d){this.xiSWFPath=!_d?"expressinstall.swf":_d;this.setAttribute("useExpressInstall",true);},setAttribute:function(_e,_f){this.attributes[_e]=_f;},getAttribute:function(_10){return this.attributes[_10];},addParam:function(_11,_12){this.params[_11]=_12;},getParams:function(){return this.params;},addVariable:function(_13,_14){this.variables[_13]=_14;},getVariable:function(_15){return this.variables[_15];},getVariables:function(){return this.variables;},getVariablePairs:function(){var _16=new Array();var key;var _18=this.getVariables();for(key in _18){_16[_16.length]=key+"="+_18[key];}return _16;},getSWFHTML:function(){var _19="";if(navigator.plugins&&navigator.mimeTypes&&navigator.mimeTypes.length){if(this.getAttribute("doExpressInstall")){this.addVariable("MMplayerType","PlugIn");this.setAttribute("swf",this.xiSWFPath);}_19="<embed type=\"application/x-shockwave-flash\" src=\""+this.getAttribute("swf")+"\" width=\""+this.getAttribute("width")+"\" height=\""+this.getAttribute("height")+"\" style=\""+this.getAttribute("style")+"\"";_19+=" id=\""+this.getAttribute("id")+"\" name=\""+this.getAttribute("id")+"\" ";var _1a=this.getParams();for(var key in _1a){_19+=[key]+"=\""+_1a[key]+"\" ";}var _1c=this.getVariablePairs().join("&");if(_1c.length>0){_19+="flashvars=\""+_1c+"\"";}_19+="/>";}else{if(this.getAttribute("doExpressInstall")){this.addVariable("MMplayerType","ActiveX");this.setAttribute("swf",this.xiSWFPath);}_19="<object id=\""+this.getAttribute("id")+"\" classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" width=\""+this.getAttribute("width")+"\" height=\""+this.getAttribute("height")+"\" style=\""+this.getAttribute("style")+"\">";_19+="<param name=\"movie\" value=\""+this.getAttribute("swf")+"\" />";var _1d=this.getParams();for(var key in _1d){_19+="<param name=\""+key+"\" value=\""+_1d[key]+"\" />";}var _1f=this.getVariablePairs().join("&");if(_1f.length>0){_19+="<param name=\"flashvars\" value=\""+_1f+"\" />";}_19+="</object>";}return _19;},write:function(_20){if(this.getAttribute("useExpressInstall")){var _21=new deconcept.PlayerVersion([6,0,65]);if(this.installedVer.versionIsValid(_21)&&!this.installedVer.versionIsValid(this.getAttribute("version"))){this.setAttribute("doExpressInstall",true);this.addVariable("MMredirectURL",escape(this.getAttribute("xiRedirectUrl")));document.title=document.title.slice(0,47)+" - Flash Player Installation";this.addVariable("MMdoctitle",document.title);}}if(this.skipDetect||this.getAttribute("doExpressInstall")||this.installedVer.versionIsValid(this.getAttribute("version"))){var n=(typeof _20=="string")?document.getElementById(_20):_20;n.innerHTML=this.getSWFHTML();return true;}else{if(this.getAttribute("redirectUrl")!=""){document.location.replace(this.getAttribute("redirectUrl"));}}return false;}};deconcept.SWFObjectUtil.getPlayerVersion=function(){var _23=new deconcept.PlayerVersion([0,0,0]);if(navigator.plugins&&navigator.mimeTypes.length){var x=navigator.plugins["Shockwave Flash"];if(x&&x.description){_23=new deconcept.PlayerVersion(x.description.replace(/([a-zA-Z]|\s)+/,"").replace(/(\s+r|\s+b[0-9]+)/,".").split("."));}}else{if(navigator.userAgent&&navigator.userAgent.indexOf("Windows CE")>=0){var axo=1;var _26=3;while(axo){try{_26++;axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash."+_26);_23=new deconcept.PlayerVersion([_26,0,0]);}catch(e){axo=null;}}}else{try{var axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash.7");}catch(e){try{var axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash.6");_23=new deconcept.PlayerVersion([6,0,21]);axo.AllowScriptAccess="always";}catch(e){if(_23.major==6){return _23;}}try{axo=new ActiveXObject("ShockwaveFlash.ShockwaveFlash");}catch(e){}}if(axo!=null){_23=new deconcept.PlayerVersion(axo.GetVariable("$version").split(" ")[1].split(","));}}}return _23;};deconcept.PlayerVersion=function(_29){this.major=_29[0]!=null?parseInt(_29[0]):0;this.minor=_29[1]!=null?parseInt(_29[1]):0;this.rev=_29[2]!=null?parseInt(_29[2]):0;};deconcept.PlayerVersion.prototype.versionIsValid=function(fv){if(this.major<fv.major){return false;}if(this.major>fv.major){return true;}if(this.minor<fv.minor){return false;}if(this.minor>fv.minor){return true;}if(this.rev<fv.rev){return false;}return true;};deconcept.util={getRequestParameter:function(_2b){var q=document.location.search||document.location.hash;if(_2b==null){return q;}if(q){var _2d=q.substring(1).split("&");for(var i=0;i<_2d.length;i++){if(_2d[i].substring(0,_2d[i].indexOf("="))==_2b){return _2d[i].substring((_2d[i].indexOf("=")+1));}}}return "";}};deconcept.SWFObjectUtil.cleanupSWFs=function(){var _2f=document.getElementsByTagName("OBJECT");for(var i=_2f.length-1;i>=0;i--){_2f[i].style.display="none";for(var x in _2f[i]){if(typeof _2f[i][x]=="function"){_2f[i][x]=function(){};}}}};if(deconcept.SWFObject.doPrepUnload){if(!deconcept.unloadSet){deconcept.SWFObjectUtil.prepUnload=function(){__flash_unloadHandler=function(){};__flash_savedUnloadHandler=function(){};window.attachEvent("onunload",deconcept.SWFObjectUtil.cleanupSWFs);};window.attachEvent("onbeforeunload",deconcept.SWFObjectUtil.prepUnload);deconcept.unloadSet=true;}}if(!document.getElementById&&document.all){document.getElementById=function(id){return document.all[id];};}var getQueryParamValue=deconcept.util.getRequestParameter;var FlashObject=deconcept.SWFObject;var SWFObject=deconcept.SWFObject;
 
 // prototype $
 function $p() {
@@ -71,22 +63,22 @@ if (document.all) { addEvent(window,'unload',EventCache.flush); }
 
 function imgFit (img, maxW)
 {
+	img.title  = 'Размеры изображения: '+img.width+' x '+img.height;
 	if (typeof(img.naturalHeight) == 'undefined') {
 		img.naturalHeight = img.height;
 		img.naturalWidth  = img.width;
 	}
-//alert(img.src+'\n\n'+'H: '+img.height+'  natH: '+img.naturalHeight+'\n'+'W: '+img.width+'  natW: '+img.naturalWidth+'  maxW: '+maxW);
 	if (img.width > maxW) {
 		img.height = Math.round((maxW/img.width)*img.height);
 		img.width  = maxW;
-		img.title  = 'Click image to view full size';
+		img.title  = 'Нажмите на изображение, чтобы посмотреть его в полный размер';
 		img.style.cursor = 'move';
 		return false;
 	}
 	else if (img.width == maxW && img.width < img.naturalWidth) {
 		img.height = img.naturalHeight;
 		img.width  = img.naturalWidth;
-		img.title  = 'Click to fit in the browser window';
+		img.title  = 'Размеры изображения: '+img.naturalWidth+' x '+img.naturalHeight;
 		return false;
 	}
 	else {
@@ -206,9 +198,7 @@ function ie6_make_clickable_labels ()
 // Menus
 //
 var Menu = {
-	openSpeed          : '',
 	hideSpeed          : 'fast',
-	getOffsetOptions   : { margin: true, border: true, padding: true, scroll: false },
 	offsetCorrection_X : -4,
 	offsetCorrection_Y : 2,
 	iframeFix          : false,
@@ -287,7 +277,7 @@ var Menu = {
 
 	showClickedMenu: function() {
 		this.setLocation();
-		this.$menu.show(this.openSpeed);
+		this.$menu.css({display: 'block'});
 		this.fixLocation();
 		this.activeMenuId = this.clickedMenuId;
 	},
@@ -330,7 +320,8 @@ $(document).ready(function(){
 		.end()
 		.filter('.error').one('focus', function(){
 			$(this).removeClass('error');
-		});
+		})
+	;
 });
 
 //
@@ -380,7 +371,7 @@ Ajax.prototype = {
 			}
 		}
 		if (response.prompt_password) {
-			var user_password = prompt('Please enter your password (for mod/admin session)', '');
+			var user_password = prompt('Для доступа к данной функции, пожалуйста, введите свой пароль', '');
 			if (user_password) {
 				var req = ajax.request[action];
 				req.user_password = user_password;
@@ -388,7 +379,7 @@ Ajax.prototype = {
 			}
 			else {
 				ajax.clearActionState(action);
-				ajax.showErrorMsg('Wrong password');
+				ajax.showErrorMsg('Введен неверный пароль');
 			}
 		}
 		else if (response.prompt_confirm) {
@@ -453,7 +444,7 @@ Ajax.prototype = {
 		$('input.editable-submit', $inputs).click(function(){
 			var params = ajax.params[rootElementId];
 			var $val = $('.editable-value', '#'+rootElementId);
-			params.value = ($val.size() == 1) ? $val.val() : $val.filter('[checked]').val();
+			params.value = ($val.size() == 1) ? $val.val() : $val.filter(':checked').val();
 			params.submit = true;
 			ajax.init[params.action](params);
 		});
@@ -496,7 +487,7 @@ $(document).ready(function(){
 			text = 'invalid data format';
 		}
 		$(this).html(
-			"Ajax error in: <i>"+ ajax.url +"</i><br /><b>"+ status +" "+ text +"</b>"
+			"Ошибка в: <i>"+ ajax.url +"</i><br /><b>"+ status +" "+ text +"</b>"
 		).show();
 		ajax.setStatusBoxPosition($(this));
 	});
