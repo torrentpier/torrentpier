cb34064647dfde41d445ae48979f6d3e44ecf7cf
PheRum <pherum@mail.ru>
2015-03-07 22:50:35 +0300

    no message

--- library/config.php
+++ library/config.php
@@ -84,7 +84,7 @@ $pconnect = false;
 // Настройка баз данных ['db']['srv_name'] => (array) srv_cfg;
 // порядок параметров srv_cfg (хост, название базы, пользователь, пароль, charset, pconnect);
 $bb_cfg['db'] = array(
-	'db1' => array('localhost', 'dbase', 'user', 'pass', $charset, $pconnect),
+	'db1' => array('localhost', 'tp_215', 'user', 'pass', $charset, $pconnect),
 	//'db2' => array('localhost2', 'dbase2', 'user2', 'pass2', $charset, $pconnect),
 	//'db3' => array('localhost3', 'dbase3', 'user2', 'pass3', $charset, $pconnect),
 );
