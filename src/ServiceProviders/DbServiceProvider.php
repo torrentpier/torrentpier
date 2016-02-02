89f5db1c5012d779acf1b84c7fc1ad4ee0a28eeb
Vasily Komrakov <diolektor@gmail.com>
2016-02-02 19:47:18 +0300

    #55 The implementation Dependency Injection Container - reorganization config - add service provider for config - add service provider for database

---
+++ src/ServiceProviders/DbServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TorrentPier\ServiceProviders;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Zend\Db\Adapter\Adapter;
+
+class DbServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function register(Container $container)
+    {
+        $container['db'] = function($container) {
+            $adapter = new Adapter($container['config.db']);
+            unset($container['config.db']);
+            return $adapter;
+        };
+    }
+}
