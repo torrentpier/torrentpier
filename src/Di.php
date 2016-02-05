071dc636696fc5974628de7442903734b999c46e
Exile <iglix@me.com>
2016-02-05 06:06:52 +0300

    Реализация Dependency Injection от @VasyOk #55

---
+++ src/Di.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace TorrentPier;
+
+use Pimple\Container;
+
+/**
+ * Class Di
+ * Dependency Injection Container
+ *
+ * @package TorrentPier
+ */
+class Di extends Container
+{
+	private static $instance;
+
+	public function __construct(array $values = [])
+	{
+		parent::__construct($values);
+		static::$instance = $this;
+	}
+
+	public static function getInstance()
+	{
+		if (static::$instance instanceof Container) {
+			return static::$instance;
+		}
+
+		throw new \Exception('The container has not been initialized');
+	}
+
+	public function __get($id)
+	{
+		if ($this->offsetExists($id)) {
+			return $this->offsetGet($id);
+		}
+
+		throw new \Exception("Service '{$id}' is not registered in the container");
+	}
+}
