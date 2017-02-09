05a5adb1a038d3ae3ef32cbf5d40a448ee6d2d2c
Yuriy Pikhtarev <iglix@me.com>
2017-02-10 02:53:47 +0300

    New translations

---
+++ messages/it.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2005-2017 TorrentPier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+return [
+    // Common
+    'Captcha' => 'Captcha',
+    'Go back' => 'Go back',
+    'Send' => 'Inviare',
+    'Submit' => 'Submit',
+
+    // Control panel
+    'Control panel' => 'Control panel',
+
+    // Errors
+    'File not found: %location%' => 'File not found: %location%',
+    'Invalid request: not specified %data%' => 'Invalid request: not specified %data%',
+
+    // Feed (feed.php)
+    'Feed' => 'Feed',
+    'Do not specify a mode for the feed' => 'Do not specify a mode for the feed',
+    'This forum does not have a feed' => 'This forum does not have a feed',
+    'This user does not have a feed' => 'This user does not have a feed',
+
+    // Style guide (styleguide.php)
+    'Hello, %name%' => 'Ciao, %name%',
+    'Style guide' => 'Guida di stile',
+
+    // Terms (terms.php)
+    'Only administrators see this line' => 'Only administrators see this line',
+    'Terms' => 'Terms',
+    'The text on this page can be edited in the' => 'The text on this page can be edited in the',
+];
