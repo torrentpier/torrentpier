721b00b1009647ec4f68b16504a88cac10ef570d
Yuriy Pikhtarev <iglix@me.com>
2017-02-10 02:53:38 +0300

    Translated

---
+++ messages/be.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2005-2017 TorrentPier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+return [
+    // Common
+    'Captcha' => 'Капча',
+    'Go back' => 'Вярнуцца',
+    'Send' => 'Адправіць',
+    'Submit' => 'Падаць',
+
+    // Control panel
+    'Control panel' => 'Панэль кіравання',
+
+    // Errors
+    'File not found: %location%' => 'Файл не знойдзены: %location%',
+    'Invalid request: not specified %data%' => 'Няправільны запыт: не паказаны %data%',
+
+    // Feed (feed.php)
+    'Feed' => 'Лента',
+    'Do not specify a mode for the feed' => 'Не паказаны рэжым для ленты',
+    'This forum does not have a feed' => 'Гэты форум не мае ленты',
+    'This user does not have a feed' => 'Гэты карыстальнік не мае ленты',
+
+    // Style guide (styleguide.php)
+    'Hello, %name%' => 'Прывітанне, %name%',
+    'Style guide' => 'Гід па стылі',
+
+    // Terms (terms.php)
+    'Only administrators see this line' => 'Толькі адміністратары бачаць гэты радок',
+    'Terms' => 'Правілы',
+    'The text on this page can be edited in the' => 'Тэкст на гэтай старонцы можа быць адрэдагаваны ў',
+];
