1bcd6a5271ad6d621061a2172f5b63ff22e9778c
Yuriy Pikhtarev <iglix@me.com>
2017-02-10 02:53:25 +0300

    New translations

---
+++ messages/ach.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2005-2017 TorrentPier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+return [
+    // Common
+    'Captcha' => 'crwdns8547:0crwdne8547:0',
+    'Go back' => 'crwdns8548:0crwdne8548:0',
+    'Send' => 'crwdns8549:0crwdne8549:0',
+    'Submit' => 'crwdns8550:0crwdne8550:0',
+
+    // Control panel
+    'Control panel' => 'crwdns8551:0crwdne8551:0',
+
+    // Errors
+    'File not found: %location%' => 'crwdns8552:0%location%crwdne8552:0',
+    'Invalid request: not specified %data%' => 'crwdns8553:0%data%crwdne8553:0',
+
+    // Feed (feed.php)
+    'Feed' => 'crwdns8554:0crwdne8554:0',
+    'Do not specify a mode for the feed' => 'crwdns8555:0crwdne8555:0',
+    'This forum does not have a feed' => 'crwdns8556:0crwdne8556:0',
+    'This user does not have a feed' => 'crwdns8557:0crwdne8557:0',
+
+    // Style guide (styleguide.php)
+    'Hello, %name%' => 'crwdns8558:0%name%crwdne8558:0',
+    'Style guide' => 'crwdns8559:0crwdne8559:0',
+
+    // Terms (terms.php)
+    'Only administrators see this line' => 'crwdns8560:0crwdne8560:0',
+    'Terms' => 'crwdns8561:0crwdne8561:0',
+    'The text on this page can be edited in the' => 'crwdns8562:0crwdne8562:0',
+];
