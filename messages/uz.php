4ecd195a2ad883ccda1909923b9fe07a15bf9d36
Yuriy Pikhtarev <iglix@me.com>
2017-02-10 03:46:38 +0300

    Translated

---
+++ messages/uz.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2005-2017 TorrentPier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+return [
+    // Common
+    'Captcha' => 'Captcha',
+    'Go back' => 'Qaytish',
+    'Send' => 'Yuborish',
+    'Submit' => 'Jo\'natish',
+
+    // Control panel
+    'Control panel' => 'Boshqaruv paneli',
+
+    // Errors
+    'File not found: %location%' => 'Fayl topilmadi: %location%',
+    'Invalid request: not specified %data%' => 'Noto`g`ri so`rov: %data% ko`rsatilmagan',
+
+    // Feed (feed.php)
+    'Feed' => 'Tasma',
+    'Do not specify a mode for the feed' => 'Tasma holati ko`rsatilmagan',
+    'This forum does not have a feed' => 'Bu forum tasmaga ega emas',
+    'This user does not have a feed' => 'Bu foydalanuvchi tasmaga ega emas',
+
+    // Style guide (styleguide.php)
+    'Hello, %name%' => 'Salom, %name%',
+    'Style guide' => 'Uslub ko`makchisi',
+
+    // Terms (terms.php)
+    'Only administrators see this line' => 'Bu qatorni faqat administratorlar ko`ra oladi',
+    'Terms' => 'Qoidalar',
+    'The text on this page can be edited in the' => 'Ushbu sahifadagi matn (link) da tahrirlangan bo\'lishi mumkin',
+];
