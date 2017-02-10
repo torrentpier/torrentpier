a4ae81b3b5118a006491898f5d5e47e1b1c78d71
Yuriy Pikhtarev <iglix@me.com>
2017-02-10 03:46:36 +0300

    Translated

---
+++ messages/uk.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2005-2017 TorrentPier
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+return [
+    // Common
+    'Captcha' => 'Захисний код',
+    'Go back' => 'Повернутися',
+    'Send' => 'Відправити',
+    'Submit' => 'Підтвердити',
+
+    // Control panel
+    'Control panel' => 'Панель керування',
+
+    // Errors
+    'File not found: %location%' => 'Файл не знайдено: %location%',
+    'Invalid request: not specified %data%' => 'Невірний запит: не вказано %data%',
+
+    // Feed (feed.php)
+    'Feed' => 'Стрічка',
+    'Do not specify a mode for the feed' => 'Не вказано режим для стрічки',
+    'This forum does not have a feed' => 'Цей форум не має стрічки',
+    'This user does not have a feed' => 'У цього користувача немає стрічки',
+
+    // Style guide (styleguide.php)
+    'Hello, %name%' => 'Привіт, %name%',
+    'Style guide' => 'Порадник по стилю',
+
+    // Terms (terms.php)
+    'Only administrators see this line' => 'Тільки адміністратори бачать цей рядок',
+    'Terms' => 'Умови',
+    'The text on this page can be edited in the' => 'Текст на цій сторінці може бути відредагований у',
+];
