a178a269c3647e9b52240be42059d90a6aeb189e
Yuriy Pikhtarev <iglix@me.com>
2017-06-25 02:28:26 +0300

    New translations source.php (Latvian)

--- messages/lv.php
+++ messages/lv.php
@@ -26,29 +26,29 @@
 return [
     // Common
     'Captcha' => 'Captcha',
-    'Go back' => 'Go back',
-    'Send' => 'Send',
-    'Submit' => 'Submit',
+    'Go back' => 'Atgriezties',
+    'Send' => 'Nosūtīt',
+    'Submit' => 'Iesniegt',
 
     // Control panel
-    'Control panel' => 'Control panel',
+    'Control panel' => 'Vadības panelis',
 
     // Errors
-    'File not found: %location%' => 'File not found: %location%',
-    'Invalid request: not specified %data%' => 'Invalid request: not specified %data%',
+    'File not found: %location%' => 'Fails nav atrasts: %location%',
+    'Invalid request: not specified %data%' => 'Nederīgs pieprasījums: nav norādīts %data%',
 
     // Feed (feed.php)
-    'Feed' => 'Feed',
-    'Do not specify a mode for the feed' => 'Do not specify a mode for the feed',
-    'This forum does not have a feed' => 'This forum does not have a feed',
-    'This user does not have a feed' => 'This user does not have a feed',
+    'Feed' => 'Barība',
+    'Do not specify a mode for the feed' => 'Nav norādīts režīmā barības',
+    'This forum does not have a feed' => 'Šis forums nav barības',
+    'This user does not have a feed' => 'Šim lietotājam nav barības',
 
     // Style guide (styleguide.php)
-    'Hello, %name%' => 'Hello, %name%',
-    'Style guide' => 'Style guide',
+    'Hello, %name%' => 'Sveiki, %name%',
+    'Style guide' => 'Stila rokasgrāmata',
 
     // Terms (terms.php)
-    'Only administrators see this line' => 'Only administrators see this line',
-    'Terms' => 'Terms',
-    'The text on this page can be edited in the' => 'The text on this page can be edited in the',
+    'Only administrators see this line' => 'Tikai administratori skatiet šo līniju',
+    'Terms' => 'Noteikumi',
+    'The text on this page can be edited in the' => 'Teksts uz šo lapu var rediģēt',
 ];
