fbd973bb79ef9a330e5c84d79f039fc7b3f31ea3
Yuriy Pikhtarev <iglix@me.com>
2017-06-25 02:28:39 +0300

    New translations source.php (Swedish)

--- messages/sv.php
+++ messages/sv.php
@@ -26,29 +26,29 @@
 return [
     // Common
     'Captcha' => 'Captcha',
-    'Go back' => 'Go back',
+    'Go back' => 'Gå tillbaka',
     'Send' => 'Skicka',
-    'Submit' => 'Submit',
+    'Submit' => 'Skicka',
 
     // Control panel
-    'Control panel' => 'Control panel',
+    'Control panel' => 'Kontrollpanelen',
 
     // Errors
-    'File not found: %location%' => 'File not found: %location%',
-    'Invalid request: not specified %data%' => 'Invalid request: not specified %data%',
+    'File not found: %location%' => 'Filen kunde inte hittas: %location%',
+    'Invalid request: not specified %data%' => 'Ogiltig begäran: ej specificerad %data%',
 
     // Feed (feed.php)
-    'Feed' => 'Feed',
-    'Do not specify a mode for the feed' => 'Do not specify a mode for the feed',
-    'This forum does not have a feed' => 'This forum does not have a feed',
-    'This user does not have a feed' => 'This user does not have a feed',
+    'Feed' => 'Foder',
+    'Do not specify a mode for the feed' => 'Inte anger ett läge för foder',
+    'This forum does not have a feed' => 'Detta forum inte har en feed',
+    'This user does not have a feed' => 'Den här användaren har inte en feed',
 
     // Style guide (styleguide.php)
     'Hello, %name%' => 'Hej, %name%',
     'Style guide' => 'Guido stil',
 
     // Terms (terms.php)
-    'Only administrators see this line' => 'Only administrators see this line',
-    'Terms' => 'Terms',
-    'The text on this page can be edited in the' => 'The text on this page can be edited in the',
+    'Only administrators see this line' => 'Endast administratörer se denna linje',
+    'Terms' => 'Villkor',
+    'The text on this page can be edited in the' => 'Texten på den här sidan kan redigeras i',
 ];
