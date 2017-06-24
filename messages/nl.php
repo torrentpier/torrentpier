ea692b455195e89d64013de3fbca9162d06ce18d
Yuriy Pikhtarev <iglix@me.com>
2017-06-25 02:29:02 +0300

    New translations source.php (Dutch)

--- messages/nl.php
+++ messages/nl.php
@@ -26,29 +26,29 @@
 return [
     // Common
     'Captcha' => 'Captcha',
-    'Go back' => 'Go back',
-    'Send' => 'Send',
-    'Submit' => 'Submit',
+    'Go back' => 'Ga terug',
+    'Send' => 'Verzenden',
+    'Submit' => 'Verzenden',
 
     // Control panel
     'Control panel' => 'Control panel',
 
     // Errors
-    'File not found: %location%' => 'File not found: %location%',
-    'Invalid request: not specified %data%' => 'Invalid request: not specified %data%',
+    'File not found: %location%' => 'Bestand niet gevonden: %location%',
+    'Invalid request: not specified %data%' => 'Ongeldig verzoek: niet opgegeven %data%',
 
     // Feed (feed.php)
     'Feed' => 'Feed',
-    'Do not specify a mode for the feed' => 'Do not specify a mode for the feed',
-    'This forum does not have a feed' => 'This forum does not have a feed',
-    'This user does not have a feed' => 'This user does not have a feed',
+    'Do not specify a mode for the feed' => 'Geef geen modus voor de invoer',
+    'This forum does not have a feed' => 'Dit forum niet feed',
+    'This user does not have a feed' => 'Deze gebruiker heeft geen feed',
 
     // Style guide (styleguide.php)
-    'Hello, %name%' => 'Hello, %name%',
+    'Hello, %name%' => 'Hallo, %name%',
     'Style guide' => 'Style guide',
 
     // Terms (terms.php)
-    'Only administrators see this line' => 'Only administrators see this line',
-    'Terms' => 'Terms',
-    'The text on this page can be edited in the' => 'The text on this page can be edited in the',
+    'Only administrators see this line' => 'Alleen beheerders van deze regel zien',
+    'Terms' => 'Voorwaarden',
+    'The text on this page can be edited in the' => 'De tekst op deze pagina kunnen worden bewerkt in de',
 ];
