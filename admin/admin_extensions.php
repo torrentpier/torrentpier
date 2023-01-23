983d364845615f4f11b820069726e72460b32ce2
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-01-23 23:38:47 +0700

    Fixed array multi sorting

--- admin/admin_extensions.php
+++ admin/admin_extensions.php
@@ -202,7 +202,7 @@ if ($mode == 'extensions') {
     DB()->sql_freeresult($result);
 
     if ($num_extension_row > 0) {
-        $extension_row = sort_multi_array($extension_row, 'group_name', 'ASC');
+        $extension_row = sort_multi_array($extension_row, 'extension');
 
         for ($i = 0; $i < $num_extension_row; $i++) {
             if ($submit) {
