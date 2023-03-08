507b6e8392cab704901a3fb34c301a4c1efc0627
Yury Pikhtarev <exileum@icloud.com>
2023-03-08 14:20:23 +0300

    New translations main.php (Japanese)

--- library/language/ja/main.php
+++ library/language/ja/main.php
@@ -1,26 +1,10 @@
 <?php
 /**
- * MIT License
+ * TorrentPier – Bull-powered BitTorrent tracker engine
  *
- * Copyright (c) 2005-2017 TorrentPier
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * @copyright Copyright (c) 2005-2023 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
  */
 
 // Common, these terms are used extensively on several pages
@@ -343,6 +327,8 @@ $lang['POST_ANNOUNCEMENT'] = '発表';
 $lang['POST_STICKY'] = '粘着';
 $lang['POST_NORMAL'] = '通常の';
 $lang['POST_DOWNLOAD'] = 'ダウンロード';
+
+$lang['PRINT_PAGE'] = 'Print page';
 
 $lang['CONFIRM_DELETE'] = 'まず削除したいことです。';
 $lang['CONFIRM_DELETE_POLL'] = 'Are you sure you want to deleteこの世論調査ですか？';
@@ -1135,10 +1121,14 @@ $lang['STATUS_DOES_EXIST'] = 'そのような状況が存在しない: ';
 
 // tor_comment
 $lang['TOR_MOD_TITLE'] = '変化する状況の分布-%s';
-$lang['TOR_MOD_MSG'] = "こんにちは、%sます。n\nの状況[url=%s]your[/url]分布に変更[b]%s[/b]";
+$lang['TOR_MOD_MSG'] = "こんにちは、%sます。n
+の状況[url=%s]your[/url]分布に変更[b]%s[/b]";
 
 $lang['TOR_AUTH_TITLE'] = '変化のデザイン-%s';
-$lang['TOR_AUTH_MSG'] = "こんにちは、%sます。n\nっ流通に変更[url=%s]%s[/url]\n\nを再チェックしてくださいます。";
+$lang['TOR_AUTH_MSG'] = "こんにちは、%sます。n
+っ流通に変更[url=%s]%s[/url]
+
+を再チェックしてくださいます。";
 $lang['TOR_AUTH_FIXED'] = '固定';
 $lang['TOR_AUTH_SENT_COMMENT'] = ' &middot; <span class="seed bold">The送信された情報をモデレーターです。 期待します。</span>';
 
