c86df1383b854a3be8e6a8019c402375d43c6a02
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 19:44:45 +0700

    New translations main.php (Uzbek)

--- library/language/uz/main.php
+++ library/language/uz/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Keyingi Run';
 $lang['CRON_RUN_COUNT'] = 'ishlaydi';
 $lang['CRON_MANAGE'] = 'boshqarish';
 $lang['CRON_OPTIONS'] = 'cron imkoniyatlari';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'cron yoqilgan';
 $lang['CRON_CHECK_INTERVAL'] = 'Cron tekshirish interval (sek)';
