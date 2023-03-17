d90087732de8323432900b614a9892de0070936c
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-03-17 08:28:03 +0700

    Applied some fixes

--- library/includes/functions.php
+++ library/includes/functions.php
@@ -783,14 +783,19 @@ function generate_user_info(&$row, $date_format, &$avatar, &$from, &$posts, &$jo
 {
     global $lang, $images, $bb_cfg;
 
-    $from = (!empty($row['user_from'])) ? $row['user_from'] : $lang['NOSELECT'];
+    $from = !empty($row['user_from']) ? $row['user_from'] : $lang['NOSELECT'];
     $joined = bb_date($row['user_regdate']);
-    $user_time ?: ((!empty($row['user_time'])) ? bb_date($row['user_time']) : $lang['NONE']);
     $posts = $row['user_posts'] ?: 0;
     $pm = $bb_cfg['text_buttons'] ? '<a class="txtb" href="' . (PM_URL . "?mode=post&amp;" . POST_USERS_URL . "=" . $row['user_id']) . '">' . $lang['SEND_PM_TXTB'] . '</a>' : '<a href="' . (PM_URL . "?mode=post&amp;" . POST_USERS_URL . "=" . $row['user_id']) . '"><img src="' . $images['icon_pm'] . '" alt="' . $lang['SEND_PRIVATE_MESSAGE'] . '" title="' . $lang['SEND_PRIVATE_MESSAGE'] . '" border="0" /></a>';
     $avatar = get_avatar($row['user_id'], $row['avatar_ext_id'], !bf($row['user_opt'], 'user_opt', 'dis_avatar'), 50, 50);
 
-    if (bf($row['user_opt'], 'user_opt', 'user_viewemail') || ($group_mod || IS_ADMIN)) {
+    if (!empty($row['user_time']) && isset($user_time)) {
+        $user_time = bb_date($row['user_time']);
+    } else {
+        $user_time = $lang['NOSELECT'];
+    }
+
+    if (bf($row['user_opt'], 'user_opt', 'user_viewemail') || ($group_mod ?? IS_ADMIN)) {
         $email_uri = ($bb_cfg['board_email_form']) ? ("profile.php?mode=email&amp;" . POST_USERS_URL . "=" . $row['user_id']) : 'mailto:' . $row['user_email'];
         $email = '<a class="editable" href="' . $email_uri . '">' . $row['user_email'] . '</a>';
     } else {
