20ac68b896574ff0800d1ad072dd933b470dbcf4
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 19:44:33 +0700

    New translations main.php (Albanian)

--- library/language/sq/main.php
+++ library/language/sq/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Drejtuar Ardhshëm';
 $lang['CRON_RUN_COUNT'] = 'Shkon';
 $lang['CRON_MANAGE'] = 'Menaxhuar';
 $lang['CRON_OPTIONS'] = 'Cron opsionet';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'Cron të aktivizuara';
 $lang['CRON_CHECK_INTERVAL'] = 'Cron kontrolloni intervalin (kshz)';
