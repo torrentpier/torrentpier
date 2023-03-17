a86412ffc82eb7e067c8eb05befd291d16dedd7f
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 19:44:40 +0700

    New translations main.php (Latvian)

--- library/language/lv/main.php
+++ library/language/lv/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Nākamajā Reizē';
 $lang['CRON_RUN_COUNT'] = 'Iet';
 $lang['CRON_MANAGE'] = 'Pārvaldīt';
 $lang['CRON_OPTIONS'] = 'Cron iespējas';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'Cron iespējots';
 $lang['CRON_CHECK_INTERVAL'] = 'Cron pārbaužu intervāls (sek.)';
