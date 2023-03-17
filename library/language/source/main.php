9f9385b22dc5800608463c3d341f63131af455da
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 14:20:57 +0300

    Adding a warning in the control panel

--- library/language/source/main.php
+++ library/language/source/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Next Run';
 $lang['CRON_RUN_COUNT'] = 'Runs';
 $lang['CRON_MANAGE'] = 'Manage';
 $lang['CRON_OPTIONS'] = 'Cron options';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'Cron enabled';
 $lang['CRON_CHECK_INTERVAL'] = 'Cron check interval (sec)';
