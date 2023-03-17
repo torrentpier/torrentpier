a28d00ce1f3401f43becc2dde5a75e169f3705a0
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 19:44:35 +0700

    New translations main.php (Indonesian)

--- library/language/id/main.php
+++ library/language/id/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Berikutnya Jalankan';
 $lang['CRON_RUN_COUNT'] = 'Berjalan';
 $lang['CRON_MANAGE'] = 'Mengelola';
 $lang['CRON_OPTIONS'] = 'Cron pilihan';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'Cron memungkinkan';
 $lang['CRON_CHECK_INTERVAL'] = 'Cron check interval (sec)';
