628f86ec9d02cb81aad8ea2b2d95d3e23d890fef
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 19:43:59 +0700

    New translations main.php (Finnish)

--- library/language/fi/main.php
+++ library/language/fi/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Seuraava Ajaa';
 $lang['CRON_RUN_COUNT'] = 'Toimii';
 $lang['CRON_MANAGE'] = 'Hallita';
 $lang['CRON_OPTIONS'] = 'Cron vaihtoehtoja';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'Cron käytössä';
 $lang['CRON_CHECK_INTERVAL'] = 'Cron-tarkista väli (sec)';
