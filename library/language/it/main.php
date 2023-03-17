1a13ba3cd8cbfaf4584e53da97d5cc753fa6cd0a
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 19:44:03 +0700

    New translations main.php (Italian)

--- library/language/it/main.php
+++ library/language/it/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Prossima Corsa';
 $lang['CRON_RUN_COUNT'] = 'Corre';
 $lang['CRON_MANAGE'] = 'Gestire';
 $lang['CRON_OPTIONS'] = 'Cron opzioni';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'Cron abilitato';
 $lang['CRON_CHECK_INTERVAL'] = 'Cron intervallo di controllo (sec)';
