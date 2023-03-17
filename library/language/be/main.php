b717dc36a4e1186622cd18deae5fce248846a37f
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 19:43:51 +0700

    New translations main.php (Belarusian)

--- library/language/be/main.php
+++ library/language/be/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Наступны Запуск';
 $lang['CRON_RUN_COUNT'] = 'Працуе';
 $lang['CRON_MANAGE'] = 'Кіраваць';
 $lang['CRON_OPTIONS'] = 'Варыянты хрон';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'Хранічны уключана';
 $lang['CRON_CHECK_INTERVAL'] = 'Хранічны інтэрвал праверкі (сек)';
