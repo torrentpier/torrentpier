16d005fb81adfb7ed2b134d3e24270be47da8f8c
Yury Pikhtarev <exileum@icloud.com>
2023-03-17 19:43:44 +0700

    New translations main.php (Romanian)

--- library/language/ro/main.php
+++ library/language/ro/main.php
@@ -2386,6 +2386,7 @@ $lang['CRON_NEXT_RUN'] = 'Următoarea Cursă';
 $lang['CRON_RUN_COUNT'] = 'Ruleaza';
 $lang['CRON_MANAGE'] = 'Gestiona';
 $lang['CRON_OPTIONS'] = 'Cron opțiuni';
+$lang['CRON_DISABLED_WARNING'] = 'Warning! Running cron scripts is disabled. To enable it, set the APP_CRON_ENABLED variable.';
 
 $lang['CRON_ENABLED'] = 'Cron activat';
 $lang['CRON_CHECK_INTERVAL'] = 'Cron interval de verificare (sec)';
