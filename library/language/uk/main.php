648564f4d95944f762b609c22eda356faf5b5659
Yury Pikhtarev <exileum@icloud.com>
2023-03-20 01:32:46 +0700

    New translations main.php (Ukrainian)

--- library/language/uk/main.php
+++ library/language/uk/main.php
@@ -2809,6 +2809,7 @@ $lang['CAPTCHA_WRONG'] = 'Ви не змогли підтвердити, що В
 $lang['CAPTCHA_SETTINGS'] = '<h2>ReCaptcha налаштована не повністю</h2><p>Якщо Ви ще не згенерували ключі, можете це зробити на сторінці <a href="https://www.google.com/recaptcha/admin">https://www.google.com/recaptcha/admin</a>.<br />Після того, як Ви згенеруєте ключі, потрібно прописати їх у файл library/config.php.</p>';
 
 // Sending email
+$lang['REPLY_TO'] = 'Reply to';
 $lang['EMAILER_SUBJECT'] = [
     'EMPTY' => 'Без теми',
     'GROUP_ADDED' => 'Вас додали до групи користувачів',
