fa2624e8e83a2ebe837cc71bddf1cdc0c8d64119
Yury Pikhtarev <exileum@icloud.com>
2023-03-20 01:32:28 +0700

    New translations main.php (Greek)

--- library/language/el/main.php
+++ library/language/el/main.php
@@ -2809,6 +2809,7 @@ $lang['CAPTCHA_WRONG'] = 'Δεν θα μπορούσε να επιβεβαιώσ
 $lang['CAPTCHA_SETTINGS'] = '<h2>ReCaptcha δεν είναι πλήρως configured</h2><p>if δεν έχετε δημιουργήσει ήδη τα κλειδιά, μπορείτε να το κάνετε στο <a href="https://www.google.com/recaptcha/admin">https://www.η google.com/recaptcha/admin</a>.<br />After μπορείτε να δημιουργήσετε τα κλειδιά, θα πρέπει να τους βάλει στο αρχείο βιβλιοθήκη/config.php.</p>';
 
 // Sending email
+$lang['REPLY_TO'] = 'Reply to';
 $lang['EMAILER_SUBJECT'] = [
     'EMPTY' => 'Χωρίς θέμα',
     'GROUP_ADDED' => 'Έχετε προστεθεί στην ομάδα χρηστών',
