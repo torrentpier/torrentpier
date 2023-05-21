1aa76f5abc8368180306fd8c1b0ad7f1d7d34dd7
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:27:23 +0300

    New translations main.php (Spanish)

--- library/language/es/main.php
+++ library/language/es/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'fuera de';
 $lang['MEMORY'] = 'Mem: ';
 $lang['QUERIES'] = 'consultas';
 $lang['LIMIT'] = 'Limitar:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'Descargar libre y a la máxima velocidad!';
