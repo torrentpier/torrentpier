40b524d4046f860a6e339e89df04261decc9ffcc
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:27:46 +0300

    New translations main.php (Lithuanian)

--- library/language/lt/main.php
+++ library/language/lt/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'off';
 $lang['MEMORY'] = 'Mem: ';
 $lang['QUERIES'] = 'užklausų';
 $lang['LIMIT'] = 'Ribos:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'Atsisiųsti nemokamai ir esant didžiausiam greičiui!';
