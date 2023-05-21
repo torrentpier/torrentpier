eea02b49b47230e7d9fbf5291494036c75dd1a2a
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:27:31 +0300

    New translations main.php (Czech)

--- library/language/cs/main.php
+++ library/language/cs/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'z';
 $lang['MEMORY'] = 'Mem: ';
 $lang['QUERIES'] = 'dotazy';
 $lang['LIMIT'] = 'Limit:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'Stáhnout zdarma a při maximální rychlosti!';
