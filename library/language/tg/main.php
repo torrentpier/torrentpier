399884ac2a7223ac9217be70a75d35010a13d129
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:28:19 +0300

    New translations main.php (Tajik)

--- library/language/tg/main.php
+++ library/language/tg/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'хомӯш кардан';
 $lang['MEMORY'] = 'Интх:';
 $lang['QUERIES'] = 'дархостҳо';
 $lang['LIMIT'] = 'Ҳадди:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'Download озод ва дар ҳадди суръати!';
