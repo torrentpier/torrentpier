ac42a5d14f2b383e90e87586d86dd805e906ac80
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:28:18 +0300

    New translations main.php (Uzbek)

--- library/language/uz/main.php
+++ library/language/uz/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'off';
 $lang['MEMORY'] = 'Mem:';
 $lang['QUERIES'] = 'so\'rovlarni';
 $lang['LIMIT'] = 'Limit:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'bepul va maksimal tezlikda yuklab oling!';
