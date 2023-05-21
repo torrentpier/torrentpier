227844f290cf247c684fd266fc036b7adad86e38
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:27:26 +0300

    New translations main.php (Arabic)

--- library/language/ar/main.php
+++ library/language/ar/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'قبالة';
 $lang['MEMORY'] = 'Mem: ';
 $lang['QUERIES'] = 'الاستفسارات';
 $lang['LIMIT'] = 'الحد:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'تحميل مجانا و في أقصى سرعة!';
