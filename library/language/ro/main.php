e17317691ee6a8435906af745a891009b84359d5
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:27:21 +0300

    New translations main.php (Romanian)

--- library/language/ro/main.php
+++ library/language/ro/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'off';
 $lang['MEMORY'] = 'Mem: ';
 $lang['QUERIES'] = 'interogări';
 $lang['LIMIT'] = 'Limita:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'Descărcați gratuit și la viteza maximă!';
