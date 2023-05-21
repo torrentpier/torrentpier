62eb69d1266b613b19f11186c4d4e49c35764f72
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:27:41 +0300

    New translations main.php (Italian)

--- library/language/it/main.php
+++ library/language/it/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'off';
 $lang['MEMORY'] = 'Mem: ';
 $lang['QUERIES'] = 'query';
 $lang['LIMIT'] = 'Limite:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'Free Download alla massima velocità!';
