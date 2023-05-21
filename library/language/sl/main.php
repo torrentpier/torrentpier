13e785035da31a149a2f0b3e6811346da19cfef3
Yury Pikhtarev <exileum@icloud.com>
2023-05-21 07:27:54 +0300

    New translations main.php (Slovenian)

--- library/language/sl/main.php
+++ library/language/sl/main.php
@@ -1481,6 +1481,9 @@ $lang['OFF'] = 'off';
 $lang['MEMORY'] = 'Tel: ';
 $lang['QUERIES'] = 'poizvedbe';
 $lang['LIMIT'] = 'Omejitev:';
+$lang['SHOW_LOG'] = 'Show log';
+$lang['EXPLAINED_LOG'] = 'Explained log';
+$lang['CUT_LOG'] = 'Cut long queries';
 
 // Attach Guest
 $lang['DOWNLOAD_INFO'] = 'Prenesite brezplačne in pri maksimalni hitrosti!';
