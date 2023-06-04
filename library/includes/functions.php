659e938c25628937546725521d4f8afedddb9ae4
Roman Kelesidis <roman25052006.kelesh@Gmail.com>
2023-06-04 10:20:38 +0700

    Generate jumpbox files only if enabled

--- library/includes/functions.php
+++ library/includes/functions.php
@@ -1000,7 +1000,11 @@ function get_userdata($u, $force_name = false, $allow_guest = false)
 
 function make_jumpbox($selected = 0)
 {
-    global $datastore, $template;
+    global $datastore, $template, $bb_cfg;
+
+    if (!$bb_cfg['show_jumpbox']) {
+        return;
+    }
 
     if (!$jumpbox = $datastore->get('jumpbox')) {
         $datastore->update('jumpbox');
