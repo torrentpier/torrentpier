133eb086bf4b2726744311184f74d9f0bb4bea88
Yury Pikhtarev <exileum@icloud.com>
2023-06-05 15:40:20 +0300

    New translations main.php (Catalan)

--- library/language/ca/main.php
+++ library/language/ca/main.php
@@ -1645,7 +1645,7 @@ $lang['NOT_AVAILABLE'] = 'No disponible';
 
 // Clear Cache
 $lang['DATASTORE'] = 'Datastore';
-$lang['CLEAR_CACHE'] = 'Esborra La Memòria Cau';
+$lang['CLEAR_CACHE'] = 'Clear Cache';
 $lang['CLEAR_TEMPLATES_CACHE'] = 'Templates cache';
 
 // Update
