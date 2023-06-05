4157e50b4086d3ced1715522f44816bea7fe42ab
Yury Pikhtarev <exileum@icloud.com>
2023-06-05 15:40:47 +0300

    New translations main.php (Albanian)

--- library/language/sq/main.php
+++ library/language/sq/main.php
@@ -1645,7 +1645,7 @@ $lang['NOT_AVAILABLE'] = 'Jo në dispozicion';
 
 // Clear Cache
 $lang['DATASTORE'] = 'Datastore';
-$lang['CLEAR_CACHE'] = 'Qartë Cache';
+$lang['CLEAR_CACHE'] = 'Clear Cache';
 $lang['CLEAR_TEMPLATES_CACHE'] = 'Templates cache';
 
 // Update
