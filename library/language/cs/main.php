3e89faf1dd0cf802ae8934988c192efd59ddc2d0
Yury Pikhtarev <exileum@icloud.com>
2023-06-05 15:40:21 +0300

    New translations main.php (Czech)

--- library/language/cs/main.php
+++ library/language/cs/main.php
@@ -1645,7 +1645,7 @@ $lang['NOT_AVAILABLE'] = 'Není k dispozici';
 
 // Clear Cache
 $lang['DATASTORE'] = 'Datastore';
-$lang['CLEAR_CACHE'] = 'Vymazat Mezipaměť';
+$lang['CLEAR_CACHE'] = 'Clear Cache';
 $lang['CLEAR_TEMPLATES_CACHE'] = 'Templates cache';
 
 // Update
