de3e622bc9c17ce6e4a2c8c97927f475ee2dc893
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:32 +0300

    New translations main.php (Albanian)

--- library/language/sq/main.php
+++ library/language/sq/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Mirë se vini në faqen %s',
     'USER_WELCOME_INACTIVE' => 'Mirë se vini në faqen %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
