11a49d5e074071cf068600cded298ddffa1d6065
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:29 +0300

    New translations main.php (Slovak)

--- library/language/sk/main.php
+++ library/language/sk/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Vitajte na stránke %s',
     'USER_WELCOME_INACTIVE' => 'Vitajte na stránke %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
