fc06d35ede45dfd4da5571b2bc6149e453665b88
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:18:54 +0300

    New translations main.php (Spanish)

--- library/language/es/main.php
+++ library/language/es/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Bienvenido al sitio %s',
     'USER_WELCOME_INACTIVE' => 'Bienvenido al sitio %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
