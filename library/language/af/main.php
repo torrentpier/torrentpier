959b7278a9931e30b25d4aeda9dccbd6320d670c
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:18:55 +0300

    New translations main.php (Afrikaans)

--- library/language/af/main.php
+++ library/language/af/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Welkom by die webwerf %s',
     'USER_WELCOME_INACTIVE' => 'Welkom by die webwerf %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
