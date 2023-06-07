efe3f4fd858f4bca8f723cc02a06b7d88c207b45
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:22 +0300

    New translations main.php (Dutch)

--- library/language/nl/main.php
+++ library/language/nl/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Welkom op de site %s',
     'USER_WELCOME_INACTIVE' => 'Welkom op de site %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
