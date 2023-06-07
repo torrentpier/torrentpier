bb848087c2043c9109b602093301a55a40e5f290
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:31 +0300

    New translations main.php (Slovenian)

--- library/language/sl/main.php
+++ library/language/sl/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Dobrodošli na spletnem mestu %s',
     'USER_WELCOME_INACTIVE' => 'Dobrodošli na spletnem mestu %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
