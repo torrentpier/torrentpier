5cd95b71ed3e7d7034300b2394d7bcd87564af1d
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:11 +0300

    New translations main.php (Hebrew)

--- library/language/he/main.php
+++ library/language/he/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'ברוכים הבאים לאתר %s',
     'USER_WELCOME_INACTIVE' => 'ברוכים הבאים לאתר %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
