9d3df2dd6785991c2e5d7b15a8ce96c63d28b63c
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:18:51 +0300

    New translations main.php (Romanian)

--- library/language/ro/main.php
+++ library/language/ro/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Bun venit pe site-ul %s',
     'USER_WELCOME_INACTIVE' => 'Bun venit pe site-ul %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
