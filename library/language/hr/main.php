faeec56b2d5b7b43e8e821c83f879ecbfaf7d5ed
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:46 +0300

    New translations main.php (Croatian)

--- library/language/hr/main.php
+++ library/language/hr/main.php
@@ -2835,9 +2835,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Dobrodošli na stranicu %s',
     'USER_WELCOME_INACTIVE' => 'Dobrodošli na stranicu %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
