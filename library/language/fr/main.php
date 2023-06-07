5aa0e8aa1d551d0f9a6a7c741e9747a39ed75ac7
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:18:52 +0300

    New translations main.php (French)

--- library/language/fr/main.php
+++ library/language/fr/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Bienvenue sur le site %s',
     'USER_WELCOME_INACTIVE' => 'Bienvenue sur le site %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
