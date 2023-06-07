ad3525a5083d403217e365e5f0a228b6bb68fd4b
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:47 +0300

    New translations main.php (Kazakh)

--- library/language/kk/main.php
+++ library/language/kk/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Сайтқа қош келдіңіз %s',
     'USER_WELCOME_INACTIVE' => 'Сайтқа қош келдіңіз %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
