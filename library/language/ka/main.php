25eaef1015ee78ba04fdbb07de83fdf82c9939aa
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:18 +0300

    New translations main.php (Georgian)

--- library/language/ka/main.php
+++ library/language/ka/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'კეთილი იყოს თქვენი მობრძანება საიტზე %s',
     'USER_WELCOME_INACTIVE' => 'კეთილი იყოს თქვენი მობრძანება საიტზე %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
