10f115debf4e62a7a622d95448abf4b97125ee7a
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:57 +0300

    New translations main.php (Tajik)

--- library/language/tg/main.php
+++ library/language/tg/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Хуш омадед ба сайт %s',
     'USER_WELCOME_INACTIVE' => 'Хуш омадед ба сайт %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
