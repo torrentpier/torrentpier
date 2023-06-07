d6e424ff461a4bd7d2a1ace2af9dcfb42a0742d0
Yury Pikhtarev <exileum@icloud.com>
2023-06-07 10:19:40 +0300

    New translations main.php (Vietnamese)

--- library/language/vi/main.php
+++ library/language/vi/main.php
@@ -2831,9 +2831,3 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Chào mừng đến với trang web %s',
     'USER_WELCOME_INACTIVE' => 'Chào mừng đến với trang web %s',
 ];
-
-// Devices
-$lang['DEVICE'] = 'Device';
-$lang['DEVICE_MOBILE'] = 'Phone';
-$lang['DEVICE_TABLET'] = 'Tablet';
-$lang['DEVICE_PC'] = 'PC';
