3017b86f363473ff72547fc0da52adece27912e1
Yury Pikhtarev <exileum@icloud.com>
2023-07-23 21:12:25 +0300

    New translations main.php (Vietnamese)

--- library/language/vi/main.php
+++ library/language/vi/main.php
@@ -25,6 +25,7 @@ $lang['POSTS_SHORT'] = 'Bài';
 $lang['POSTED'] = 'Đăng';
 $lang['USERNAME'] = 'Tên';
 $lang['PASSWORD'] = 'Mật khẩu';
+$lang['PASSWORD_SHOW_BTN'] = 'Show password';
 $lang['EMAIL'] = 'Email';
 $lang['PM'] = 'PM';
 $lang['AUTHOR'] = 'Giả';
