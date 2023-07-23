d1b33b450519ddc7166839b6a1ed2e4b412d088b
Yury Pikhtarev <exileum@icloud.com>
2023-07-23 20:07:14 +0300

    New translations main.php (Slovak)

--- library/language/sk/main.php
+++ library/language/sk/main.php
@@ -1068,6 +1068,8 @@ $lang['BT_GEN_PASSKEY'] = 'vytvoriť nový';
 $lang['BT_PASSKEY_VIEW'] = 'zobraziť';
 $lang['BT_GEN_PASSKEY_NEW'] = "Pozornosť! Po výmene nové kľúče, budete musieť znova stiahnuť všetky aktívne torrentov! \n Sú si istí, že chcete vytvoriť nový univerzálny kľúč?";
 $lang['BT_NO_SEARCHABLE_FORUMS'] = 'Č vyhľadávať fóra našiel';
+$lang['PASSKEY_ALERT_TITLE'] = 'Creating a passkey';
+$lang['PASSKEY_ALERT_INFO'] = 'To use the tracker functionality, you need to create a passkey.';
 
 $lang['SEEDS'] = 'Semeno';
 $lang['LEECHS'] = 'Pijavice';
