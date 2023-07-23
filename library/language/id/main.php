e1ca07b45a29358aa29b07aeac55fbe3437104cd
Yury Pikhtarev <exileum@icloud.com>
2023-07-23 21:12:27 +0300

    New translations main.php (Indonesian)

--- library/language/id/main.php
+++ library/language/id/main.php
@@ -25,6 +25,7 @@ $lang['POSTS_SHORT'] = 'Posting';
 $lang['POSTED'] = 'Diposting';
 $lang['USERNAME'] = 'Username';
 $lang['PASSWORD'] = 'Password';
+$lang['PASSWORD_SHOW_BTN'] = 'Show password';
 $lang['EMAIL'] = 'Email';
 $lang['PM'] = 'PM';
 $lang['AUTHOR'] = 'Penulis';
