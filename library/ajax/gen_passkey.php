d9bfe1d956e4421fd53ace5d94bafd0989430d21
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-07-24 13:29:30 +0700

    Minor improvements

--- library/ajax/gen_passkey.php
+++ library/ajax/gen_passkey.php
@@ -14,22 +14,40 @@ if (!defined('IN_AJAX')) {
 global $userdata, $lang;
 
 $req_uid = (int)$this->request['user_id'];
+$mode = (string)$this->request['mode'];
 
 if ($req_uid == $userdata['user_id'] || IS_ADMIN) {
-    $first_creation = !\TorrentPier\Legacy\Torrent::getPasskey($req_uid);
+    switch ($mode) {
+        case 'create':
+            $first_creation = !\TorrentPier\Legacy\Torrent::getPasskey($req_uid);
 
-    if (empty($this->request['confirmed']) && !$first_creation) {
-        $this->prompt_for_confirm($lang['BT_GEN_PASSKEY_NEW']);
+            if (empty($this->request['confirmed']) && !$first_creation) {
+                $this->prompt_for_confirm($lang['BT_GEN_PASSKEY_NEW']);
+            }
+
+            if (!$passkey = \TorrentPier\Legacy\Torrent::generate_passkey($req_uid, IS_ADMIN)) {
+                $this->ajax_die('Could not insert passkey');
+            }
+
+            \TorrentPier\Legacy\Torrent::tracker_rm_user($req_uid);
+
+            $this->response['first_creation'] = $first_creation;
+            $this->response['passkey'] = $passkey;
+            break;
+        case 'remove':
+            if (empty($this->request['confirmed'])) {
+                $this->prompt_for_confirm($lang['QUESTION']);
+            }
+
+            if (!$delete = \TorrentPier\Legacy\Torrent::deletePasskey($req_uid)) {
+                $this->ajax_die('Could not remove passkey');
+            }
+
+            $this->response['passkey_removed'] = $delete;
+            break;
+        default:
+            $this->ajax_die('Invalid mode');
     }
-
-    if (!$passkey = \TorrentPier\Legacy\Torrent::generate_passkey($req_uid, IS_ADMIN)) {
-        $this->ajax_die('Could not insert passkey');
-    }
-
-    \TorrentPier\Legacy\Torrent::tracker_rm_user($req_uid);
-
-    $this->response['first_creation'] = $first_creation;
-    $this->response['passkey'] = $passkey;
 } else {
     $this->ajax_die($lang['NOT_AUTHORISED']);
 }
