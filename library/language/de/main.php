ab037fcc1e4266b9116ceec1ad379d9ed5bdc942
Yury Pikhtarev <exileum@icloud.com>
2023-08-22 22:46:59 +0400

    New translations main.php (German)

--- library/language/de/main.php
+++ library/language/de/main.php
@@ -1719,6 +1719,8 @@ $lang['USER_AUTH_UPDATED'] = 'Benutzer-Berechtigungen wurden aktualisiert';
 $lang['GROUP_AUTH_UPDATED'] = 'Gruppe Berechtigungen wurden aktualisiert';
 
 $lang['AUTH_UPDATED'] = 'Berechtigungen wurden aktualisiert';
+$lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
+$lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
 $lang['CLICK_RETURN_USERAUTH'] = 'Klicken Sie auf %sHere%s zurück zu Benutzer-Berechtigungen';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'Klicken Sie auf %sHere%s zurück, um eine Gruppe von Berechtigungen';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'Klicken Sie auf %sHere%s zurück zu Forum-Berechtigungen';
