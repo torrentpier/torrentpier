2388ea447fabe8b4301be39e253eb04d71b041ab
Yury Pikhtarev <exileum@icloud.com>
2023-08-27 18:11:12 +0400

    New translations main.php (Korean)

--- library/language/ko/main.php
+++ library/language/ko/main.php
@@ -1069,8 +1069,6 @@ $lang['BT_GEN_PASSKEY'] = '새로 만들고';
 $lang['BT_PASSKEY_VIEW'] = '보';
 $lang['BT_GEN_PASSKEY_NEW'] = "주의! 후 변경하는 새로운 암호,당신을 다시 다운로드해야 하는 모든 활동 급류! \n 확신을 만들고 싶은 새로 암호를니까?";
 $lang['BT_NO_SEARCHABLE_FORUMS'] = '일 수 있 포럼 발견';
-$lang['PASSKEY_ALERT_TITLE'] = 'Creating a passkey';
-$lang['PASSKEY_ALERT_INFO'] = 'To use the tracker functionality, you need to create a passkey.';
 
 $lang['SEEDS'] = '씨';
 $lang['LEECHS'] = '니';
