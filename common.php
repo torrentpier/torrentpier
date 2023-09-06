821dbd739a4bbf0b1be2234ddede9e991f2a6f1f
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-09-06 16:05:18 +0700

    Update common.php

--- common.php
+++ common.php
@@ -107,7 +107,7 @@ define('BT_AUTH_KEY_LENGTH', 15);
 define('PEER_HASH_PREFIX', 'peer_');
 define('PEERS_LIST_PREFIX', 'peers_list_');
 define('PEER_HASH_EXPIRE', round($bb_cfg['announce_interval'] * (0.85 * $bb_cfg['tracker']['expire_factor'])));
-define('PEERS_LIST_EXPIRE', $bb_cfg['announce_interval'] * 0.7);
+define('PEERS_LIST_EXPIRE', round($bb_cfg['announce_interval'] * 0.7));
 
 define('DL_STATUS_RELEASER', -1);
 define('DL_STATUS_DOWN', 0);
