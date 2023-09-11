159dce7bd45cad936f28354258175dcb9c203045
Constantine Kovalensky <45331093+kovalensky@users.noreply.github.com>
2023-09-11 16:20:01 +0400

    Magnet name re-formatting

--- library/includes/functions.php
+++ library/includes/functions.php
@@ -1799,11 +1799,7 @@ function create_magnet(string $infohash, string $infohash_v2, string $auth_key, 
         $magnet .= 'xt=urn:btmh:1220' . bin2hex($infohash_v2);
     }
 
-    if (!empty($name)) {
-        $magnet .= '&dn=' . urlencode($name);
-    }
-
-    return '<a href="' . $magnet . '&tr=' . urlencode($bb_cfg['bt_announce_url'] . "?{$bb_cfg['passkey_key']}=$auth_key") . '"><img src="' . $images['icon_magnet'] . '" width="12" height="12" border="0" /></a>';
+    return '<a href="' . $magnet . '&tr=' . urlencode($bb_cfg['bt_announce_url'] . "?{$bb_cfg['passkey_key']}=$auth_key") . '&dn=' . urlencode($name) . '"><img src="' . $images['icon_magnet'] . '" width="12" height="12" border="0" /></a>';
 }
 
 function set_die_append_msg($forum_id = null, $topic_id = null, $group_id = null)
