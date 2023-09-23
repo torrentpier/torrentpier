7874b75f35fd74e78e766d974a7e8078454d4eec
Yury Pikhtarev <exileum@icloud.com>
2023-09-23 21:27:42 +0400

    New translations main.php (Danish)

--- library/language/da/main.php
+++ library/language/da/main.php
@@ -234,7 +234,8 @@ $lang['RULES_VOTE_CAN'] = 'Du <b>can</b> stemme på afstemninger i dette forum';
 $lang['RULES_VOTE_CANNOT'] = 'Du <b>cannot</b> stemme på afstemninger i dette forum';
 $lang['RULES_MODERATE'] = 'Du <b>can</b> moderat dette forum';
 
-$lang['NO_TOPICS_POST_ONE'] = 'Der er ingen indlæg i dette forum.<br />Click på <b>Post Nye Topic</b> link på denne side til at sende en.';
+$lang['NO_TOPICS_POST_ONE'] = 'There are no posts in this forum yet<br />Click on the <b>New Topic</b> icon, and your post will be the first.';
+$lang['NO_RELEASES_POST_ONE'] = 'There are no releases in this forum yet<br />Click on the <b>New Release</b> icon, and your release will be the first.';
 
 // Viewtopic
 $lang['VIEW_TOPIC'] = 'Vis emne';
