c61ac0dd6521c730f7f7456375f98475c68e7806
Yury Pikhtarev <exileum@icloud.com>
2023-10-13 13:43:38 +0400

    New translations main.php (Indonesian)

--- library/language/id/main.php
+++ library/language/id/main.php
@@ -1542,6 +1542,7 @@ $lang['DL_DL'] = 'DL';
 $lang['DL_UL_SPEED'] = 'UL kecepatan';
 $lang['DL_DL_SPEED'] = 'DL kecepatan';
 $lang['DL_PORT'] = 'Port';
+$lang['DL_CLIENT'] = 'Client';
 $lang['DL_FORMULA'] = 'Rumus: Upload/TorrentSize';
 $lang['DL_ULR'] = 'URL';
 $lang['DL_STOPPED'] = 'berhenti';
