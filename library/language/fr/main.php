4f6b7a6c53312045308f60fad6c29a2182363fc4
Yury Pikhtarev <exileum@icloud.com>
2023-10-13 13:44:04 +0400

    New translations main.php (French)

--- library/language/fr/main.php
+++ library/language/fr/main.php
@@ -1542,6 +1542,7 @@ $lang['DL_DL'] = 'DL';
 $lang['DL_UL_SPEED'] = 'UL vitesse';
 $lang['DL_DL_SPEED'] = 'DL vitesse';
 $lang['DL_PORT'] = 'Port';
+$lang['DL_CLIENT'] = 'Client';
 $lang['DL_FORMULA'] = 'Formule: Uploaded/TorrentSize';
 $lang['DL_ULR'] = 'URL';
 $lang['DL_STOPPED'] = 'arrêté';
