11025d6709e8b730329c715f2da8a0e57dd4ad01
Yury Pikhtarev <exileum@icloud.com>
2023-10-13 13:42:59 +0400

    New translations main.php (Georgian)

--- library/language/ka/main.php
+++ library/language/ka/main.php
@@ -1542,6 +1542,7 @@ $lang['DL_DL'] = 'DL';
 $lang['DL_UL_SPEED'] = 'UL სიჩქარე';
 $lang['DL_DL_SPEED'] = 'DL სიჩქარე';
 $lang['DL_PORT'] = 'პორტი';
+$lang['DL_CLIENT'] = 'Client';
 $lang['DL_FORMULA'] = 'ფორმულა: აიტვირთა/TorrentSize';
 $lang['DL_ULR'] = 'URL';
 $lang['DL_STOPPED'] = 'შეწყვიტა';
