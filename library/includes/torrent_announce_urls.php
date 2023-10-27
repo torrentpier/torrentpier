00198350e1ae82b6be0c811206594172a84d6c00
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-10-27 14:44:23 +0700

    Added the ability to add additional announce URLs into torrent files

--- library/includes/torrent_announce_urls.php
+++ library/includes/torrent_announce_urls.php
@@ -11,10 +11,27 @@ if (!defined('BB_ROOT')) {
     die(basename(__FILE__));
 }
 
-$announce_urls = [];
+$announce_urls = $additional_announce_urls = [];
 
-// Here you can define additional allowed announce urls
-// For example, if you want to add http://demo.torrentpier.com
-// add this line: $announce_urls[] = 'http://demo.torrentpier.com/bt/announce.php';
-
+// ==============================================================================================================================
+// Allowed Announcer URLs
+// ------------------------------------------------------------------------------------------------------------------------------
+// Examples:
 // $announce_urls[] = 'http://demo.torrentpier.com/bt/announce.php';
+// $announce_urls[] = 'http://tracker.openbittorrent.com:80/announce';
+// $announce_urls[] = 'udp://tracker.openbittorrent.com:6969/announce';
+// ------------------------------------------------------------------------------------------------------------------------------
+// Note:
+// - Add URLs without GET parameters at the end
+// - For this file to work, you need to enable the "Check announce url" option in the admin panel in "Forum Settings"
+// ==============================================================================================================================
+// Additional announcer URLs that will be added to your giveaways
+// ------------------------------------------------------------------------------------------------------------------------------
+// Examples:
+// $additional_announce_urls[] = 'http://tracker.openbittorrent.com:80/announce';
+// $additional_announce_urls[] = 'udp://tracker.openbittorrent.com:6969/announce';
+// ------------------------------------------------------------------------------------------------------------------------------
+// Note:
+// - It is better not to add advertisers with GET parameters (for example passkey or another access authenticator)
+// - For this file to work, you need to disable the option “Delete all additional announce urls” in the admin panel in “Forum Settings”
+// ==============================================================================================================================
