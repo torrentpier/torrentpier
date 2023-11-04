eede0ea21c8a89e7fb00185dbeb46f9b5eb70ea5
Yury Pikhtarev <exileum@icloud.com>
2023-11-04 19:26:44 +0400

    New translations main.php (German)

--- library/language/de/main.php
+++ library/language/de/main.php
@@ -1084,6 +1084,7 @@ $lang['SEEDING'] = 'Samen';
 $lang['LEECHING'] = 'Blutegel';
 $lang['IS_REGISTERED'] = 'Registriert';
 $lang['MAGNET'] = 'Magnet-link';
+$lang['MAGNET_v2'] = 'Magnet-link (BitTorrent v2 supported)';
 
 //torrent status mod
 $lang['TOR_STATUS'] = 'Status';
