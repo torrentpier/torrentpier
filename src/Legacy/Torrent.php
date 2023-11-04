60bace1d16e54afa6dc2eb2d3b86e23288786ab0
Constantine Kovalensky <45331093+kovalensky@users.noreply.github.com>
2023-11-04 10:28:26 +0400

    Update Torrent.php

--- src/Legacy/Torrent.php
+++ src/Legacy/Torrent.php
@@ -388,7 +388,7 @@ class Torrent
         $totallen = 0;
         if (isset($info['length'])) {
             $totallen = (float)$info['length'];
-        } elseif ($bt_v1 && isset($info['files']) && \is_array($info['files'])) {
+        } elseif (isset($bt_v1, $info['files']) && \is_array($info['files'])) {
             foreach ($info['files'] as $fn => $f) {
                 // Exclude padding files
                 if (($f['attr'] ?? null) !== 'p') {
