05b5a2ddadfde1882aaee9ba4d536701f9a194ef
Yury Pikhtarev <exileum@icloud.com>
2023-11-04 18:17:28 +0400

    New translations main.php (Croatian)

--- library/language/hr/main.php
+++ library/language/hr/main.php
@@ -1059,6 +1059,7 @@ $lang['BT_ADDED'] = 'Objavljeno';
 $lang['BT_REG_ON_TRACKER'] = 'Prijavite se na adresi';
 $lang['BT_REG_FAIL'] = 'Nije moguće registrirati torrent na adresi';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Drugi torrent sa istim info_hash već <a href="%s"><b>registered</b></a>';
+$lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Ukloni iz tracker';
 $lang['BT_UNREGISTERED'] = 'Torrent neregistrirane';
 $lang['BT_UNREGISTERED_ALREADY'] = 'Torrent already unregistered';
