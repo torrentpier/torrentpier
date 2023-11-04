df2990c869f5ecc02314154aafb111457fb8275f
Yury Pikhtarev <exileum@icloud.com>
2023-11-04 18:17:18 +0400

    New translations main.php (Ukrainian)

--- library/language/uk/main.php
+++ library/language/uk/main.php
@@ -1059,6 +1059,7 @@ $lang['BT_ADDED'] = 'Додано';
 $lang['BT_REG_ON_TRACKER'] = 'Зареєструватися на трекері';
 $lang['BT_REG_FAIL'] = 'Не вдалося зареєструвати торент на трекері';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Інший торрент з таким же info_hash вже <a href="%s"><b>зареєстрований</b></a>';
+$lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Вилучити з трекера';
 $lang['BT_UNREGISTERED'] = 'Торент незареєстрований';
 $lang['BT_UNREGISTERED_ALREADY'] = 'Torrent already unregistered';
