bd75dd1862bd02a56f6ad6d5e5744aba570e4e46
Yury Pikhtarev <exileum@icloud.com>
2023-11-04 19:27:35 +0400

    New translations main.php (Tajik)

--- library/language/tg/main.php
+++ library/language/tg/main.php
@@ -1084,6 +1084,7 @@ $lang['SEEDING'] = 'тухмӣ';
 $lang['LEECHING'] = 'пиявка';
 $lang['IS_REGISTERED'] = 'Сабтшуда';
 $lang['MAGNET'] = 'Magnet-link';
+$lang['MAGNET_v2'] = 'Magnet-link (BitTorrent v2 supported)';
 
 //torrent status mod
 $lang['TOR_STATUS'] = 'Статуси';
