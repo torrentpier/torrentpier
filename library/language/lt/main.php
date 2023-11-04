beb29aee41d57ef39731e05b0d054a32613a0e41
Yury Pikhtarev <exileum@icloud.com>
2023-11-04 19:26:57 +0400

    New translations main.php (Lithuanian)

--- library/language/lt/main.php
+++ library/language/lt/main.php
@@ -1084,6 +1084,7 @@ $lang['SEEDING'] = 'Sėklos';
 $lang['LEECHING'] = 'Dėlės';
 $lang['IS_REGISTERED'] = 'Registruotas';
 $lang['MAGNET'] = 'Magnet-link';
+$lang['MAGNET_v2'] = 'Magnet-link (BitTorrent v2 supported)';
 
 //torrent status mod
 $lang['TOR_STATUS'] = 'Statusas';
