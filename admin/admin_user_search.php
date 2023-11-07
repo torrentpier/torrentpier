130628248da010f737582195b4fd85fcdfd4d956
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-11-07 08:07:06 +0700

    Fixed: Moderators can't see self IP addresses (#1049)

--- admin/admin_user_search.php
+++ admin/admin_user_search.php
@@ -926,7 +926,7 @@ if (!isset($_REQUEST['dosearch'])) {
             'USER' => profile_url($rowset[$i]),
             'EMAIL' => $rowset[$i]['user_email'],
             'JOINDATE' => bb_date($rowset[$i]['user_regdate']),
-            'LASTVISIT' => bb_date($rowset[$i]['user_lastvisit']),
+            'LASTVISIT' => $rowset[$i]['user_lastvisit'] ? bb_date($rowset[$i]['user_lastvisit']) : $lang['NEVER'],
             'POSTS' => $rowset[$i]['user_posts'],
             'BAN' => (!isset($banned[$rowset[$i]['user_id']])) ? $lang['NOT_BANNED'] : $lang['BANNED'],
             'ABLED' => $rowset[$i]['user_active'] ? $lang['ENABLED'] : $lang['DISABLED'],
