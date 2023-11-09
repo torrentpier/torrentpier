c563993c5e40ebfd9f6e01adabed147dd3ed35c9
Yury Pikhtarev <exileum@icloud.com>
2023-11-09 20:21:17 +0400

    New translations main.php (Serbian (Cyrillic))

--- library/language/sr/main.php
+++ library/language/sr/main.php
@@ -1060,6 +1060,7 @@ $lang['BT_REG_ON_TRACKER'] = 'Пријавите се на хттп трацке
 $lang['BT_REG_FAIL'] = 'Не могу да се региструју на торрент трацкер';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Други торент са истим info_hash већ <a href="%s"><b>registered</b></a>';
 $lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
+$lang['BT_V2_FILE_LIST_ONLY'] = 'Currently, only torrents with BitTorrent version 2 support are enabled for separate file listing';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Уклоните из трацкер';
 $lang['BT_UNREGISTERED'] = 'Торрент нерегистрованих';
 $lang['BT_UNREGISTERED_ALREADY'] = 'Torrent already unregistered';
@@ -2321,6 +2322,8 @@ $lang['BT_ANNOUNCE_URL'] = 'Извештај УРЛ адреса';
 $lang['BT_ANNOUNCE_URL_EXPL'] = 'можете одредити додатне дозвољених УРЛ адресе у "includes/torrent_announce_urls.php"';
 $lang['BT_DISABLE_DHT'] = 'Онемогућите мрежу DHT у';
 $lang['BT_DISABLE_DHT_EXPL'] = 'Искључите дељење пирами и DHT (препоручује се за приватне мреже, само УРЛ адреса извештај)';
+$lang['BT_PRIVATE_TRACKER'] = 'This tracker is private: file listing (for guests), DHT | PEX are disabled';
+$lang['BT_PRIVATE_TORRENT'] = 'The creator of this torrent made it private';
 $lang['BT_CHECK_ANNOUNCE_URL'] = 'Проверите да обавести УРЛ адреса';
 $lang['BT_CHECK_ANNOUNCE_URL_EXPL'] = 'Пријавите се на хттп трацкер је дозвољено само УРЛ адресе';
 $lang['BT_REPLACE_ANN_URL'] = 'Замени објави УРЛ адреса';
