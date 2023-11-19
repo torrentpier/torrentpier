8f9e670c3dd3c796d28bf59d958b46e601077d9b
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-11-19 13:59:49 +0700

    Updated

--- library/ajax/mod_action.php
+++ library/ajax/mod_action.php
@@ -55,7 +55,7 @@ switch ($mode) {
 
         $topic_title_sql = DB()->escape($new_title);
 
-        DB()->query("UPDATE " . BB_TOPICS . " SET topic_title = '$topic_title_sql' WHERE topic_id = $topic_id");
+        DB()->query("UPDATE " . BB_TOPICS . " SET topic_title = '$topic_title_sql' WHERE topic_id = $topic_id LIMIT 1");
 
         // Обновление кеша новостей на главной
         $news_forums = array_flip(explode(',', $bb_cfg['latest_news_forum_id']));
