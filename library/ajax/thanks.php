d662311c2a7a2a04c042c2e685e361fc5a9674f0
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-11-21 00:55:45 +0700

    Vote button code improvements

--- library/ajax/thanks.php
+++ library/ajax/thanks.php
@@ -14,7 +14,7 @@ if (!defined('IN_AJAX')) {
 global $bb_cfg, $lang, $userdata;
 
 if (!$bb_cfg['tor_thank']) {
-    $this->ajax_die($lang['DISABLED']);
+    $this->ajax_die($lang['MODULE_OFF']);
 }
 
 $mode = (string)$this->request['mode'];
@@ -22,12 +22,12 @@ $topic_id = (int)$this->request['topic_id'];
 
 switch ($mode) {
     case 'add':
-        if (DB()->fetch_row('SELECT * FROM ' . BB_THX . " WHERE topic_id = $topic_id  AND user_id = " . $userdata['user_id'])) {
-            $this->ajax_die($lang['LIKE_ALREADY']);
-        }
-
         if (DB()->fetch_row('SELECT poster_id FROM ' . BB_BT_TORRENTS . " WHERE topic_id = $topic_id AND poster_id = " . $userdata['user_id'])) {
             $this->ajax_die($lang['LIKE_OWN_POST']);
+        }
+
+        if (DB()->fetch_row('SELECT topic_id FROM ' . BB_THX . " WHERE topic_id = $topic_id  AND user_id = " . $userdata['user_id'])) {
+            $this->ajax_die($lang['LIKE_ALREADY']);
         }
 
         $columns = 'topic_id, user_id, time';
@@ -51,3 +51,5 @@ switch ($mode) {
     default:
         $this->ajax_die('Invalid mode');
 }
+
+$this->response['mode'] = $mode;
