e099d6ef62ac83625ca4bda48e32d7b942a59fa6
Yury Pikhtarev <exileum@icloud.com>
2023-11-20 23:05:51 +0400

    New translations main.php (French)

--- library/language/fr/main.php
+++ library/language/fr/main.php
@@ -680,6 +680,14 @@ $lang['ASC'] = 'Ascendant';
 $lang['DESC'] = 'Descendant';
 $lang['ORDER'] = 'Commande';
 
+// Thanks
+$lang['THANK_TOPIC'] = 'Vote for this topic';
+$lang['THANKS_GRATITUDE'] = 'We appreciate your gratitude';
+$lang['LAST_LIKES'] = 'Last votes';
+$lang['LIKE_OWN_POST'] = 'You can\'t vote for your own topic';
+$lang['NO_LIKES'] = 'Nobody gave a vote yet';
+$lang['LIKE_ALREADY'] = 'You already voted this topic';
+
 // Group control panel
 $lang['GROUP_CONTROL_PANEL'] = 'Les Groupes D\'Utilisateurs';
 $lang['GROUP_CONFIGURATION'] = 'Configuration D\'Un Groupe De';
