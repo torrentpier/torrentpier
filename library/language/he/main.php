d59bed66c59716a30eaad124eb2204c8b6f74309
Yury Pikhtarev <exileum@icloud.com>
2023-11-20 23:06:10 +0400

    New translations main.php (Hebrew)

--- library/language/he/main.php
+++ library/language/he/main.php
@@ -680,6 +680,14 @@ $lang['ASC'] = 'עולה';
 $lang['DESC'] = 'יורד';
 $lang['ORDER'] = 'סדר';
 
+// Thanks
+$lang['THANK_TOPIC'] = 'Vote for this topic';
+$lang['THANKS_GRATITUDE'] = 'We appreciate your gratitude';
+$lang['LAST_LIKES'] = 'Last votes';
+$lang['LIKE_OWN_POST'] = 'You can\'t vote for your own topic';
+$lang['NO_LIKES'] = 'Nobody gave a vote yet';
+$lang['LIKE_ALREADY'] = 'You already voted this topic';
+
 // Group control panel
 $lang['GROUP_CONTROL_PANEL'] = 'קבוצות משתמשים';
 $lang['GROUP_CONFIGURATION'] = 'התצורה של קבוצת';
