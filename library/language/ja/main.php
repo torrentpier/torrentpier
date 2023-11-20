bdf7b02ec35b09355070e20ccd215f0dfb45125f
Yury Pikhtarev <exileum@icloud.com>
2023-11-20 23:06:16 +0400

    New translations main.php (Japanese)

--- library/language/ja/main.php
+++ library/language/ja/main.php
@@ -680,6 +680,14 @@ $lang['ASC'] = '昇順';
 $lang['DESC'] = '降順';
 $lang['ORDER'] = '注文';
 
+// Thanks
+$lang['THANK_TOPIC'] = 'Vote for this topic';
+$lang['THANKS_GRATITUDE'] = 'We appreciate your gratitude';
+$lang['LAST_LIKES'] = 'Last votes';
+$lang['LIKE_OWN_POST'] = 'You can\'t vote for your own topic';
+$lang['NO_LIKES'] = 'Nobody gave a vote yet';
+$lang['LIKE_ALREADY'] = 'You already voted this topic';
+
 // Group control panel
 $lang['GROUP_CONTROL_PANEL'] = 'ユーザーグループ';
 $lang['GROUP_CONFIGURATION'] = 'グループ設定';
