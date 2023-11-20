607df9ad5e89547c3e21387f6a1f2de202cd1c10
Yury Pikhtarev <exileum@icloud.com>
2023-11-20 23:05:55 +0400

    New translations main.php (Arabic)

--- library/language/ar/main.php
+++ library/language/ar/main.php
@@ -680,6 +680,14 @@ $lang['ASC'] = 'تصاعدي';
 $lang['DESC'] = 'تنازلي';
 $lang['ORDER'] = 'النظام';
 
+// Thanks
+$lang['THANK_TOPIC'] = 'Vote for this topic';
+$lang['THANKS_GRATITUDE'] = 'We appreciate your gratitude';
+$lang['LAST_LIKES'] = 'Last votes';
+$lang['LIKE_OWN_POST'] = 'You can\'t vote for your own topic';
+$lang['NO_LIKES'] = 'Nobody gave a vote yet';
+$lang['LIKE_ALREADY'] = 'You already voted this topic';
+
 // Group control panel
 $lang['GROUP_CONTROL_PANEL'] = 'مجموعات المستخدمين';
 $lang['GROUP_CONFIGURATION'] = 'فريق التكوين';
