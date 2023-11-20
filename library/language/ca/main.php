bb8a6d6c20033c3f773e3b2f33d7dfc0f3048a14
Yury Pikhtarev <exileum@icloud.com>
2023-11-20 23:06:00 +0400

    New translations main.php (Catalan)

--- library/language/ca/main.php
+++ library/language/ca/main.php
@@ -680,6 +680,14 @@ $lang['ASC'] = 'Ascendent';
 $lang['DESC'] = 'Descendent';
 $lang['ORDER'] = 'Ordre';
 
+// Thanks
+$lang['THANK_TOPIC'] = 'Vote for this topic';
+$lang['THANKS_GRATITUDE'] = 'We appreciate your gratitude';
+$lang['LAST_LIKES'] = 'Last votes';
+$lang['LIKE_OWN_POST'] = 'You can\'t vote for your own topic';
+$lang['NO_LIKES'] = 'Nobody gave a vote yet';
+$lang['LIKE_ALREADY'] = 'You already voted this topic';
+
 // Group control panel
 $lang['GROUP_CONTROL_PANEL'] = 'Grups D\'Usuaris';
 $lang['GROUP_CONFIGURATION'] = 'Grup De Configuració';
