ab41634f0b045f98120a2c54e8c8b125f18eed69
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-11-21 08:17:19 +0700

    Fix typo

--- library/ajax/thanks.php
+++ library/ajax/thanks.php
@@ -33,6 +33,7 @@ switch ($mode) {
         $columns = 'topic_id, user_id, time';
         $values = "$topic_id, {$userdata['user_id']}, " . TIMENOW;
         DB()->query('REPLACE INTO ' . BB_THX . " ($columns) VALUES ($values)");
+
         $this->response['html'] = '<b>' . profile_url($userdata) . ' <i>(' . bb_date(TIMENOW) . ')</i></b>';
         break;
 
