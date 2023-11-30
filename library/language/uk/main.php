5af35c216d053c88c1bedb0f3d72d407aff65be9
Yury Pikhtarev <exileum@icloud.com>
2023-11-30 10:47:01 +0400

    New translations main.php (Ukrainian)

--- library/language/uk/main.php
+++ library/language/uk/main.php
@@ -1072,6 +1072,7 @@ $lang['BT_ADDED'] = 'Додано';
 $lang['BT_REG_ON_TRACKER'] = 'Зареєструватися на трекері';
 $lang['BT_REG_FAIL'] = 'Не вдалося зареєструвати торент на трекері';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Інший торрент з таким же info_hash вже <a href="%s"><b>зареєстрований</b></a>';
+$lang['BT_V1_ONLY_DISALLOWED'] = 'v1-only torrents have been disabled by the administrator at the moment, allowed: v2 and hybrids';
 $lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
 $lang['BT_V2_FILE_LIST_ONLY'] = 'Currently, only torrents with BitTorrent version 2 support are enabled for separate file listing';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Вилучити з трекера';
