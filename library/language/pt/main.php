2385e5c66ee00f12a7ab794961def3c31b03e39e
Yury Pikhtarev <exileum@icloud.com>
2023-11-30 10:47:07 +0400

    New translations main.php (Portuguese, Brazilian)

--- library/language/pt/main.php
+++ library/language/pt/main.php
@@ -1072,6 +1072,7 @@ $lang['BT_ADDED'] = 'Adicionado';
 $lang['BT_REG_ON_TRACKER'] = 'Registar-se no tracker';
 $lang['BT_REG_FAIL'] = 'Não foi possível registrar torrent no tracker';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Outro torrent com o mesmo info_hash já <a href="%s"><b>registered</b></a>';
+$lang['BT_V1_ONLY_DISALLOWED'] = 'v1-only torrents have been disabled by the administrator at the moment, allowed: v2 and hybrids';
 $lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
 $lang['BT_V2_FILE_LIST_ONLY'] = 'Currently, only torrents with BitTorrent version 2 support are enabled for separate file listing';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Retire do tracker';
