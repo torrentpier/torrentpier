85be68adfd492010aac85bd75e73c8a88d06d882
Yury Pikhtarev <exileum@icloud.com>
2023-12-18 21:32:14 +0400

    New translations main.php (Romanian)

--- library/language/ro/main.php
+++ library/language/ro/main.php
@@ -2,7 +2,7 @@
 /**
  * TorrentPier – Bull-powered BitTorrent tracker engine
  *
- * @copyright Copyright (c) 2005-2023 TorrentPier (https://torrentpier.com)
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
  * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
  * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
  */
@@ -532,7 +532,6 @@ $lang['SEARCH_USER_TOPICS'] = 'Găsi subiecte de utilizator'; // Find all topics
 $lang['NO_USER_ID_SPECIFIED'] = 'Îmi pare rău, dar acest utilizator nu există.';
 $lang['WRONG_PROFILE'] = 'Nu puteți modifica un profil care nu este a ta.';
 
-$lang['ONLY_ONE_AVATAR'] = 'Doar un singur tip de avatar poate fi specificat';
 $lang['FILE_NO_DATA'] = 'Fișierul de la URL-ul dat nu conține date';
 $lang['NO_CONNECTION_URL'] = 'Conexiunea nu a putut fi făcute la URL-ai dat';
 $lang['INCOMPLETE_URL'] = 'URL-ul introdus este incompletă';
