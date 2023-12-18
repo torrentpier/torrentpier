4b38149b77ff191dcc43467d6583d00602c2c5c4
Yury Pikhtarev <exileum@icloud.com>
2023-12-18 21:38:36 +0400

    New translations main.php (Japanese)

--- library/language/ja/main.php
+++ library/language/ja/main.php
@@ -2,7 +2,7 @@
 /**
  * TorrentPier – Bull-powered BitTorrent tracker engine
  *
- * @copyright Copyright (c) 2005-2023 TorrentPier (https://torrentpier.com)
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
  * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
  * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
  */
@@ -532,7 +532,6 @@ $lang['SEARCH_USER_TOPICS'] = '見つユーザー課題'; // Find all topics by 
 $lang['NO_USER_ID_SPECIFIED'] = '申し訳ございません、あなたのユーザーは存在しない。';
 $lang['WRONG_PROFILE'] = '変更できませんのプロファイルになります。';
 
-$lang['ONLY_ONE_AVATAR'] = 'みタイプのアバターに指定できる';
 $lang['FILE_NO_DATA'] = 'ファイルのURLやりくりは夫婦で別にされていま含まないデータ';
 $lang['NO_CONNECTION_URL'] = '接続できませんでしたURLへのやりくりは夫婦で別にされていま';
 $lang['INCOMPLETE_URL'] = 'URLを入力された不完全';
