ac5ec44c7881588804a4b8c63d3e1c9b519696cc
Yury Pikhtarev <exileum@icloud.com>
2023-12-18 22:42:39 +0400

    New translations main.php (Romanian)

--- library/language/ro/main.php
+++ library/language/ro/main.php
@@ -532,6 +532,7 @@ $lang['SEARCH_USER_TOPICS'] = 'Găsi subiecte de utilizator'; // Find all topics
 $lang['NO_USER_ID_SPECIFIED'] = 'Îmi pare rău, dar acest utilizator nu există.';
 $lang['WRONG_PROFILE'] = 'Nu puteți modifica un profil care nu este a ta.';
 
+$lang['ONLY_ONE_AVATAR'] = 'Only one type of avatar can be specified';
 $lang['FILE_NO_DATA'] = 'Fișierul de la URL-ul dat nu conține date';
 $lang['NO_CONNECTION_URL'] = 'Conexiunea nu a putut fi făcute la URL-ai dat';
 $lang['INCOMPLETE_URL'] = 'URL-ul introdus este incompletă';
@@ -1073,7 +1074,8 @@ $lang['BT_REG_FAIL'] = 'Nu ar putea înregistra torrent pe tracker';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Un alt torrent cu același info_hash deja <a href="%s"><b>registered</b></a>';
 $lang['BT_V1_ONLY_DISALLOWED'] = 'v1-only torrents have been disabled by the administrator at the moment, allowed: v2 and hybrids';
 $lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
-$lang['BT_V2_FILE_LIST_ONLY'] = 'Currently, only torrents with BitTorrent version 2 support are enabled for separate file listing';
+$lang['BT_V2_FLIST_ONLY'] = 'Currently, only torrents with BitTorrent version 2 support are enabled for separate file listing';
+$lang['BT_V2_FLIST_LIMIT'] = 'Tracker settings do not allow to process lists with more than %d files. Current number is: %d';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Eliminați de la tracker';
 $lang['BT_UNREGISTERED'] = 'Torrent neînregistrate';
 $lang['BT_UNREGISTERED_ALREADY'] = 'Torrent already unregistered';
