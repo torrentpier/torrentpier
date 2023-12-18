37c738bcfbb1e57459f4472955a98f30f49e543c
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-12-18 14:49:16 +0700

    Update search.php

--- search.php
+++ search.php
@@ -860,10 +860,8 @@ function fetch_search_ids($sql, $search_type = SEARCH_TYPE_POST)
     }
 
     // Save results in DB
-    if ($items_count > $per_page) {
-        if (!$search_id) {
-            $search_id = make_rand_str(SEARCH_ID_LENGTH);
-        }
+    if ($items_count > $per_page && !$search_id) {
+        $search_id = make_rand_str(SEARCH_ID_LENGTH);
         $search_array = implode(',', $items_found);
 
         $save_in_db = array(
