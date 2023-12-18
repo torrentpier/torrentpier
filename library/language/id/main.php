e59122c5487a8cdb6dd25b9cbb8c7606fcbb341d
Yury Pikhtarev <exileum@icloud.com>
2023-12-18 22:43:31 +0400

    New translations main.php (Indonesian)

--- library/language/id/main.php
+++ library/language/id/main.php
@@ -532,6 +532,7 @@ $lang['SEARCH_USER_TOPICS'] = 'Menemukan pengguna topik'; // Find all topics by 
 $lang['NO_USER_ID_SPECIFIED'] = 'Maaf, tapi itu tidak ada.';
 $lang['WRONG_PROFILE'] = 'Anda tidak dapat mengubah profil yang tidak anda sendiri.';
 
+$lang['ONLY_ONE_AVATAR'] = 'Only one type of avatar can be specified';
 $lang['FILE_NO_DATA'] = 'File di URL yang anda berikan tidak mengandung data';
 $lang['NO_CONNECTION_URL'] = 'Koneksi tidak bisa dilakukan untuk URL yang anda berikan';
 $lang['INCOMPLETE_URL'] = 'URL yang anda masukkan tidak lengkap';
@@ -1073,7 +1074,8 @@ $lang['BT_REG_FAIL'] = 'Tidak bisa mendaftar torrent tracker';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Torrent lain yang sama info_hash sudah <a href="%s"><b>registered</b></a>';
 $lang['BT_V1_ONLY_DISALLOWED'] = 'v1-only torrents have been disabled by the administrator at the moment, allowed: v2 and hybrids';
 $lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
-$lang['BT_V2_FILE_LIST_ONLY'] = 'Currently, only torrents with BitTorrent version 2 support are enabled for separate file listing';
+$lang['BT_V2_FLIST_ONLY'] = 'Currently, only torrents with BitTorrent version 2 support are enabled for separate file listing';
+$lang['BT_V2_FLIST_LIMIT'] = 'Tracker settings do not allow to process lists with more than %d files. Current number is: %d';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Hapus dari tracker';
 $lang['BT_UNREGISTERED'] = 'Torrent terdaftar';
 $lang['BT_UNREGISTERED_ALREADY'] = 'Torrent already unregistered';
