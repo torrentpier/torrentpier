0e7a60ad4191bba739ab869b95ca3038fc3b1dd6
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-12-18 15:28:09 +0700

    Minor improvements (#1217)

--- search.php
+++ search.php
@@ -860,8 +860,9 @@ function fetch_search_ids($sql, $search_type = SEARCH_TYPE_POST)
     }
 
     // Save results in DB
+    $search_id = make_rand_str(SEARCH_ID_LENGTH);
+
     if ($items_count > $per_page) {
-        $search_id = make_rand_str(SEARCH_ID_LENGTH);
         $search_array = implode(',', $items_found);
 
         $save_in_db = array(
