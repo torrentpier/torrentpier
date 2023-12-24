469098230ef0707a039a7f6c64473cfcc13aa8ba
Constantine Kovalensky <45331093+kovalensky@users.noreply.github.com>
2023-12-24 20:46:16 +0400

    Update file_list_v2.php
    Some SEO little-thingy

--- library/includes/file_list_v2.php
+++ library/includes/file_list_v2.php
@@ -88,7 +88,7 @@ header('Cache-Control: public, max-age=3600');
 
 echo <<<EOF
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=0.1, maximum-scale=1.0" />
