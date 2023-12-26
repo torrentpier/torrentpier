8270fb151608a9537296522dd2728c5a5b7918d9
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-12-27 01:01:39 +0700

    Update admin_user_ban.php

--- admin/admin_user_ban.php
+++ admin/admin_user_ban.php
@@ -21,42 +21,24 @@ if (!$bans = $datastore->get('ban_list')) {
 }
 
 if (isset($_POST['submit'])) {
-    $user_list = [];
+    // Ban action
     if (!empty($_POST['username'])) {
         if (!$this_userdata = get_userdata($_POST['username'], true)) {
             bb_die($lang['NO_USER_ID_SPECIFIED']);
         }
 
-        $user_list[] = $this_userdata['user_id'];
-    }
-
-    $sql = 'SELECT * FROM ' . BB_BANLIST;
-    if (!($result = DB()->sql_query($sql))) {
-        bb_die('Could not obtain banlist information');
-    }
-
-    $current_banlist = DB()->sql_fetchrowset($result);
-    DB()->sql_freeresult($result);
-
-    for ($i = 0, $iMax = count($user_list); $i < $iMax; $i++) {
-        $in_banlist = false;
-        for ($j = 0, $jMax = count($current_banlist); $j < $jMax; $j++) {
-            if ($user_list[$i] == $current_banlist[$j]['ban_userid']) {
-                $in_banlist = true;
-            }
-        }
-
-        if (!$in_banlist) {
-            $sql = 'INSERT INTO ' . BB_BANLIST . ' (ban_userid) VALUES (' . $user_list[$i] . ')';
+        if (!array_key_exists($this_userdata['user_id'], $bans)) {
+            $sql = 'INSERT INTO ' . BB_BANLIST . ' (ban_userid) VALUES (' . $this_userdata['user_id'] . ')';
             if (!DB()->sql_query($sql)) {
                 bb_die('Could not insert ban_userid info into database');
             }
         }
     }
 
+    // Unban action
     $where_sql = '';
 
-    if (isset($_POST['unban_user'])) {
+    if (!empty($_POST['unban_user'])) {
         $user_list = $_POST['unban_user'];
 
         for ($i = 0, $iMax = count($user_list); $i < $iMax; $i++) {
