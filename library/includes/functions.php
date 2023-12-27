56937c42d51982a24cb2a523db76063219697cb9
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2023-12-27 20:05:57 +0700

    Update functions.php

--- library/includes/functions.php
+++ library/includes/functions.php
@@ -35,7 +35,7 @@ function get_attach_path($id, $ext_id = '', $base_path = null, $first_div = 1000
 function delete_avatar($user_id, $avatar_ext_id)
 {
     $avatar_file = $avatar_ext_id ? get_avatar_path($user_id, $avatar_ext_id) : false;
-    return ($avatar_file && file_exists($avatar_file) && unlink($avatar_file));
+    return ($avatar_file && is_file($avatar_file) && unlink($avatar_file));
 }
 
 function get_tracks($type)
