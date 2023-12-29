d9223638e7977a606bd8ef58dd41696866bd058a
Yury Pikhtarev <exileum@icloud.com>
2023-12-29 21:31:58 +0400

    New translations main.php (Turkish)

--- library/language/tr/main.php
+++ library/language/tr/main.php
@@ -514,7 +514,6 @@ $lang['AVATAR_PANEL'] = 'Avatar kontrol paneli';
 
 $lang['WEBSITE'] = 'Web sitesi';
 $lang['LOCATION'] = 'Konum';
-$lang['LOCATION_FLAGS'] = 'Flag images are rendered by country codes:';
 $lang['CONTACT'] = 'İletişim';
 $lang['EMAIL_ADDRESS'] = 'E-posta adresi';
 $lang['SEND_PRIVATE_MESSAGE'] = 'Özel mesaj gönder';
