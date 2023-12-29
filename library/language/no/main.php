7c58cc5a76815a97d6b7c0daaabf9f31599cb249
Yury Pikhtarev <exileum@icloud.com>
2023-12-29 21:29:13 +0400

    New translations main.php (Norwegian)

--- library/language/no/main.php
+++ library/language/no/main.php
@@ -514,7 +514,6 @@ $lang['AVATAR_PANEL'] = 'Avatar kontrollpanel';
 
 $lang['WEBSITE'] = 'Nettstedet';
 $lang['LOCATION'] = 'Beliggenhet';
-$lang['LOCATION_FLAGS'] = 'Flag images are rendered by country codes:';
 $lang['CONTACT'] = 'Kontakt';
 $lang['EMAIL_ADDRESS'] = 'E-post adresse';
 $lang['SEND_PRIVATE_MESSAGE'] = 'Send privat melding';
