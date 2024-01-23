b95ff7f608c9a1341a71d561bbb11ae82caa5df4
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-01-23 15:10:18 +0700

    Updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+'use strict';
 
-exports.default = defaultTask
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+// Config
+const paths = {
+  styles: {
+    src: './styles/templates/default/css/*.css',
+    css: './styles/templates/default/css/',
+  },
+};
+
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(
+      postcss([
+        autoprefixer(),
+        sorting(sortOrder),
+      ]),
+    )
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.styles.css));
+}
