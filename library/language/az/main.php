93ed3208c98fea1ed8e2e4c12e6621c1d701e72c
Yury Pikhtarev <exileum@icloud.com>
2024-01-23 22:26:25 +0400

    New translations main.php (Azerbaijani)

--- library/language/az/main.php
+++ library/language/az/main.php
@@ -1587,7 +1587,7 @@ $lang['DEL_LIST_INFO'] = 'Aradan qaldırılması üçün sifariş siyahıdan ico
 
 // Watched topics
 $lang['WATCHED_TOPICS'] = 'Baxdı mövzular';
-$lang['NO_WATCHED_TOPICS'] = 'Siz baxmaq hər hansı bir mövzu';
+$lang['NO_WATCHED_TOPICS'] = 'No watching any topics';
 
 // set_die_append_msg
 $lang['INDEX_RETURN'] = 'Geri ana səhifə';
