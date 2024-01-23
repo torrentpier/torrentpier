112a8b9ae123d04911af0aafaab85edb628ab774
Yury Pikhtarev <exileum@icloud.com>
2024-01-23 22:26:15 +0400

    New translations main.php (Indonesian)

--- library/language/id/main.php
+++ library/language/id/main.php
@@ -1584,7 +1584,7 @@ $lang['DEL_LIST_INFO'] = 'Untuk menghapus aplikasi dari daftar, klik pada ikon d
 
 // Watched topics
 $lang['WATCHED_TOPICS'] = 'Melihat topik';
-$lang['NO_WATCHED_TOPICS'] = 'Anda tidak menonton setiap topik';
+$lang['NO_WATCHED_TOPICS'] = 'No watching any topics';
 
 // set_die_append_msg
 $lang['INDEX_RETURN'] = 'Kembali ke halaman rumah';
