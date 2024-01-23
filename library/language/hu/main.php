5efc03d997aa6daac7741b748b35bafe12e37070
Yury Pikhtarev <exileum@icloud.com>
2024-01-23 22:25:43 +0400

    New translations main.php (Hungarian)

--- library/language/hu/main.php
+++ library/language/hu/main.php
@@ -1584,7 +1584,7 @@ $lang['DEL_LIST_INFO'] = 'Ha megrendelést szeretne törölni a listából, katt
 
 // Watched topics
 $lang['WATCHED_TOPICS'] = 'Nézte témák';
-$lang['NO_WATCHED_TOPICS'] = 'Nem nézed minden olyan téma';
+$lang['NO_WATCHED_TOPICS'] = 'No watching any topics';
 
 // set_die_append_msg
 $lang['INDEX_RETURN'] = 'Vissza a kezdőlapra';
