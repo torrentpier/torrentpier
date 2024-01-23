1d5458f42cf6266b40154ad2bc4c276e001212d0
Yury Pikhtarev <exileum@icloud.com>
2024-01-23 22:25:39 +0400

    New translations main.php (Finnish)

--- library/language/fi/main.php
+++ library/language/fi/main.php
@@ -1584,7 +1584,7 @@ $lang['DEL_LIST_INFO'] = 'Voit poistaa tilauksen listasta, klikkaa kuvaketta vas
 
 // Watched topics
 $lang['WATCHED_TOPICS'] = 'Katselin aiheita';
-$lang['NO_WATCHED_TOPICS'] = 'Et ole katsomassa mitään aiheita';
+$lang['NO_WATCHED_TOPICS'] = 'No watching any topics';
 
 // set_die_append_msg
 $lang['INDEX_RETURN'] = 'Takaisin kotisivulle';
