9e587c14e7fecb00b73aa12e0c56883648e82ca1
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-02-01 18:06:56 +0700

    Fixed `admin_terns.php` textarea reset in preview mode

--- admin/admin_terms.php
+++ admin/admin_terms.php
@@ -15,7 +15,9 @@ if (!empty($setmodules)) {
 require __DIR__ . '/pagestart.php';
 require INC_DIR . '/bbcode.php';
 
-if (isset($_POST['post']) && $bb_cfg['terms'] != $_POST['message']) {
+$preview = isset($_POST['preview']);
+
+if (isset($_POST['post']) && ($bb_cfg['terms'] !== $_POST['message'])) {
     bb_update_config(['terms' => $_POST['message']]);
     bb_die($lang['CONFIG_UPDATED']);
 }
@@ -23,8 +25,8 @@ if (isset($_POST['post']) && $bb_cfg['terms'] != $_POST['message']) {
 $template->assign_vars([
     'S_ACTION' => 'admin_terms.php',
     'EXT_LINK_NW' => $bb_cfg['ext_link_new_win'],
-    'MESSAGE' => $bb_cfg['terms'] ?: '',
-    'PREVIEW_HTML' => isset($_REQUEST['preview']) ? bbcode2html($_POST['message']) : '',
+    'MESSAGE' => $preview ? $_POST['message'] : $bb_cfg['terms'],
+    'PREVIEW_HTML' => $preview ? bbcode2html($_POST['message']) : '',
 ]);
 
 print_page('admin_terms.tpl', 'admin');
