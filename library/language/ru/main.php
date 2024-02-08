c7250933c8c3b0a0c13cb8412638932bab942345
Yury Pikhtarev <exileum@icloud.com>
2024-02-08 16:05:12 +0400

    New translations main.php (Russian)

--- library/language/ru/main.php
+++ library/language/ru/main.php
@@ -1321,6 +1321,7 @@ $lang['NEVER'] = 'Никогда';
 //mpd
 $lang['DELETE_POSTS'] = 'Удалить сообщения';
 $lang['DELETE_POSTS_SUCCESFULLY'] = 'Выбранные сообщения были успешно удалены';
+$lang['NO_POSTS_REMOVED'] = 'No posts were removed.';
 
 //ts
 $lang['TOPICS_ANNOUNCEMENT'] = 'Объявления';
