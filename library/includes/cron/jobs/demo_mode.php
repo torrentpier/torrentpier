7f88c2486b449d0e773390b425c93614035aa884
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-02-11 18:39:01 +0700

    Updated

---
+++ library/includes/cron/jobs/demo_mode.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+if (!defined('BB_ROOT')) {
+    die(basename(__FILE__));
+}
+
+if (!IN_DEMO_MODE) {
+    return;
+}
