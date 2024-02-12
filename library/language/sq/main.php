cb42199ce679d854b9284e19a57651c13adbfb35
Yury Pikhtarev <exileum@icloud.com>
2024-02-12 20:43:08 +0400

    New translations main.php (Albanian)

--- library/language/sq/main.php
+++ library/language/sq/main.php
@@ -128,6 +128,8 @@ $lang['ONLINE_ADMIN'] = 'Administratori';
 $lang['ONLINE_MOD'] = 'Moderator';
 $lang['ONLINE_GROUP_MEMBER'] = 'Anëtari i grupit';
 
+$lang['CANT_EDIT_IN_DEMO_MODE'] = 'This action can not be performed in demo mode!';
+
 $lang['CURRENT_TIME'] = 'Kohën e tanishme është: <span class="tz_time">%s</span>';
 
 $lang['SEARCH_NEW'] = 'Pikëpamje më e re postimet';
