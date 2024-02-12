6b14e16630b2902fb528af6200a0cab54f929132
Yury Pikhtarev <exileum@icloud.com>
2024-02-12 20:42:40 +0400

    New translations main.php (Danish)

--- library/language/da/main.php
+++ library/language/da/main.php
@@ -128,6 +128,8 @@ $lang['ONLINE_ADMIN'] = 'Administrator';
 $lang['ONLINE_MOD'] = 'Moderator';
 $lang['ONLINE_GROUP_MEMBER'] = 'Et medlem af gruppen';
 
+$lang['CANT_EDIT_IN_DEMO_MODE'] = 'This action can not be performed in demo mode!';
+
 $lang['CURRENT_TIME'] = 'Nuværende tidspunkt er: <span class="tz_time">%s</span>';
 
 $lang['SEARCH_NEW'] = 'Se nyeste indlæg';
