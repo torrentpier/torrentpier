149be677e875b072c13be6194b3a8c9e433cc854
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-02-13 00:23:23 +0700

    Update index.php

--- install/index.php
+++ install/index.php
@@ -11,3 +11,33 @@ define('BB_ROOT', './../');
 define('IN_INSTALL', true);
 
 require BB_ROOT . 'common.php';
+
+set_time_limit(600);
+
+$step = request_var('step', 'welcoming');
+
+// Load SQL dump
+$dump_path = BB_ROOT . 'install/sql/mysql.sql';
+
+// Drop tables & Insert sql dump
+switch ($step) {
+    case 'welcoming':
+        break;
+    case 'insert_dump':
+        $temp_line = '';
+        foreach (file($dump_path) as $line) {
+            if (str_starts_with($line, '--') || $line == '') {
+                continue;
+            }
+
+            $temp_line .= $line;
+            if (str_ends_with(trim($line), ';')) {
+                DB()->query($temp_line);
+                $temp_line = '';
+            }
+        }
+        break;
+    default:
+        bb_simple_die('Invalid step: ' . $step);
+        break;
+}
