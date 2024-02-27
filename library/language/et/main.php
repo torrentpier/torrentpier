41bd168a39aa5b439c356e1b9b87245e130c5020
Yury Pikhtarev <exileum@icloud.com>
2024-02-27 11:14:20 +0400

    New translations main.php (Estonian)

--- library/language/et/main.php
+++ library/language/et/main.php
@@ -272,8 +272,8 @@ $lang['SPOILER_HEAD'] = 'peidetud tekst';
 $lang['SPOILER_CLOSE'] = 'pööra';
 $lang['PLAY_ON_CURPAGE'] = 'Start mängib praegusele lehele';
 
-$lang['EDITED_TIME_TOTAL'] = 'Viimati toimetanud %s kohta %s; toimetanud %d aeg kokku'; // Last edited by me on 12 Oct 2001; edited 1 time in total
-$lang['EDITED_TIMES_TOTAL'] = 'Viimati toimetanud %s kohta %s; toimetanud %d korda kokku'; // Last edited by me on 12 Oct 2001; edited 2 times in total
+$lang['EDITED_TIME_TOTAL'] = 'Last edited by <b>%s</b> on %s; edited %d time in total'; // Last edited by me on 12 Oct 2001; edited 1 time in total
+$lang['EDITED_TIMES_TOTAL'] = 'Last edited by <b>%s</b> on %s; edited %d times in total'; // Last edited by me on 12 Oct 2001; edited 2 times in total
 
 $lang['LOCK_TOPIC'] = 'Lukku teema';
 $lang['UNLOCK_TOPIC'] = 'Avada teema';
