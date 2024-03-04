a8db3515a98ae6708047a27515903f85f928d07c
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-03-04 23:08:25 +0700

    Added BBCode Acronym tag

--- src/Legacy/BBCode.php
+++ src/Legacy/BBCode.php
@@ -82,6 +82,7 @@ class BBCode
         $this->preg = [
             '#\[quote="(.+?)"\]#isu' => $tpl['quote_username_open'],
             '#\[spoiler="(.+?)"\]#isu' => $tpl['spoiler_title_open'],
+            '#\[acronym="(.+?)"\](.*?)\[/acronym\]#isu' => '<span class="post-acronym" title="$1">$2</span>',
             '#\[list=(a|A|i|I|1)\]#isu' => '<ul type="$1">',
             '#\[\*=(\d+)\]#isu' => '<li value="$1">',
             '#\[pre\](.*?)\[/pre\]#isu' => '<pre class="post-pre">$1</pre>',
