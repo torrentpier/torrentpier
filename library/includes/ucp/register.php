77122d86debb520acb6bec8bb962180ce5d0a602
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-03-08 11:21:56 +0700

    Update register.php

--- library/includes/ucp/register.php
+++ library/includes/ucp/register.php
@@ -399,7 +399,7 @@ foreach ($profile_fields as $field => $can_edit) {
          */
         case 'user_gravatar':
             if ($submit && !bf($pr_data['user_opt'], 'user_opt', 'dis_avatar')) {
-                if (isset($_POST['set_gravatar'])) {
+                if (isset($_POST['set_gravatar']) && !isset($_POST['delete_avatar'])) {
                     $gravatarImage = new \Gravatar\Image($pr_data['user_email']);
                     $gravatarImage->setSize($bb_cfg['use_gravatar_provider']['size'])
                         ->defaultImage($bb_cfg['use_gravatar_provider']['default_avatar'])
