94186a71f358c67dcd7c34a3239c14938d49790a
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-04-10 18:05:19 +0700

    Update posting.php

--- posting.php
+++ posting.php
@@ -539,7 +539,7 @@ if ($post_info['allow_reg_tracker'] && $post_data['first_post'] && ($topic_dl_ty
 
         $dl_ds = $dl_ch = $dl_hid = '';
         $dl_type_name = 'topic_dl_type';
-        $dl_type_val = ($topic_dl_type) ? 1 : 0;
+        $dl_type_val = $topic_dl_type ? 1 : 0;
 
         if (!$post_info['allow_reg_tracker'] && !$is_auth['auth_mod']) {
             $dl_ds = ' disabled ';
@@ -547,7 +547,7 @@ if ($post_info['allow_reg_tracker'] && $post_data['first_post'] && ($topic_dl_ty
             $dl_type_name = '';
         }
 
-        $dl_ch = ($mode == 'editpost' && $post_data['first_post'] && $topic_dl_type) ? ' checked ' : '';
+        $dl_ch = ($mode == 'editpost' && $topic_dl_type) ? ' checked ' : '';
 
         $topic_type_toggle .= '<nobr><input type="checkbox" name="' . $dl_type_name . '" id="topic_dl_type_id" ' . $dl_ds . $dl_ch . ' /><label for="topic_dl_type_id"> ' . $lang['POST_DOWNLOAD'] . '</label></nobr>';
         $topic_type_toggle .= $dl_hid;
