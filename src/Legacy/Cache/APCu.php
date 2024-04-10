40f298e1378839c6ec64609bac756ed893838ec1
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-04-10 19:12:47 +0700

    Create APCu.php

---
+++ src/Legacy/Cache/APCu.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+namespace TorrentPier\Legacy\Cache;
+
+use TorrentPier\Dev;
+
+/**
+ * Class APCu
+ * @package TorrentPier\Legacy\Cache
+ */
+class APCu extends Common
+{
+    public $used = true;
+    public $engine = 'APCu';
+    public $prefix;
+
+    public function __construct($prefix = null)
+    {
+        if (!$this->is_installed()) {
+            die("Error: $this->engine extension not installed");
+        }
+
+        $this->prefix = $prefix;
+        $this->dbg_enabled = Dev::sql_dbg_enabled();
+    }
+
+    public function get($name, $get_miss_key_callback = '', $ttl = 0)
+    {
+        $this->cur_query = "cache->get('$name')";
+        $this->debug('start');
+        $this->debug('stop');
+        $this->cur_query = null;
+        $this->num_queries++;
+
+        return apcu_fetch($name);
+    }
+
+    public function set($name, $value, $ttl = 0)
+    {
+        $this->cur_query = "cache->set('$name')";
+        $this->debug('start');
+
+        if (apcu_add($name, $value, $ttl)) {
+            $this->debug('stop');
+            $this->cur_query = null;
+            $this->num_queries++;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function rm($name = '')
+    {
+        if ($name) {
+            $this->cur_query = "cache->rm('$name')";
+            $this->debug('start');
+            $this->debug('stop');
+            $this->cur_query = null;
+            $this->num_queries++;
+
+            return apcu_delete($name);
+        }
+
+        return apcu_clear_cache();
+    }
+
+    public function is_installed(): bool
+    {
+        return function_exists('apcu_add');
+    }
+}
