09d7b2ced913d5a7098d65c8d6377ffadd9874df
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-05-04 11:30:10 +0700

    Update group_membership.php

--- library/ajax/group_membership.php
+++ library/ajax/group_membership.php
@@ -48,7 +48,7 @@ switch ($mode) {
                 }
                 if ($row['group_moderator'] == $user->id) {
                     $class .= ' selfMod';
-                    $href .= "&amp;u=$user_id";  // сам юзер модератор этой группы
+                    $href .= "&amp;" . POST_USERS_URL . "=$user_id";  // сам юзер модератор этой группы
                 }
                 $link = '<a href="' . $href . '" class="' . $class . '" target="_blank">' . htmlCHR($row['group_name']) . '</a>';
                 $html[] = $link;
