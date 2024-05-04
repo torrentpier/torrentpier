47d9968f840736b5893123cc21ebafa56290d787
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-05-04 15:07:53 +0700

    Update mysql.sql

--- install/sql/mysql.sql
+++ install/sql/mysql.sql
@@ -923,6 +923,7 @@ CREATE TABLE IF NOT EXISTS `bb_posts`
   `post_edit_time`  INT(11)               NOT NULL DEFAULT '0',
   `post_edit_count` SMALLINT(5) UNSIGNED  NOT NULL DEFAULT '0',
   `post_attachment` TINYINT(1)            NOT NULL DEFAULT '0',
+  `post_anonymous`  TINYINT(1)            NOT NULL DEFAULT '0',
   `user_post`       TINYINT(1)            NOT NULL DEFAULT '1',
   `mc_comment`      TEXT                  NOT NULL DEFAULT '',
   `mc_type`         TINYINT(1)            NOT NULL DEFAULT '0',
