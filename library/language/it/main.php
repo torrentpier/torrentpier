dde1e727b54888f8c7bd643e953752114054009f
Yury Pikhtarev <exileum@icloud.com>
2024-05-05 23:50:08 +0530

    New translations main.php (Italian)

--- library/language/it/main.php
+++ library/language/it/main.php
@@ -979,6 +979,7 @@ $lang['DATETIME']['DEC'] = 'Dec';
 
 // Country selector
 $lang['COUNTRIES'] = [
+    0 => 'Non selezionare',
     'AD' => 'Andorra',
     'AE' => 'United Arab Emirates',
     'AF' => 'Afghanistan',
