42473f452a865ace220efe50a20020c045900f6d
Yury Pikhtarev <exileum@icloud.com>
2024-05-05 23:49:56 +0530

    New translations main.php (Czech)

--- library/language/cs/main.php
+++ library/language/cs/main.php
@@ -979,6 +979,7 @@ $lang['DATETIME']['DEC'] = 'Prosinec';
 
 // Country selector
 $lang['COUNTRIES'] = [
+    0 => 'Ne, zvolte',
     'AD' => 'Andorra',
     'AE' => 'United Arab Emirates',
     'AF' => 'Afghanistan',
