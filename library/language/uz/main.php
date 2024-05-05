27f9b7e389945285c7b7eb0dd948b6ec3c862890
Yury Pikhtarev <exileum@icloud.com>
2024-05-05 23:50:47 +0530

    New translations main.php (Uzbek)

--- library/language/uz/main.php
+++ library/language/uz/main.php
@@ -979,6 +979,7 @@ $lang['DATETIME']['DEC'] = 'Dec';
 
 // Country selector
 $lang['COUNTRIES'] = [
+    0 => 'Hech tanlang',
     'AD' => 'Andorra',
     'AE' => 'United Arab Emirates',
     'AF' => 'Afghanistan',
