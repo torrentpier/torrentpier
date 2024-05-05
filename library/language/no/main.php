72971d69950dd572784ff7162b453d504df6f261
Yury Pikhtarev <exileum@icloud.com>
2024-05-05 23:50:16 +0530

    New translations main.php (Norwegian)

--- library/language/no/main.php
+++ library/language/no/main.php
@@ -979,6 +979,7 @@ $lang['DATETIME']['DEC'] = 'Dec';
 
 // Country selector
 $lang['COUNTRIES'] = [
+    0 => 'Ingen velger',
     'AD' => 'Andorra',
     'AE' => 'United Arab Emirates',
     'AF' => 'Afghanistan',
