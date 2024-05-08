4329d4a22bd9423cbca92b376ff9b0749466c3b6
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-05-08 17:37:24 +0700

    Update update_geolite_db.php

--- library/includes/cron/jobs/update_geolite_db.php
+++ library/includes/cron/jobs/update_geolite_db.php
@@ -21,18 +21,18 @@ $repo_link = 'https://api.github.com/repos/P3TERX/GeoLite.mmdb/releases/latest';
 
 if (is_file($old_file_path)) {
     if (unlink($old_file_path)) {
-        $cron_runtime_log = date('Y-m-d H:i:s') . " -- Old GeoLite file successfully removed\n";
+        $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Old GeoLite file successfully removed\n";
     }
 }
 
 if (is_file($save_path)) {
     if (rename($save_path, $old_file_path)) {
-        $cron_runtime_log = date('Y-m-d H:i:s') . " -- Successfully created old GeoLite file\n";
+        $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Successfully created old GeoLite file\n";
     } else {
-        $cron_runtime_log = date('Y-m-d H:i:s') . " -- Cannot create old GeoLite file\n";
+        $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Cannot create old GeoLite file\n";
     }
 } else {
-    $cron_runtime_log = date('Y-m-d H:i:s') . " -- Cannot find GeoLite file. Trying to download it...\n";
+    $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Cannot find GeoLite file. Trying to download it...\n";
 }
 
 $context = stream_context_create(['http' => ['header' => 'User-Agent: ' . APP_NAME]]);
@@ -41,40 +41,40 @@ $repo_content = file_get_contents($repo_link, context: $context);
 $json_response = false;
 if ($repo_content !== false) {
     $json_response = json_decode(utf8_encode($repo_content), true);
-    $cron_runtime_log = date('Y-m-d H:i:s') . " -- Successfully connected to: " . $repo_link . "\n";
+    $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Successfully connected to: " . $repo_link . "\n";
 } else {
-    $cron_runtime_log = date('Y-m-d H:i:s') . " -- Cannot access to: " . $repo_link . "\n";
+    $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Cannot access to: " . $repo_link . "\n";
 }
 
 if (is_array($json_response) && !empty($json_response)) {
     $download_link = $json_response['assets'][1]['browser_download_url'];
     if (!empty($download_link)) {
-        $cron_runtime_log = date('Y-m-d H:i:s') . " -- Link to download is valid: " . $download_link . "\n";
+        $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Link to download is valid: " . $download_link . "\n";
         $get_file = file_get_contents($download_link);
         if ($get_file !== false) {
-            $cron_runtime_log = date('Y-m-d H:i:s') . " -- GeoLite file obtained\n";
+            $cron_runtime_log .= date('Y-m-d H:i:s') . " -- GeoLite file obtained\n";
             file_put_contents($save_path, $get_file); // Save new GeoLite file!
             if (is_file($save_path) && is_file($old_file_path)) {
                 unlink($old_file_path);
-                $cron_runtime_log = date('Y-m-d H:i:s') . " -- GeoLite file successfully saved\n";
+                $cron_runtime_log .= date('Y-m-d H:i:s') . " -- GeoLite file successfully saved\n";
             } else {
-                $cron_runtime_log = date('Y-m-d H:i:s') . " -- Reverting all changes...\n";
+                $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Reverting all changes...\n";
                 if (is_file($old_file_path)) {
                     if (rename($old_file_path, $save_path)) {
-                        $cron_runtime_log = date('Y-m-d H:i:s') . " -- Successfully reverted\n";
+                        $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Successfully reverted\n";
                     } else {
-                        $cron_runtime_log = date('Y-m-d H:i:s') . " -- Cannot revert changes, old file not renamed\n";
+                        $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Cannot revert changes, old file not renamed\n";
                     }
                 } else {
-                    $cron_runtime_log = date('Y-m-d H:i:s') . " -- Cannot revert changes, old file not found\n";
+                    $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Cannot revert changes, old file not found\n";
                 }
             }
         } else {
-            $cron_runtime_log = date('Y-m-d H:i:s') . " -- GeoLite file not obtained\n";
+            $cron_runtime_log .= date('Y-m-d H:i:s') . " -- GeoLite file not obtained\n";
         }
     } else {
-        $cron_runtime_log = date('Y-m-d H:i:s') . " -- Cannot find link to download\n";
+        $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Cannot find link to download\n";
     }
 } else {
-    $cron_runtime_log = date('Y-m-d H:i:s') . " -- Invalid response from server: " . $json_response . "\n";
+    $cron_runtime_log .= date('Y-m-d H:i:s') . " -- Invalid response from server: " . $json_response . "\n";
 }
