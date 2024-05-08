7ddeec8658b2ea230411666cce851b0318b46e84
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-05-08 16:17:30 +0700

    Updated

---
+++ library/includes/cron/jobs/update_geolite_db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+if (!defined('BB_ROOT')) {
+    die(basename(__FILE__));
+}
+
+set_time_limit(600);
+
+global $cron_runtime_log;
+
+$save_path = INT_DATA_DIR . '/';
+// $cron_runtime_log = date('Y-m-d H:i:s') . " -- Error performing query: " . $temp_line . " | " . DB()->sql_error()['message'] . "\n";
