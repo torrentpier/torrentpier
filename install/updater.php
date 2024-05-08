c7785d7a1316f46cfa78c1f6ed0970ef04ae877c
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-05-08 22:29:03 +0700

    Update updater.php

--- install/updater.php
+++ install/updater.php
@@ -1,1 +1,8 @@
 <?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
