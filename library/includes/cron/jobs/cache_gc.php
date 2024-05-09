aa49e0db999413d5346aaceefb2c7170b85a0ea9
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-05-10 01:22:36 +0700

    Updated

--- library/includes/cron/jobs/cache_gc.php
+++ library/includes/cron/jobs/cache_gc.php
@@ -16,6 +16,6 @@ global $cron_runtime_log;
 foreach ($bb_cfg['cache']['engines'] as $cache_name => $cache_val) {
     if (method_exists(CACHE($cache_name), 'gc')) {
         $changes = CACHE($cache_name)->gc();
-        $cron_runtime_log .= date('Y-m-d H:i:s') . " -- " . str_pad("$cache_name ", 25, '-', STR_PAD_RIGHT) . " del: $changes\n";
+        $cron_runtime_log[] = date('Y-m-d H:i:s') . " -- " . str_pad("$cache_name ", 25, '-', STR_PAD_RIGHT) . " del: $changes\n";
     }
 }
