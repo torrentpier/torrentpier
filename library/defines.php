a11ec074b3c59e048fce5b38b7de7057ec11d3d1
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-06-04 14:20:51 +0700

    Updated

--- library/defines.php
+++ library/defines.php
@@ -33,7 +33,7 @@ define('APP_NAME', 'TorrentPier');
 define('UPDATER_URL', 'https://api.github.com/repos/torrentpier/torrentpier/releases/latest');
 define('UPDATER_FILE', INT_DATA_DIR . '/updater.ver');
 define('API_IP_URL', 'https://freeipapi.com/api/json/');
-define('CHECK_REQIREMENTS', [
+define('CHECK_REQUIREMENTS', [
     'status' => true,
     'php_min_version' => '8.1.0',
     'ext_list' => [
