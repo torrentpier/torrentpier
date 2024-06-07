1be1224f4f2a1114977bc1e4d5825d2c74538f85
Yury Pikhtarev <exileum@icloud.com>
2024-06-08 00:00:48 +0530

    New translations main.php (Italian)

--- library/language/it/main.php
+++ library/language/it/main.php
@@ -2283,6 +2283,12 @@ $lang['DISALLOWED_ALREADY'] = 'Il nome che hai inserito non può essere consenti
 
 $lang['CLICK_RETURN_DISALLOWADMIN'] = 'Fare clic su %sHere%s di ritorno per Impedire Username Amministrazione';
 
+// Integrity check
+$lang['INTEGRITY_CHECK_SUCCESS'] = 'TorrentPier files integrity check was successful!';
+$lang['INTEGRITY_CHECK_FAIL'] = 'Some TorrentPier files not pass integrity check!';
+$lang['INTEGRITY_CHECKED'] = 'Total checked: %s file(s), of which pass integrity check: %s file(s).';
+$lang['INTEGRITY_LAST_CHECK'] = 'Last check: %s.';
+
 // Version Check
 $lang['VERSION_INFORMATION'] = 'Informazioni Sulla Versione';
 $lang['UPDATE_AVAILABLE'] = 'Update available';
