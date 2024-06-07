ae849eac542edc2ee949b051d2a776f86be30852
Yury Pikhtarev <exileum@icloud.com>
2024-06-08 00:00:27 +0530

    New translations main.php (Afrikaans)

--- library/language/af/main.php
+++ library/language/af/main.php
@@ -2283,6 +2283,12 @@ $lang['DISALLOWED_ALREADY'] = 'Die naam wat u ingevoer het, kan nie toegelaat wo
 
 $lang['CLICK_RETURN_DISALLOWADMIN'] = 'Klik %sHere%s om terug te keer na Administrasie van gebruikersnaam';
 
+// Integrity check
+$lang['INTEGRITY_CHECK_SUCCESS'] = 'TorrentPier files integrity check was successful!';
+$lang['INTEGRITY_CHECK_FAIL'] = 'Some TorrentPier files not pass integrity check!';
+$lang['INTEGRITY_CHECKED'] = 'Total checked: %s file(s), of which pass integrity check: %s file(s).';
+$lang['INTEGRITY_LAST_CHECK'] = 'Last check: %s.';
+
 // Version Check
 $lang['VERSION_INFORMATION'] = 'Weergawe-inligting';
 $lang['UPDATE_AVAILABLE'] = 'Update available';
