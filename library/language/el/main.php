f2f63c1ae5c1c22e576f78d762a570396166e59c
Yury Pikhtarev <exileum@icloud.com>
2024-06-08 00:00:40 +0530

    New translations main.php (Greek)

--- library/language/el/main.php
+++ library/language/el/main.php
@@ -2283,6 +2283,12 @@ $lang['DISALLOWED_ALREADY'] = 'Το όνομα που έχετε εισάγει 
 
 $lang['CLICK_RETURN_DISALLOWADMIN'] = 'Κάντε κλικ %sHere%s να επιστρέψει για να Απαγορεύσει το όνομα Χρήστη Διοίκησης';
 
+// Integrity check
+$lang['INTEGRITY_CHECK_SUCCESS'] = 'TorrentPier files integrity check was successful!';
+$lang['INTEGRITY_CHECK_FAIL'] = 'Some TorrentPier files not pass integrity check!';
+$lang['INTEGRITY_CHECKED'] = 'Total checked: %s file(s), of which pass integrity check: %s file(s).';
+$lang['INTEGRITY_LAST_CHECK'] = 'Last check: %s.';
+
 // Version Check
 $lang['VERSION_INFORMATION'] = 'Πληροφορίες Για Την Έκδοση';
 $lang['UPDATE_AVAILABLE'] = 'Update available';
