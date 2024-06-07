459f72b54e6418e024de469df2fbd55e1418f9d1
Yury Pikhtarev <exileum@icloud.com>
2024-06-08 00:01:26 +0530

    New translations main.php (Estonian)

--- library/language/et/main.php
+++ library/language/et/main.php
@@ -2283,6 +2283,12 @@ $lang['DISALLOWED_ALREADY'] = 'Teie poolt sisestatud nimele ei saa välistada. S
 
 $lang['CLICK_RETURN_DISALLOWADMIN'] = 'Klõpsake %sHere%s tagasi Keelata Kasutajanime Haldus';
 
+// Integrity check
+$lang['INTEGRITY_CHECK_SUCCESS'] = 'TorrentPier files integrity check was successful!';
+$lang['INTEGRITY_CHECK_FAIL'] = 'Some TorrentPier files not pass integrity check!';
+$lang['INTEGRITY_CHECKED'] = 'Total checked: %s file(s), of which pass integrity check: %s file(s).';
+$lang['INTEGRITY_LAST_CHECK'] = 'Last check: %s.';
+
 // Version Check
 $lang['VERSION_INFORMATION'] = 'Versiooni Informatsioon';
 $lang['UPDATE_AVAILABLE'] = 'Update available';
