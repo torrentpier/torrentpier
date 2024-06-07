a21dbaba7f716efb0c2f075c60e9bd194694db10
Yury Pikhtarev <exileum@icloud.com>
2024-06-08 00:00:46 +0530

    New translations main.php (Armenian)

--- library/language/hy/main.php
+++ library/language/hy/main.php
@@ -2286,6 +2286,12 @@ $lang['DISALLOWED_ALREADY'] = 'Մուտքագրված անունը չի կարո
 
 $lang['CLICK_RETURN_DISALLOWADMIN'] = 'Սեղմեք %sHere%s վերադարձնել արգելել Վարչակազմի Անունը';
 
+// Integrity check
+$lang['INTEGRITY_CHECK_SUCCESS'] = 'TorrentPier files integrity check was successful!';
+$lang['INTEGRITY_CHECK_FAIL'] = 'Some TorrentPier files not pass integrity check!';
+$lang['INTEGRITY_CHECKED'] = 'Total checked: %s file(s), of which pass integrity check: %s file(s).';
+$lang['INTEGRITY_LAST_CHECK'] = 'Last check: %s.';
+
 // Version Check
 $lang['VERSION_INFORMATION'] = 'Տեղեկություններ Տարբերակը';
 $lang['UPDATE_AVAILABLE'] = 'Update available';
