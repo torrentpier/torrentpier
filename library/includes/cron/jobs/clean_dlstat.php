0b945f5ea73f333ed7d55e41b1c6f322b062c0e7
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-07-10 23:43:59 +0700

    Update clean_dlstat.php

--- library/includes/cron/jobs/clean_dlstat.php
+++ library/includes/cron/jobs/clean_dlstat.php
@@ -35,19 +35,6 @@ if ($delete_dlstat_sql = implode(') OR (', $delete_dlstat_sql)) {
     DB()->query("DELETE QUICK FROM " . BB_BT_DLSTATUS . " WHERE ($delete_dlstat_sql)");
 }
 
-// Save the last 50 votes for topics
-DB()->query('
-    DELETE t1
-    FROM ' . BB_THX . ' t1
-    JOIN (
-        SELECT topic_id, MAX(time) as max_time
-        FROM ' . BB_THX . '
-        GROUP BY topic_id
-        HAVING COUNT(*) > 50
-    ) t2
-    ON t1.topic_id = t2.topic_id AND t1.time = t2.max_time;
-');
-
 // Delete orphans
 DB()->query("
 	DELETE QUICK dl
