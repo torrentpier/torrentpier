7be7c021d3a8e591b0e37b95007793ae072e6e03
Yury Pikhtarev <exileum@icloud.com>
2024-07-19 13:13:26 +0400

    New translations main.php (Czech)

--- library/language/cs/main.php
+++ library/language/cs/main.php
@@ -607,6 +607,7 @@ $lang['RETURN_PROFILE'] = 'Zpět na profil';
 $lang['SELECT_CATEGORY'] = 'Vyberte kategorii';
 
 $lang['DELETE_IMAGE'] = 'Smazat obrázek';
+$lang['SET_MONSTERID_AVATAR'] = 'Set avatar from MonsterID';
 $lang['CURRENT_IMAGE'] = 'Aktuální snímek';
 
 $lang['NOTIFY_ON_PRIVMSG'] = 'Upozornit na novou soukromou zprávu';
@@ -3060,3 +3061,11 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Vítejte na stránkách %s',
     'USER_WELCOME_INACTIVE' => 'Vítejte na stránkách %s',
 ];
+
+// Null ratio
+$lang['BT_NULL_RATIO'] = 'Reset ratio';
+$lang['BT_NULL_RATIO_NONE'] = 'You don\'t have a ratio';
+$lang['BT_NULL_RATIO_ALERT'] = "Attention!\n\nAre you sure you want to reset your ratio?";
+$lang['BT_NULL_RATIO_AGAIN'] = 'You have already reset your ratio!';
+$lang['BT_NULL_RATIO_NOT_NEEDED'] = 'You have a good ratio. Reset is possible only with a ratio less than %s';
+$lang['BT_NULL_RATIO_SUCCESS'] = 'The ratio has been reset successfully!';
