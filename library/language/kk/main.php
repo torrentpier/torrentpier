61c848d2ddccb0c3ebcaee0e13e958bf9e298964
Yury Pikhtarev <exileum@icloud.com>
2024-07-19 13:14:17 +0400

    New translations main.php (Kazakh)

--- library/language/kk/main.php
+++ library/language/kk/main.php
@@ -607,6 +607,7 @@ $lang['RETURN_PROFILE'] = 'бейіні оралу';
 $lang['SELECT_CATEGORY'] = 'санатын таңдаңыз';
 
 $lang['DELETE_IMAGE'] = 'суретті жойыңыз';
+$lang['SET_MONSTERID_AVATAR'] = 'Set avatar from MonsterID';
 $lang['CURRENT_IMAGE'] = 'Ағымдағы сурет';
 
 $lang['NOTIFY_ON_PRIVMSG'] = 'жаңа жеке жолдауын хабардар етуге';
@@ -3060,3 +3061,11 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Сайтқа қош келдіңіз %s',
     'USER_WELCOME_INACTIVE' => 'Сайтқа қош келдіңіз %s',
 ];
+
+// Null ratio
+$lang['BT_NULL_RATIO'] = 'Reset ratio';
+$lang['BT_NULL_RATIO_NONE'] = 'You don\'t have a ratio';
+$lang['BT_NULL_RATIO_ALERT'] = "Attention!\n\nAre you sure you want to reset your ratio?";
+$lang['BT_NULL_RATIO_AGAIN'] = 'You have already reset your ratio!';
+$lang['BT_NULL_RATIO_NOT_NEEDED'] = 'You have a good ratio. Reset is possible only with a ratio less than %s';
+$lang['BT_NULL_RATIO_SUCCESS'] = 'The ratio has been reset successfully!';
