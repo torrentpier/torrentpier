2bf90b4016c8d025ee900862c5d04cc98fea6451
Yury Pikhtarev <exileum@icloud.com>
2024-07-19 13:13:55 +0400

    New translations main.php (Slovenian)

--- library/language/sl/main.php
+++ library/language/sl/main.php
@@ -607,6 +607,7 @@ $lang['RETURN_PROFILE'] = 'Vrnitev za profil';
 $lang['SELECT_CATEGORY'] = 'Izberite kategorijo';
 
 $lang['DELETE_IMAGE'] = 'Zbriši sliko';
+$lang['SET_MONSTERID_AVATAR'] = 'Set avatar from MonsterID';
 $lang['CURRENT_IMAGE'] = 'Trenutno sliko';
 
 $lang['NOTIFY_ON_PRIVMSG'] = 'Obvesti na novo zasebno sporočilo';
@@ -3060,3 +3061,11 @@ $lang['EMAILER_SUBJECT'] = [
     'USER_WELCOME' => 'Dobrodošli na spletnem mestu %s',
     'USER_WELCOME_INACTIVE' => 'Dobrodošli na spletnem mestu %s',
 ];
+
+// Null ratio
+$lang['BT_NULL_RATIO'] = 'Reset ratio';
+$lang['BT_NULL_RATIO_NONE'] = 'You don\'t have a ratio';
+$lang['BT_NULL_RATIO_ALERT'] = "Attention!\n\nAre you sure you want to reset your ratio?";
+$lang['BT_NULL_RATIO_AGAIN'] = 'You have already reset your ratio!';
+$lang['BT_NULL_RATIO_NOT_NEEDED'] = 'You have a good ratio. Reset is possible only with a ratio less than %s';
+$lang['BT_NULL_RATIO_SUCCESS'] = 'The ratio has been reset successfully!';
