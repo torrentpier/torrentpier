c7816b68dc1ea43f6cd9784a492277573c6d3b49
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-08-03 12:51:37 +0700

    Update viewprofile.php

--- library/includes/ucp/viewprofile.php
+++ library/includes/ucp/viewprofile.php
@@ -77,7 +77,7 @@ if (bf($profiledata['user_opt'], 'user_opt', 'dis_sig')) {
 // Null ratio
 if ($bb_cfg['ratio_null_enabled']) {
     $btu = get_bt_userdata($profiledata['user_id']);
-    $template->assign_vars(array('NULLED_RATIO' => (bool)$btu['ratio_nulled']));
+    $template->assign_vars(array('NULLED_RATIO' => $btu['ratio_nulled'] ?? false));
 }
 
 // Ban information
