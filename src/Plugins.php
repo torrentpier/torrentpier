9211475396963af4fac7d01467daf70e72123691
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-08-16 23:18:21 +0700

    WIP: Plugins autoloading system

---
+++ src/Plugins.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+namespace TorrentPier;
+
+/**
+ * Class Plugins
+ * @package TorrentPier
+ */
+class Plugins
+{
+
+}
