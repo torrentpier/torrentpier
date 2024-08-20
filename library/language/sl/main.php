8876d53590f16f2ec1bb366af08e3f81a2db0c40
Yury Pikhtarev <exileum@icloud.com>
2024-08-20 23:45:06 +0400

    New translations main.php (Slovenian)

--- library/language/sl/main.php
+++ library/language/sl/main.php
@@ -1267,6 +1267,7 @@ $lang['DOWNLOAD'] = 'Download'; // this Language Variable is defined in admin.ph
 $lang['FILESIZE'] = 'Velikost datoteke';
 $lang['VIEWED'] = 'Ogledov';
 $lang['EXTENSION_DISABLED_AFTER_POSTING'] = 'Razširitev \'%s" je bil izključen s odbora skrbnik, zato to Prilogo, se ne prikaže.'; // used in Posts and PM's, replace %s with mime type
+$lang['DOWNLOAD_M3U_FILE'] = 'Download .m3u file';
 
 $lang['ATTACHMENT'] = 'Priloge';
 $lang['ATTACHMENT_THUMBNAIL'] = 'Prilogo Sličico';
@@ -1353,6 +1354,7 @@ $lang['BT_FLIST_BTMR_HASH'] = 'BTMR Hash';
 $lang['BT_FLIST_BTMR_NOTICE'] = 'BitTorrent Merkle Root is a hash of a file embedded in torrents with BitTorrent v2 support, tracker users can extract, calculate them, also download deduplicated torrents using desktop tools such as <a href="%s" target="_blank" referrerpolicy="origin">Torrent Merkle Root Reader</a>';
 $lang['BT_FLIST_CREATION_DATE'] = 'Creation date';
 $lang['BT_FLIST_FILE_PATH'] = 'Path (%s)';
+$lang['BT_FLIST_LINK_TITLE'] = 'File hashes | .torrent meta-info';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Odstrani iz tracker';
 $lang['BT_UNREGISTERED'] = 'Torrent neregistrirane';
 $lang['BT_UNREGISTERED_ALREADY'] = 'Torrent already unregistered';
