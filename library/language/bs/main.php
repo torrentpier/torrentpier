765ad1f1e91499f512414695456f9cde8529cf97
Yury Pikhtarev <exileum@icloud.com>
2024-08-29 19:10:32 +0400

    New translations main.php (Bosnian)

--- library/language/bs/main.php
+++ library/language/bs/main.php
@@ -63,6 +63,7 @@ $lang['SELECT_ACTION'] = 'Odaberite akciju';
 $lang['CLEAR'] = 'Clear';
 $lang['MOVE_TO_TOP'] = 'Move to top';
 $lang['UNKNOWN'] = 'Nepoznati';
+$lang['COPY_TO_CLIPBOARD'] = 'Copy to clipboard';
 
 $lang['NEXT_PAGE'] = 'Sljedeći';
 $lang['PREVIOUS_PAGE'] = 'Prethodni';
@@ -1268,6 +1269,10 @@ $lang['FILESIZE'] = 'Filesize';
 $lang['VIEWED'] = 'Smatra';
 $lang['EXTENSION_DISABLED_AFTER_POSTING'] = 'Produžni \'%s\' je deaktivirana od odbor admin, stoga ovo Prilog nije prikazao.'; // used in Posts and PM's, replace %s with mime type
 $lang['DOWNLOAD_M3U_FILE'] = 'Download .m3u file';
+$lang['PLAYBACK_M3U'] = 'Playback .m3u file';
+$lang['STREAM'] = 'Stream';
+$lang['COPY_STREAM_LINK'] = 'Copy stream link to clipboard';
+$lang['M3U_NOTICE'] = 'Some browsers do not support playback of certain video formats. In such a case, you can download the .m3u file and play it using a third-party player';
 
 $lang['ATTACHMENT'] = 'Prilozi';
 $lang['ATTACHMENT_THUMBNAIL'] = 'Nema Prilog';
