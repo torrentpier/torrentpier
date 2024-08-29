bdc062730dcb08f293722006b5a42648eb38032a
Yury Pikhtarev <exileum@icloud.com>
2024-08-29 19:09:36 +0400

    New translations main.php (Greek)

--- library/language/el/main.php
+++ library/language/el/main.php
@@ -63,6 +63,7 @@ $lang['SELECT_ACTION'] = 'Επιλέξτε δράση';
 $lang['CLEAR'] = 'Clear';
 $lang['MOVE_TO_TOP'] = 'Move to top';
 $lang['UNKNOWN'] = 'Άγνωστο';
+$lang['COPY_TO_CLIPBOARD'] = 'Copy to clipboard';
 
 $lang['NEXT_PAGE'] = 'Την επόμενη';
 $lang['PREVIOUS_PAGE'] = 'Προηγούμενο';
@@ -1268,6 +1269,10 @@ $lang['FILESIZE'] = 'Μέγεθος αρχείου';
 $lang['VIEWED'] = 'Είδαν';
 $lang['EXTENSION_DISABLED_AFTER_POSTING'] = 'Η Επέκταση "%s" ήταν απενεργοποιηθεί από το διαχειριστή του συστήματος, ως εκ τούτου, αυτό το Συνημμένο δεν εμφανίζεται.'; // used in Posts and PM's, replace %s with mime type
 $lang['DOWNLOAD_M3U_FILE'] = 'Download .m3u file';
+$lang['PLAYBACK_M3U'] = 'Playback .m3u file';
+$lang['STREAM'] = 'Stream';
+$lang['COPY_STREAM_LINK'] = 'Copy stream link to clipboard';
+$lang['M3U_NOTICE'] = 'Some browsers do not support playback of certain video formats. In such a case, you can download the .m3u file and play it using a third-party player';
 
 $lang['ATTACHMENT'] = 'Συνημμένα';
 $lang['ATTACHMENT_THUMBNAIL'] = 'Συνημμένο Μικρογραφία';
