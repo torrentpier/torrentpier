1f12dd558eff5acdc613a3e824019c3f0535a2eb
Yury Pikhtarev <exileum@icloud.com>
2024-08-29 19:09:37 +0400

    New translations main.php (Finnish)

--- library/language/fi/main.php
+++ library/language/fi/main.php
@@ -63,6 +63,7 @@ $lang['SELECT_ACTION'] = 'Valitse toiminta';
 $lang['CLEAR'] = 'Clear';
 $lang['MOVE_TO_TOP'] = 'Move to top';
 $lang['UNKNOWN'] = 'Tuntematon';
+$lang['COPY_TO_CLIPBOARD'] = 'Copy to clipboard';
 
 $lang['NEXT_PAGE'] = 'Seuraava';
 $lang['PREVIOUS_PAGE'] = 'Edellinen';
@@ -1268,6 +1269,10 @@ $lang['FILESIZE'] = 'Filesize';
 $lang['VIEWED'] = 'Katsella';
 $lang['EXTENSION_DISABLED_AFTER_POSTING'] = 'Tiedostopääte on \'%s\' oli pois päältä, jonka board admin, siksi tämä Kiinnitys ei näy.'; // used in Posts and PM's, replace %s with mime type
 $lang['DOWNLOAD_M3U_FILE'] = 'Download .m3u file';
+$lang['PLAYBACK_M3U'] = 'Playback .m3u file';
+$lang['STREAM'] = 'Stream';
+$lang['COPY_STREAM_LINK'] = 'Copy stream link to clipboard';
+$lang['M3U_NOTICE'] = 'Some browsers do not support playback of certain video formats. In such a case, you can download the .m3u file and play it using a third-party player';
 
 $lang['ATTACHMENT'] = 'Liitteet';
 $lang['ATTACHMENT_THUMBNAIL'] = 'Kiinnitys Thumbnail';
