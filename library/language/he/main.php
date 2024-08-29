41de7a6e605c991643cc200aa1e3bc52569f9b24
Yury Pikhtarev <exileum@icloud.com>
2024-08-29 19:09:39 +0400

    New translations main.php (Hebrew)

--- library/language/he/main.php
+++ library/language/he/main.php
@@ -63,6 +63,7 @@ $lang['SELECT_ACTION'] = 'בחר פעולה';
 $lang['CLEAR'] = 'Clear';
 $lang['MOVE_TO_TOP'] = 'Move to top';
 $lang['UNKNOWN'] = 'לא ידוע';
+$lang['COPY_TO_CLIPBOARD'] = 'Copy to clipboard';
 
 $lang['NEXT_PAGE'] = 'הבא';
 $lang['PREVIOUS_PAGE'] = 'הקודם';
@@ -1268,6 +1269,10 @@ $lang['FILESIZE'] = 'גודל קובץ';
 $lang['VIEWED'] = 'צפו';
 $lang['EXTENSION_DISABLED_AFTER_POSTING'] = 'הסיומת \'%s\' הייתה מנוטרלת על-ידי לוח admin, לכן קובץ מצורף זה לא מוצג.'; // used in Posts and PM's, replace %s with mime type
 $lang['DOWNLOAD_M3U_FILE'] = 'Download .m3u file';
+$lang['PLAYBACK_M3U'] = 'Playback .m3u file';
+$lang['STREAM'] = 'Stream';
+$lang['COPY_STREAM_LINK'] = 'Copy stream link to clipboard';
+$lang['M3U_NOTICE'] = 'Some browsers do not support playback of certain video formats. In such a case, you can download the .m3u file and play it using a third-party player';
 
 $lang['ATTACHMENT'] = 'קבצים מצורפים';
 $lang['ATTACHMENT_THUMBNAIL'] = 'התמונה הממוזערת של הקובץ המצורף';
