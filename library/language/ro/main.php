f25c97af21dc318f73629c80c0aa4d1ab7ba03a8
Yury Pikhtarev <exileum@icloud.com>
2024-08-29 19:09:17 +0400

    New translations main.php (Romanian)

--- library/language/ro/main.php
+++ library/language/ro/main.php
@@ -63,6 +63,7 @@ $lang['SELECT_ACTION'] = 'Selectați acțiune';
 $lang['CLEAR'] = 'Clear';
 $lang['MOVE_TO_TOP'] = 'Move to top';
 $lang['UNKNOWN'] = 'Necunoscut';
+$lang['COPY_TO_CLIPBOARD'] = 'Copy to clipboard';
 
 $lang['NEXT_PAGE'] = 'Următoarea';
 $lang['PREVIOUS_PAGE'] = 'Anterior';
@@ -1268,6 +1269,10 @@ $lang['FILESIZE'] = 'Filesize';
 $lang['VIEWED'] = 'Vizualizate';
 $lang['EXTENSION_DISABLED_AFTER_POSTING'] = 'Extensia \'%s\' a fost dezactivat de către un consiliu de administrare, prin urmare, acest Atașament nu este afișat.'; // used in Posts and PM's, replace %s with mime type
 $lang['DOWNLOAD_M3U_FILE'] = 'Download .m3u file';
+$lang['PLAYBACK_M3U'] = 'Playback .m3u file';
+$lang['STREAM'] = 'Stream';
+$lang['COPY_STREAM_LINK'] = 'Copy stream link to clipboard';
+$lang['M3U_NOTICE'] = 'Some browsers do not support playback of certain video formats. In such a case, you can download the .m3u file and play it using a third-party player';
 
 $lang['ATTACHMENT'] = 'Atașamente';
 $lang['ATTACHMENT_THUMBNAIL'] = 'Atașament Miniatură';
