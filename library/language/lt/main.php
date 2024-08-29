d46087160709b3623377cac79cfb49a96038df0d
Yury Pikhtarev <exileum@icloud.com>
2024-08-29 19:09:51 +0400

    New translations main.php (Lithuanian)

--- library/language/lt/main.php
+++ library/language/lt/main.php
@@ -63,6 +63,7 @@ $lang['SELECT_ACTION'] = 'Pasirinkite veiksmą';
 $lang['CLEAR'] = 'Clear';
 $lang['MOVE_TO_TOP'] = 'Move to top';
 $lang['UNKNOWN'] = 'Nežinomas';
+$lang['COPY_TO_CLIPBOARD'] = 'Copy to clipboard';
 
 $lang['NEXT_PAGE'] = 'Kitas';
 $lang['PREVIOUS_PAGE'] = 'Ankstesnis';
@@ -1268,6 +1269,10 @@ $lang['FILESIZE'] = 'Failo dydis';
 $lang['VIEWED'] = 'Peržiūrėti';
 $lang['EXTENSION_DISABLED_AFTER_POSTING'] = 'Pratęsimo \'%s" buvo išimtas ūkio valdybos admin, todėl šis priedas yra nerodomas.'; // used in Posts and PM's, replace %s with mime type
 $lang['DOWNLOAD_M3U_FILE'] = 'Download .m3u file';
+$lang['PLAYBACK_M3U'] = 'Playback .m3u file';
+$lang['STREAM'] = 'Stream';
+$lang['COPY_STREAM_LINK'] = 'Copy stream link to clipboard';
+$lang['M3U_NOTICE'] = 'Some browsers do not support playback of certain video formats. In such a case, you can download the .m3u file and play it using a third-party player';
 
 $lang['ATTACHMENT'] = 'Priedai';
 $lang['ATTACHMENT_THUMBNAIL'] = 'Tvirtinimo Miniatiūros';
