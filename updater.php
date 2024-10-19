18276d10210f74d9f2c597be866144778bea4fde
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-10-19 11:59:36 +0700

    Update updater.php

--- updater.php
+++ updater.php
@@ -8,8 +8,15 @@
  */
 
 define('BB_ROOT', __DIR__ . '/');
+define('BB_PATH', __DIR__);
 
 // Check CLI mode
 if (php_sapi_name() !== 'cli') {
     die('Please run <code style="background:#222;color:#00e01f;padding:2px 6px;border-radius:3px;">php ' . basename(__FILE__) . '</code> in CLI mode');
 }
+
+// Get all constants
+require_once BB_ROOT . '/library/defines.php';
+
+// Include functions
+require_once INC_DIR . '/functions.php';
