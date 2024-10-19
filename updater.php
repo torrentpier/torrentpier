5ee5d16407faf9e36b50d2f1daca6b01b785fdfb
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-10-19 11:53:18 +0700

    Create updater.php

---
+++ updater.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+define('BB_ROOT', __DIR__ . '/');
+
+// Check CLI mode
+if (php_sapi_name() !== 'cli') {
+    die('Please run <code style="background:#222;color:#00e01f;padding:2px 6px;border-radius:3px;">php ' . basename(__FILE__) . '</code> in CLI mode');
+}
