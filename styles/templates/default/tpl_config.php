df86028072284e68c9c2a8e22f5c8b4826253c72
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-11-22 00:51:46 +0700

    Update tpl_config.php

--- styles/templates/default/tpl_config.php
+++ styles/templates/default/tpl_config.php
@@ -135,7 +135,7 @@ if (!empty($page_cfg['load_tpl_vars']) and $vars = array_flip($page_cfg['load_tp
         $template->assign_vars([
             'CODE_IMG' => $bb_cfg['text_buttons'] ? $lang['CODE_TOPIC_TXTB'] : '<img src="' . $images['icon_code'] . '" alt="' . $lang['CODE_TOPIC_TXTB'] . '" title="' . $lang['CODE'] . '" />',
             'QUOTE_IMG' => $bb_cfg['text_buttons'] ? $lang['REPLY_WITH_QUOTE_TXTB'] : '<img src="' . $images['icon_quote'] . '" alt="' . $lang['REPLY_WITH_QUOTE_TXTB'] . '" title="' . $lang['REPLY_WITH_QUOTE'] . '" />',
-            'PIN_IMG' => $bb_cfg['text_buttons'] ? $lang['PIN_POST_TXTB'] : '<img src="' . $images['icon_pin_post'] . '" alt="' . $lang['PIN_POST_TXTB'] . '" title="' . $lang['PIN_MESSAGE'] . '" />',
+            'PIN_POST_IMG' => $bb_cfg['text_buttons'] ? $lang['PIN_POST_TXTB'] : '<img src="' . $images['icon_pin_post'] . '" alt="' . $lang['PIN_POST_TXTB'] . '" title="' . $lang['PIN_MESSAGE'] . '" />',
             'EDIT_POST_IMG' => $bb_cfg['text_buttons'] ? $lang['EDIT_DELETE_POST_TXTB'] : '<img src="' . $images['icon_edit'] . '" alt="' . $lang['EDIT_DELETE_POST_TXTB'] . '" title="' . $lang['EDIT_POST'] . '" />',
             'DELETE_POST_IMG' => $bb_cfg['text_buttons'] ? $lang['DELETE_POST_TXTB'] : '<img src="' . $images['icon_delpost'] . '" alt="' . $lang['DELETE_POST_TXTB'] . '" title="' . $lang['DELETE_POST'] . '" />',
             'IP_POST_IMG' => $bb_cfg['text_buttons'] ? $lang['VIEW_IP_TXTB'] : '<img src="' . $images['icon_ip'] . '" alt="' . $lang['VIEW_IP_TXTB'] . '" title="' . $lang['VIEW_IP'] . '" />',
