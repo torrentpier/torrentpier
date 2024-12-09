b275ba57bdc10bb7dc396d34e58971f1c7571779
Yury Pikhtarev <exileum@icloud.com>
2024-12-09 18:12:43 +0400

    New translations main.php (Serbian (Cyrillic))

--- library/language/sr/main.php
+++ library/language/sr/main.php
@@ -1436,6 +1436,7 @@ $lang['CHANGE_TOR_TYPE'] = 'Тип бујице успешно промењен'
 $lang['DEL_TORRENT'] = 'Да ли сте сигурни да желите да уклоните торент?';
 $lang['DEL_MOVE_TORRENT'] = 'Да ли сте сигурни да желите да уклоните и преместите тему?';
 $lang['UNEXECUTED_RELEASE'] = 'Имате бесформенные издање, пре него што креирате нови Фик неоформленном!';
+$lang['TOR_STATUS_LOG_ACTION'] = 'New status: %s.<br>Previous status: %s.';
 
 // tor_comment
 $lang['TOR_MOD_TITLE'] = 'Промена статуса карте - %s';
@@ -1470,6 +1471,7 @@ $lang['SET_SILVER_TORRENT'] = 'Сребро';
 $lang['UNSET_SILVER_TORRENT'] = 'Оверплаи сребро';
 $lang['GOLD_STATUS'] = 'ЗЛАТНИ ТОРЕНТ! ДОВНЛОАД САОБРАЋАЈ НЕ ВЕРУЈЕ!';
 $lang['SILVER_STATUS'] = 'СРЕБРНА ТОРЕНТ! ДОВНЛОАД САОБРАЋАЈ ДЕЛИМИЧНО СЕ БАВИ!';
+$lang['TOR_TYPE_LOG_ACTION'] = 'Torrent type changed to: %s';
 
 $lang['TORRENT_STATUS'] = 'Search by status of release';
 $lang['SEARCH_IN_FORUMS'] = 'Претрага у форумима';
@@ -2811,6 +2813,9 @@ $lang['LOG_ACTION']['LOG_TYPE'] = [
     'mod_topic_split' => 'Тема:<br /> <b>split</b>',
     'mod_topic_set_downloaded' => 'Topic:<br /> <b>set downloaded</b>',
     'mod_topic_unset_downloaded' => 'Topic:<br /> <b>unset downloaded</b>',
+    'mod_topic_change_tor_status' => 'Topic:<br /> <b>changed torrent status</b>',
+    'mod_topic_change_tor_type' => 'Topic:<br /> <b>changed torrent type</b>',
+    'mod_topic_tor_unregister' => 'Topic:<br /> <b>torrent unregistered</b>',
     'mod_topic_renamed' => 'Topic:<br /> <b>renamed</b>',
     'mod_post_delete' => 'Позиција:<br /> <b>deleted</b>',
     'mod_post_pin' => 'Post:<br /> <b>pinned</b>',
