e21389d331498721545682cc91078c7b9dfc2716
Yury Pikhtarev <exileum@icloud.com>
2024-12-14 12:34:48 +0400

    New translations main.php (Croatian)

--- library/language/hr/main.php
+++ library/language/hr/main.php
@@ -1370,12 +1370,17 @@ $lang['BT_REG_FAIL'] = 'Nije moguće registrirati torrent na adresi';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Drugi torrent sa istim info_hash već <a href="%s"><b>registered</b></a>';
 $lang['BT_V1_ONLY_DISALLOWED'] = 'v1-only torrents have been disabled by the administrator at the moment, allowed: v2 and hybrids';
 $lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
+$lang['BT_FLIST'] = 'Files list';
 $lang['BT_FLIST_LIMIT'] = 'Tracker settings do not allow to process lists with more than %d files. Current number is: %d';
 $lang['BT_FLIST_BTMR_HASH'] = 'BTMR Hash';
 $lang['BT_FLIST_BTMR_NOTICE'] = 'BitTorrent Merkle Root is a hash of a file embedded in torrents with BitTorrent v2 support, tracker users can extract, calculate them, also download deduplicated torrents using desktop tools such as <a href="%s" target="_blank" referrerpolicy="origin">Torrent Merkle Root Reader</a>';
 $lang['BT_FLIST_CREATION_DATE'] = 'Creation date';
+$lang['BT_IS_PRIVATE'] = 'Private torrent';
 $lang['BT_FLIST_FILE_PATH'] = 'Path (%s)';
 $lang['BT_FLIST_LINK_TITLE'] = 'File hashes | .torrent meta-info';
+$lang['BT_FLIST_ANNOUNCERS_LIST'] = 'Announcers list';
+$lang['BT_FLIST_ANNOUNCERS'] = 'Announcers';
+$lang['BT_FLIST_ANNOUNCERS_NOTICE'] = 'This list contains announcers of torrent file';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Ukloni iz tracker';
 $lang['BT_UNREGISTERED'] = 'Torrent neregistrirane';
 $lang['BT_UNREGISTERED_ALREADY'] = 'Torrent already unregistered';
@@ -1400,6 +1405,7 @@ $lang['SEEDING'] = 'Sjeme';
 $lang['LEECHING'] = 'Pijavica';
 $lang['IS_REGISTERED'] = 'Registriran od';
 $lang['MAGNET'] = 'Magnet-link';
+$lang['MAGNET_FOR_GUESTS'] = 'Show magnet-link for guests';
 $lang['MAGNET_v2'] = 'Magnet-link (BitTorrent v2 supported)';
 
 //torrent status mod
@@ -1722,7 +1728,6 @@ $lang['NOTICE'] = '!Pažnja!';
 $lang['COPY'] = 'Web stranica ne daje elektroničke verzije proizvoda, i bavi se samo sakupljanje i каталогизацией linkova, šalju i koje objavljujete na forumu našim čitateljima. Ako ste prava bilo kojeg podnosi materijala i ne želite da link na njega se nalazila na našem spisku, kontaktirajte nas i mi odmah ćemo ga. Datoteke za dijeljenje na adresi dati korisnicima stranice, a uprava nije odgovorna za njihov sadržaj. Nemojte namakati datoteke zaštićene autorskim pravima, kao i datoteke ilegalnih sadržaja!';
 
 // FILELIST
-$lang['FILELIST'] = 'Popis';
 $lang['COLLAPSE'] = 'Katalog raspada';
 $lang['EXPAND'] = 'Proširite';
 $lang['SWITCH'] = 'Prekidač';
