9ad078a1bc1d73d09216ebb7914bd13f6d4ed7a5
Yury Pikhtarev <exileum@icloud.com>
2024-12-14 12:34:45 +0400

    New translations main.php (Indonesian)

--- library/language/id/main.php
+++ library/language/id/main.php
@@ -1370,12 +1370,17 @@ $lang['BT_REG_FAIL'] = 'Tidak bisa mendaftar torrent tracker';
 $lang['BT_REG_FAIL_SAME_HASH'] = 'Torrent lain yang sama info_hash sudah <a href="%s"><b>registered</b></a>';
 $lang['BT_V1_ONLY_DISALLOWED'] = 'v1-only torrents have been disabled by the administrator at the moment, allowed: v2 and hybrids';
 $lang['BT_V2_ONLY_DISALLOWED'] = 'v2-only torrents have been disabled by the administrator at the moment, allowed: v1 and hybrids';
+$lang['BT_FLIST'] = 'Files list';
 $lang['BT_FLIST_LIMIT'] = 'Tracker settings do not allow to process lists with more than %d files. Current number is: %d';
 $lang['BT_FLIST_BTMR_HASH'] = 'BTMR Hash';
 $lang['BT_FLIST_BTMR_NOTICE'] = 'BitTorrent Merkle Root is a hash of a file embedded in torrents with BitTorrent v2 support, tracker users can extract, calculate them, also download deduplicated torrents using desktop tools such as <a href="%s" target="_blank" referrerpolicy="origin">Torrent Merkle Root Reader</a>';
 $lang['BT_FLIST_CREATION_DATE'] = 'Creation date';
+$lang['BT_IS_PRIVATE'] = 'Private torrent';
 $lang['BT_FLIST_FILE_PATH'] = 'Path (%s)';
 $lang['BT_FLIST_LINK_TITLE'] = 'File hashes | .torrent meta-info';
+$lang['BT_FLIST_ANNOUNCERS_LIST'] = 'Announcers list';
+$lang['BT_FLIST_ANNOUNCERS'] = 'Announcers';
+$lang['BT_FLIST_ANNOUNCERS_NOTICE'] = 'This list contains announcers of torrent file';
 $lang['BT_UNREG_FROM_TRACKER'] = 'Hapus dari tracker';
 $lang['BT_UNREGISTERED'] = 'Torrent terdaftar';
 $lang['BT_UNREGISTERED_ALREADY'] = 'Torrent already unregistered';
@@ -1400,6 +1405,7 @@ $lang['SEEDING'] = 'Benih';
 $lang['LEECHING'] = 'Lintah';
 $lang['IS_REGISTERED'] = 'Terdaftar';
 $lang['MAGNET'] = 'Magnet-link';
+$lang['MAGNET_FOR_GUESTS'] = 'Show magnet-link for guests';
 $lang['MAGNET_v2'] = 'Magnet-link (BitTorrent v2 supported)';
 
 //torrent status mod
@@ -1719,7 +1725,6 @@ $lang['NOTICE'] = '!PERHATIAN!';
 $lang['COPY'] = 'Situs ini tidak memberikan versi elektronik dari produk, dan bergerak hanya dalam mengumpulkan dan katalogisasi dari referensi yang dikirim dan dipublikasikan di forum oleh pembaca kami. Jika anda adalah pemilik sah dari setiap materi yang disampaikan dan tidak berharap bahwa referensi itu di katalog kami, hubungi kami dan kami akan segera menghapus nya. File untuk pertukaran pada tracker yang diberikan oleh pengguna dari sebuah situs, dan pemerintah tidak menanggung tanggung jawab untuk pemeliharaan mereka. Permintaan untuk tidak mengisi file yang dilindungi oleh hak cipta, dan juga file-file ilegal pemeliharaan!';
 
 // FILELIST
-$lang['FILELIST'] = 'Daftar';
 $lang['COLLAPSE'] = 'Runtuhnya direktori';
 $lang['EXPAND'] = 'Memperluas';
 $lang['SWITCH'] = 'Beralih';
