07516639ca07609a48aca22204a62c08760ec145
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-01-15 15:16:29 +0300

    misc(copyright): Updated copyright year

--- styles/templates/default/css/youtube.css
+++ styles/templates/default/css/youtube.css
@@ -1,7 +1,7 @@
 /*
  * TorrentPier – Bull-powered BitTorrent tracker engine
  *
- * @copyright Copyright (c) 2005-2024 TorrentPier (https://torrentpier.com)
+ * @copyright Copyright (c) 2005-2025 TorrentPier (https://torrentpier.com)
  * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
  * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
  */
