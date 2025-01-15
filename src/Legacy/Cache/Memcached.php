53874391d77657ad23c6d27ec373bc1e99ce7de6
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-01-15 14:01:57 +0300

    feat(cache): Checking if extensions are installed

--- src/Legacy/Cache/Memcached.php
+++ src/Legacy/Cache/Memcached.php
@@ -14,6 +14,8 @@ use TorrentPier\Dev;
 use Memcached as MemcachedClient;
 use MatthiasMullie\Scrapbook\Adapters\Memcached as MemcachedCache;
 
+use Exception;
+
 /**
  * Class Memcached
  * @package TorrentPier\Legacy\Cache
@@ -77,6 +79,9 @@ class Memcached extends Common
      */
     public function __construct(array $cfg, string $prefix)
     {
+        if (!$this->isInstalled()) {
+            throw new Exception('- ext-memcached not installed. Check out php.ini file');
+        }
         $this->client = new MemcachedClient();
         $this->cfg = $cfg;
         $this->prefix = $prefix;
@@ -98,7 +103,7 @@ class Memcached extends Common
         }
 
         if (!$this->connected) {
-            die("Could not connect to $this->engine server");
+            throw new Exception("Could not connect to $this->engine server");
         }
 
         $this->memcached = new MemcachedCache($this->client);
@@ -186,5 +191,15 @@ class Memcached extends Common
         $this->num_queries++;
 
         return $result;
+    }
+
+    /**
+     * Checking if Memcached is installed
+     *
+     * @return bool
+     */
+    private function isInstalled(): bool
+    {
+        return extension_loaded('memcached') && class_exists('Memcached');
     }
 }
