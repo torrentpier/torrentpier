8504ee8473f4818079eeae97048aa5c84270fdd2
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-01-25 15:08:12 +0300

    feat(captcha): Added some new services

---
+++ src/Captcha/GoogleCaptchaV2.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2025 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+namespace TorrentPier\Captcha;
+
+/**
+ * Class GoogleCaptchaV2
+ * @package TorrentPier\Captcha
+ */
+class GoogleCaptchaV2
+{
+
+}
