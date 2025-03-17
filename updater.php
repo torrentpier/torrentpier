814972ac6e9e2713e07d5f1392cefadc2f516d50
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-03-17 19:09:00 +0300

    feat(updater): Added SQL updater script

---
+++ updater.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2025 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+define('BB_SCRIPT', 'updater');
+
+require __DIR__ . '/common.php';
+
+$user->session_start(['req_login' => true]);
+
+// Check admin rights
+if (!IS_SUPER_ADMIN) {
+    bb_die($lang['ONLY_FOR_SUPER_ADMIN']);
+}
+
+// Read updater file
+$updaterFile = readUpdaterFile();
+if (empty($updaterFile) || !is_array($updaterFile)) {
+    bb_die($lang['ONLY_FOR_SUPER_ADMIN']);
+}
+
+$latestVersion = \TorrentPier\Helpers\VersionHelper::removerPrefix($updaterFile['latest_version']);
+$currentVersion = \TorrentPier\Helpers\VersionHelper::removerPrefix($bb_cfg['tp_version']);
+
+// Checking version
+if (\z4kn4fein\SemVer\Version::equal($latestVersion, $currentVersion)) {
+    // todo: have installed actual version (ok!)
+} elseif (\z4kn4fein\SemVer\Version::greaterThan($latestVersion, $currentVersion)) {
+    // todo: need to update first
+}
