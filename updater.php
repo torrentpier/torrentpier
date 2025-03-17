b2855b7c381417cabb0a4ca6cb736788bfb99a4f
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-03-17 20:08:17 +0300

    Revert "Update updater.php"
    This reverts commit 356e08162860945dcfb4970cad5341ad2724c213.

--- updater.php
+++ updater.php
@@ -33,14 +33,13 @@ if (\z4kn4fein\SemVer\Version::equal($latestVersion, $currentVersion)) {
     $files = glob(BB_PATH . '/install/upgrade/update-*.sql');
     $updatesVersions = [];
     foreach ($files as $file) {
-        $version = str_replace('update-v', '', pathinfo(basename($file), PATHINFO_FILENAME));
-        $updatesVersions[] = [
-            'path' => hide_bb_path($file),
-            'version' => \z4kn4fein\SemVer\Version::parse($version)
-        ];
+        $file = pathinfo(basename($file), PATHINFO_FILENAME);
+        $version = str_replace('update-v', '', $file);
+        $updatesVersions[] = \z4kn4fein\SemVer\Version::parse($version);
     }
 
-    $sortedVersionsList = \z4kn4fein\SemVer\Version::sort(array_column($updatesVersions, 'version'));
+    $sortedVersionsList = \z4kn4fein\SemVer\Version::sort($updatesVersions);
+
     foreach ($sortedVersionsList as $version) {
         if (\z4kn4fein\SemVer\Version::greaterThan($version, $currentVersion) &&
             \z4kn4fein\SemVer\Version::lessThanOrEqual($version, $latestVersion)) {
