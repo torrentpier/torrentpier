a38744eb04fd6ec9afdf851045decabbc963b0f7
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-05-04 12:32:37 +0300

    misc: Minor improvements

--- library/attach_mod/displaying_torrent.php
+++ library/attach_mod/displaying_torrent.php
@@ -465,7 +465,7 @@ if ($tor_reged && $tor_info) {
                     $peerCountry = $lang['UNKNOWN'];
                     if (IS_AM || $peer['user_id'] == $userdata['user_id'] || !bf($peer['user_opt'], 'user_opt', 'user_hide_peer_country')) {
                         if ($infoByIP = infoByIP((!empty($peer['ipv6']) ? $peer['ipv6'] : $peer['ip']), $peer['port'])) {
-                            if (isset($infoByIP['countryCode'])) {
+                            if (!empty($infoByIP['countryCode'])) {
                                 $peerCountry = render_flag($infoByIP['countryCode']);
                             }
                         }
