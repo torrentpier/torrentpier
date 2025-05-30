0319928dd55a63325b58a82cb1ab4f5a5d0977e8
Yury Pikhtarev <exileum@icloud.com>
2025-05-30 18:56:01 +0300

    New translations main.php (Uzbek)

--- library/language/uz/main.php
+++ library/language/uz/main.php
@@ -1986,6 +1986,11 @@ $lang['USER_POSTS_COUNT_SYNCHRONIZED'] = 'Foydalanuvchi bilan aloqa Foydalanuvch
 // Online Userlist
 $lang['SHOW_ONLINE_USERLIST'] = 'onlayn foydalanuvchilar ro\'yxatini ko\'rsatish';
 
+// Robots.txt editor
+$lang['ROBOTS_TXT_EDITOR_TITLE'] = 'Manage robots.txt';
+$lang['ROBOTS_TXT_UPDATED_SUCCESSFULLY'] = 'File robots.txt has been updated successfully';
+$lang['CLICK_RETURN_ROBOTS_TXT_CONFIG'] = '%sClick Here to return to robots.txt manager%s';
+
 // Auth pages
 $lang['USER_SELECT'] = 'Agar foydalanuvchi tanlang';
 $lang['GROUP_SELECT'] = 'Guruh tanlash';
@@ -3021,6 +3026,8 @@ $lang['HASH_NOT_FOUND'] = 'hash %s topilmadi bilan ozod';
 
 $lang['TERMS_EMPTY_TEXT'] = '[align=center]The text of this page is edited at: [url]%s[/url]. This line can see only administrators.[/align]';
 $lang['TERMS_EXPLAIN'] = 'Ushbu sahifada, siz resurs asosiy qoidalarini matn foydalanuvchilarga ko\'rsatish mumkin.';
+$lang['TERMS_UPDATED_SUCCESSFULLY'] = 'Terms have been updated successfully';
+$lang['CLICK_RETURN_TERMS_CONFIG'] = '%sClick Here to return to Terms editor%s';
 
 $lang['TR_STATS'] = [
     0 => '30 kun ichida harakatsiz foydalanuvchilar',
