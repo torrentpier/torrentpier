2c3a26214a2f15823d183d56167a38a8c70fe280
Yury Pikhtarev <exileum@icloud.com>
2025-05-30 18:55:19 +0300

    New translations main.php (Italian)

--- library/language/it/main.php
+++ library/language/it/main.php
@@ -1986,6 +1986,11 @@ $lang['USER_POSTS_COUNT_SYNCHRONIZED'] = 'I messaggi dell\'utente conte è stato
 // Online Userlist
 $lang['SHOW_ONLINE_USERLIST'] = 'Mostra l\'elenco degli utenti online';
 
+// Robots.txt editor
+$lang['ROBOTS_TXT_EDITOR_TITLE'] = 'Manage robots.txt';
+$lang['ROBOTS_TXT_UPDATED_SUCCESSFULLY'] = 'File robots.txt has been updated successfully';
+$lang['CLICK_RETURN_ROBOTS_TXT_CONFIG'] = '%sClick Here to return to robots.txt manager%s';
+
 // Auth pages
 $lang['USER_SELECT'] = 'Selezionare un Utente';
 $lang['GROUP_SELECT'] = 'Selezionare un Gruppo di';
@@ -3021,6 +3026,8 @@ $lang['HASH_NOT_FOUND'] = 'Versione con hash %s non trovato';
 
 $lang['TERMS_EMPTY_TEXT'] = '[align=center]The text of this page is edited at: [url]%s[/url]. This line can see only administrators.[/align]';
 $lang['TERMS_EXPLAIN'] = 'Su questa pagina, è possibile specificare il testo di le regole di base della risorsa viene visualizzato dagli utenti.';
+$lang['TERMS_UPDATED_SUCCESSFULLY'] = 'Terms have been updated successfully';
+$lang['CLICK_RETURN_TERMS_CONFIG'] = '%sClick Here to return to Terms editor%s';
 
 $lang['TR_STATS'] = [
     0 => 'gli utenti inattivi in 30 giorni',
