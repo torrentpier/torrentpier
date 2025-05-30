eb0d49afac6292980fb8fc6f80a5098cc8ef8c15
Yury Pikhtarev <exileum@icloud.com>
2025-05-30 18:56:03 +0300

    New translations main.php (Tajik)

--- library/language/tg/main.php
+++ library/language/tg/main.php
@@ -1986,6 +1986,11 @@ $lang['USER_POSTS_COUNT_SYNCHRONIZED'] = 'ҳисоб Заметки корбар
 // Online Userlist
 $lang['SHOW_ONLINE_USERLIST'] = 'Нишон номгўи истифодабарандагони онлайн';
 
+// Robots.txt editor
+$lang['ROBOTS_TXT_EDITOR_TITLE'] = 'Manage robots.txt';
+$lang['ROBOTS_TXT_UPDATED_SUCCESSFULLY'] = 'File robots.txt has been updated successfully';
+$lang['CLICK_RETURN_ROBOTS_TXT_CONFIG'] = '%sClick Here to return to robots.txt manager%s';
+
 // Auth pages
 $lang['USER_SELECT'] = 'Интихоби корбар';
 $lang['GROUP_SELECT'] = 'Интихоб кунед гурӯҳи';
@@ -3021,6 +3026,8 @@ $lang['HASH_NOT_FOUND'] = 'Озод бо шудаи %s ёфт нашуд';
 
 $lang['TERMS_EMPTY_TEXT'] = '[align=center]The text of this page is edited at: [url]%s[/url]. This line can see only administrators.[/align]';
 $lang['TERMS_EXPLAIN'] = 'Дар ин саҳифа шумо метавонед муайян кунед, ки матни қоидаҳои асосии захираҳои аст, ба истифодабарандагон нишон дода мешавад.';
+$lang['TERMS_UPDATED_SUCCESSFULLY'] = 'Terms have been updated successfully';
+$lang['CLICK_RETURN_TERMS_CONFIG'] = '%sClick Here to return to Terms editor%s';
 
 $lang['TR_STATS'] = [
     0 => 'истифодабарандагони ғайрифаъол дар 30 рӯз',
