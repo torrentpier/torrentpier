b50d7ef9f3921a9901c7e5726bd6ead5fbf04f25
Yury Pikhtarev <exileum@icloud.com>
2025-05-30 18:55:09 +0300

    New translations main.php (Danish)

--- library/language/da/main.php
+++ library/language/da/main.php
@@ -1986,6 +1986,11 @@ $lang['USER_POSTS_COUNT_SYNCHRONIZED'] = 'Bruger indlæg tæller er blevet synkr
 // Online Userlist
 $lang['SHOW_ONLINE_USERLIST'] = 'Vis en liste over online brugere';
 
+// Robots.txt editor
+$lang['ROBOTS_TXT_EDITOR_TITLE'] = 'Manage robots.txt';
+$lang['ROBOTS_TXT_UPDATED_SUCCESSFULLY'] = 'File robots.txt has been updated successfully';
+$lang['CLICK_RETURN_ROBOTS_TXT_CONFIG'] = '%sClick Here to return to robots.txt manager%s';
+
 // Auth pages
 $lang['USER_SELECT'] = 'Vælg en Bruger';
 $lang['GROUP_SELECT'] = 'Vælg en Gruppe';
@@ -3021,6 +3026,8 @@ $lang['HASH_NOT_FOUND'] = 'Udgivelse med hash %s ikke fundet';
 
 $lang['TERMS_EMPTY_TEXT'] = '[align=center]The text of this page is edited at: [url]%s[/url]. This line can see only administrators.[/align]';
 $lang['TERMS_EXPLAIN'] = 'På denne side, kan du angive den tekst, der i de grundlæggende regler af den ressource, der vises til brugerne.';
+$lang['TERMS_UPDATED_SUCCESSFULLY'] = 'Terms have been updated successfully';
+$lang['CLICK_RETURN_TERMS_CONFIG'] = '%sClick Here to return to Terms editor%s';
 
 $lang['TR_STATS'] = [
     0 => 'inaktive brugere i 30 dage',
