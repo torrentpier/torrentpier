7fccf6b0f95a3e93e8dd2baa4e9e3f01036d4f27
Yury Pikhtarev <exileum@icloud.com>
2025-06-08 12:54:24 +0300

    New translations main.php (Belarusian)

--- library/language/be/main.php
+++ library/language/be/main.php
@@ -1844,6 +1844,8 @@ $lang['UNDERLINE'] = 'Падкрэсліванне тэксту: [u]text[/u] (к
 $lang['STRIKEOUT'] = 'Закрэслены тэкст: [s]text[/s] (Ctrl+з)';
 $lang['BOX_TAG'] = 'Frame around text: [box]text[/box] or [box=#333,#888]text[/box]';
 $lang['INDENT_TAG'] = 'Insert indent: [indent]text[/indent]';
+$lang['PRE_TAG'] = 'Preformatted text: [pre]text[/pre]';
+$lang['NFO_TAG'] = 'NFO: [nfo]text[/nfo]';
 $lang['SUPERSCRIPT'] = 'Superscript text: [sup]text[/sup]';
 $lang['SUBSCRIPT'] = 'Subscript text: [sub]text[/sub]';
 $lang['QUOTE_TITLE'] = 'Цытую тэкст: [quote]text[/quote] (спалучэнне клавіш Ctrl+М)';
