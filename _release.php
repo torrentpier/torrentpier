505ca26884b13ae37ab3aae77f567da3fa8dfbf6
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-06-09 14:17:42 +0300

    Update _release.php

--- _release.php
+++ _release.php
@@ -1,1 +1,22 @@
 <?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2025 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
+
+define('BB_ROOT', __DIR__ . DIRECTORY_SEPARATOR);
+define('BB_PATH', BB_ROOT);
+
+// Check CLI mode
+if (php_sapi_name() !== 'cli') {
+    die('Please run <code style="background:#222;color:#00e01f;padding:2px 6px;border-radius:3px;">php ' . basename(__FILE__) . '</code> in CLI mode');
+}
+
+// Get all constants
+require_once BB_ROOT . 'library/defines.php';
+
+// Include CLI functions
+require INC_DIR . '/functions_cli.php';
