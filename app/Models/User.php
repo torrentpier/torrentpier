fffb5a7169e54ef46f5b9fe5c45d9e287069274f
Yury Pikhtarev <exileum@icloud.com>
2025-06-24 01:45:44 +0400

    refactor: reorder traits in User model and add newlines in test files
    - Reordered the traits in the User model for better organization.
    - Added missing newlines at the end of several test files to comply with coding standards.
    
    These changes improve code readability and maintainability across the project.

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
