e6976721dc2f6cde6a09b6a55e2f37e5f43f5932
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-07-03 10:10:48 +0300

    revert: "refactor: Moved `Select` class into `Legacy\Common` (#1846)"
    This reverts commit bd0ef063fac328ed16537aacbc12e287a8d8206b.

--- src/Legacy/Select.php
+++ src/Legacy/Select.php
@@ -7,11 +7,11 @@
  * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
  */
 
-namespace TorrentPier\Legacy\Common;
+namespace TorrentPier\Legacy;
 
 /**
  * Class Select
- * @package TorrentPier\Legacy\Common
+ * @package TorrentPier\Legacy
  */
 class Select
 {
