29dd78d7f16ad92ba8c62cb36248776ee5533da5
Yury Pikhtarev <exileum@icloud.com>
2025-07-10 00:31:40 +0200

    Sync with laravel/laravel (#2041)
    * remove apc
    
    * remove apc (#6611)
    
    * Add JSON Schema to package.json (#6613)
    
    Co-authored-by: Martin Bean <martinbean@users.noreply.github.com>
    
    * Minor language update (#6615)
    
    PhpStorm's Natural Language module recommends this change to a countable noun, which happens to be exactly 6 characters long, so as to not affect the overall length of the line, while still making it ever-so-slightly more parseable.
    
    * fix alphabetical order (#6627)
    
    move `resend` before `ses`
    
    * [12.x] Fix: Add void return type to withMiddleware and withExceptions to satisfy Rector analysis (#6628)
    
    * disable nightwatch in testing (#6632)
    
    * Reorder environment variables in phpunit.xml for logical grouping (#6634)
    
    * Change to hyphenate prefixes and cookie names (#6636)
    
    * fix
    
    * [12.x] Fix type casting for environment variables in config files (#6637)
    
    * Update app.php
    
    * Update cache.php
    
    * Update database.php
    
    * Update logging.php
    
    * Update mail.php
    
    ---------
    
    Co-authored-by: Taylor Otwell <taylor@laravel.com>
    Co-authored-by: Ahmed Alaa <92916738+AhmedAlaa4611@users.noreply.github.com>
    Co-authored-by: Martin Bean <martin@martinbean.co.uk>
    Co-authored-by: Martin Bean <martinbean@users.noreply.github.com>
    Co-authored-by: Wogan May <225550+woganmay@users.noreply.github.com>
    Co-authored-by: Khaled Huthaily <khaledenglish@yahoo.com>
    Co-authored-by: Aluísio Pires <88967089+aluisio-pires@users.noreply.github.com>
    Co-authored-by: laserhybiz <100562257+laserhybiz@users.noreply.github.com>
    Co-authored-by: Jonathan Goode <u01jmg3@users.noreply.github.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
@@ -23,6 +23,6 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
