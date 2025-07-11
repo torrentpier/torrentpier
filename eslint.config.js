18bcea02d826b1e83962a7ebdd7151e151375fed
Yury Pikhtarev <exileum@icloud.com>
2025-07-11 23:39:10 +0200

    Update ESLint configuration and GitHub Actions workflow name (#2045)
    - Added 'docs' and 'legacy' to the ignored paths in the ESLint configuration to exclude additional directories from linting.
    - Renamed the GitHub Actions documentation workflow for improved clarity, simplifying the name to "Documentation".
    
    These changes enhance the development workflow and maintain cleaner code quality checks.

--- eslint.config.js
+++ eslint.config.js
@@ -38,7 +38,7 @@ export default [
         },
     },
     {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js'],
+        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'docs', 'legacy'],
     },
     prettier, // Turn off all rules that might conflict with Prettier
 ];
