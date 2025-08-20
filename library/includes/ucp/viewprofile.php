9f05e49d0160ded7c0a13ec01f309e73ed742422
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-08-20 13:04:23 +0300

    Updated

--- library/includes/ucp/viewprofile.php
+++ library/includes/ucp/viewprofile.php
@@ -83,7 +83,7 @@ if ($bb_cfg['ratio_null_enabled'] && $btu = get_bt_userdata($profiledata['user_i
 if ($banInfo = getBanInfo((int)$profiledata['user_id'])) {
     $template->assign_block_vars('ban', [
         'IS_BANNED' => true,
-        'BAN_REASON' => $banInfo['ban_reason']
+        'BAN_REASON' => IS_ADMIN ? $banInfo['ban_reason'] : '',
     ]);
 }
 
