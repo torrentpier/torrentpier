d7b4457d53fb8c7ec8ea0196d083c0b91c3e2d3a
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-08-21 10:55:01 +0300

    Update config.php

--- library/config.php
+++ library/config.php
@@ -553,8 +553,13 @@ $bb_cfg['posts_cache_days_keep'] = 14;
 $bb_cfg['use_ajax_posts'] = true;
 
 // Search
-$bb_cfg['search_engine_type'] = 'mysql'; // none, mysql, sphinx
+$bb_cfg['search_engine_type'] = 'mysql'; // none, mysql, sphinx, manticore
 
+// Manticore
+$bb_cfg['manticore_host'] = '127.0.0.1';
+$bb_cfg['manticore_port'] = 9306;
+
+// Sphinx
 $bb_cfg['sphinx_topic_titles_host'] = '127.0.0.1';
 $bb_cfg['sphinx_topic_titles_port'] = 3312;
 $bb_cfg['sphinx_config_path'] = '../install/sphinx/sphinx.conf';
