5ebdcb2c0ddd9430cbbdb08ce60bd09c42eaa1e0
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2025-09-22 16:28:54 +0300

    Update main.php

--- library/language/source/main.php
+++ library/language/source/main.php
@@ -791,7 +791,6 @@ $lang['MORE_RELEASES'] = 'Find all releases of the group';
 $lang['NOT_A_RELEASE_GROUP'] = 'This group is not a release group';
 
 // Search
-$lang['SEARCH_OFF'] = 'Search is temporarily disabled';
 $lang['SEARCH_ERROR'] = 'At the moment, the search engine is not available<br /><br />Try to repeat the request after several seconds';
 $lang['SEARCH_HELP_URL'] = 'Search Help';
 $lang['SEARCH_QUERY'] = 'Search Query';
