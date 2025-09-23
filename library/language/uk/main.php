621ba042a02095a2a741da71120a89aa0ccf8679
Yury Pikhtarev <exileum@icloud.com>
2025-09-23 23:35:55 +0300

    New translations main.php (Ukrainian)

--- library/language/uk/main.php
+++ library/language/uk/main.php
@@ -1914,6 +1914,8 @@ $lang['PROFILE_EDIT_RETURN'] = 'Повернутися до редагуванн
 $lang['PROFILE_RETURN'] = 'Перейти до профілю';
 
 $lang['WARNING'] = 'Увага';
+$lang['INDEXER'] = 'Reindex search';
+$lang['INDEXER_SUCCESS'] = 'Search index updated successfully';
 
 $lang['FORUM_STYLE'] = 'Стиль форуму';
 
