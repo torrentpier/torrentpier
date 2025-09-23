210163c080e519214ea27b613da9e1746392c647
Yury Pikhtarev <exileum@icloud.com>
2025-09-23 23:36:08 +0300

    New translations main.php (Kazakh)

--- library/language/kk/main.php
+++ library/language/kk/main.php
@@ -1914,6 +1914,8 @@ $lang['PROFILE_EDIT_RETURN'] = 'редакциялау оралу';
 $lang['PROFILE_RETURN'] = 'бейіні өту';
 
 $lang['WARNING'] = 'ескерту';
+$lang['INDEXER'] = 'Reindex search';
+$lang['INDEXER_SUCCESS'] = 'Search index updated successfully';
 
 $lang['FORUM_STYLE'] = 'Форум стилі';
 
