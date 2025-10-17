110489aba3389e31d04dd2af1cb53fca8d5fdde6
Yury Pikhtarev <exileum@icloud.com>
2025-10-17 11:14:18 +0300

    New translations main.php (Romanian)

--- library/language/ro/main.php
+++ library/language/ro/main.php
@@ -2097,6 +2097,7 @@ $lang['GROUP_AUTH_UPDATED'] = 'Permisiuni de grup au fost actualizate';
 $lang['AUTH_UPDATED'] = 'Permisiuni au fost actualizate';
 $lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
 $lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
+$lang['NO_FORUMS_AVAILABLE'] = 'No forums available. Create forums first to manage permissions.';
 $lang['CLICK_RETURN_USERAUTH'] = 'Faceți clic pe %sHere%s pentru a reveni la Permisiuni de Utilizator';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'Faceți clic pe %sHere%s să se întoarcă la Grupul Permissions';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'Faceți clic pe %sHere%s pentru a reveni la Forum Permissions';
