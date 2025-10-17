185d5364f7e40f62fb249218e00661d6f15b933b
Yury Pikhtarev <exileum@icloud.com>
2025-10-17 11:14:27 +0300

    New translations main.php (Belarusian)

--- library/language/be/main.php
+++ library/language/be/main.php
@@ -2097,6 +2097,7 @@ $lang['GROUP_AUTH_UPDATED'] = 'Дазволу групы былі абноўле
 $lang['AUTH_UPDATED'] = 'Дазволу былі абноўленыя';
 $lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
 $lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
+$lang['NO_FORUMS_AVAILABLE'] = 'No forums available. Create forums first to manage permissions.';
 $lang['CLICK_RETURN_USERAUTH'] = 'Націсніце %sHere%s вярнуць правы карыстальніка';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'Націсніце %sHere%s вярнуцца да групе дазволу';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'Націсніце %sHere%s, каб вярнуцца на форум дазволу';
