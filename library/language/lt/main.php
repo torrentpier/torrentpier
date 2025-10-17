1070b0143adc6f818d0fc92c67a18791b67e1a0d
Yury Pikhtarev <exileum@icloud.com>
2025-10-17 11:14:58 +0300

    New translations main.php (Lithuanian)

--- library/language/lt/main.php
+++ library/language/lt/main.php
@@ -2097,6 +2097,7 @@ $lang['GROUP_AUTH_UPDATED'] = 'Grupės teisės buvo atnaujinti';
 $lang['AUTH_UPDATED'] = 'Leidimai buvo atnaujinti';
 $lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
 $lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
+$lang['NO_FORUMS_AVAILABLE'] = 'No forums available. Create forums first to manage permissions.';
 $lang['CLICK_RETURN_USERAUTH'] = 'Spustelėkite %sHere%s norite grįžti į Vartotojo Teises';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'Spustelėkite %sHere%s grįžti į Grupės Teisės';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'Spustelėkite %sHere%s grįžti į Forumo Teisės';
