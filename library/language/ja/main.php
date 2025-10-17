fdbe380c4a45c87026efe5dc8035ff892ba07aef
Yury Pikhtarev <exileum@icloud.com>
2025-10-17 11:14:52 +0300

    New translations main.php (Japanese)

--- library/language/ja/main.php
+++ library/language/ja/main.php
@@ -2101,6 +2101,7 @@ $lang['GROUP_AUTH_UPDATED'] = 'グループの権限の情報を更新いたし'
 $lang['AUTH_UPDATED'] = 'アクセス権の情報を更新いたし';
 $lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
 $lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
+$lang['NO_FORUMS_AVAILABLE'] = 'No forums available. Create forums first to manage permissions.';
 $lang['CLICK_RETURN_USERAUTH'] = 'をクリックし%sHere%sに戻りユーザーの権限';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'をクリックし%sHere%s戻グループの権限';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'をクリックし%sHere%sに戻すフォーラムアクセス権';
