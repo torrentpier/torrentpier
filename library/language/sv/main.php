782a692ca4b099115b594c41b7f5e0d2f102eeec
Yury Pikhtarev <exileum@icloud.com>
2025-10-17 11:15:17 +0300

    New translations main.php (Swedish)

--- library/language/sv/main.php
+++ library/language/sv/main.php
@@ -2097,6 +2097,7 @@ $lang['GROUP_AUTH_UPDATED'] = 'Behörigheter grupp har uppdaterats';
 $lang['AUTH_UPDATED'] = 'Behörigheter har uppdaterats';
 $lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
 $lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
+$lang['NO_FORUMS_AVAILABLE'] = 'No forums available. Create forums first to manage permissions.';
 $lang['CLICK_RETURN_USERAUTH'] = 'Klicka %sHere%s för att återgå till Användare Behörigheter';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'Klicka %sHere%s för att återgå till Grupp Behörigheter';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'Klicka %sHere%s för att återvända till Forumet Behörigheter';
