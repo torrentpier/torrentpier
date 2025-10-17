a53d765bed9595cfce4a1aa89c65fd0a900bbfba
Yury Pikhtarev <exileum@icloud.com>
2025-10-17 11:15:53 +0300

    New translations main.php (Uzbek)

--- library/language/uz/main.php
+++ library/language/uz/main.php
@@ -2097,6 +2097,7 @@ $lang['GROUP_AUTH_UPDATED'] = 'Group togri yangilandi';
 $lang['AUTH_UPDATED'] = 'Ruxsatlar yangilandi';
 $lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
 $lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
+$lang['NO_FORUMS_AVAILABLE'] = 'No forums available. Create forums first to manage permissions.';
 $lang['CLICK_RETURN_USERAUTH'] = 'Foydalanuvchi ruxsati qaytish uchun %sHere%s bosing';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'Group ruxsati qaytish uchun %sHere%s bosing';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'Forum ruxsati qaytish uchun %sHere%s bosing';
