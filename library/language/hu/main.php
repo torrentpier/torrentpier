66897fe5f99bf813bc04f8a2b897545a032b1b86
Yury Pikhtarev <exileum@icloud.com>
2025-10-17 11:14:46 +0300

    New translations main.php (Hungarian)

--- library/language/hu/main.php
+++ library/language/hu/main.php
@@ -2097,6 +2097,7 @@ $lang['GROUP_AUTH_UPDATED'] = 'A csoport jogosultságait frissültek';
 $lang['AUTH_UPDATED'] = 'Engedélyek frissültek';
 $lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
 $lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
+$lang['NO_FORUMS_AVAILABLE'] = 'No forums available. Create forums first to manage permissions.';
 $lang['CLICK_RETURN_USERAUTH'] = 'Kattintson %sHere%s vissza, hogy a Felhasználói Jogosultságok';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'Kattintson %sHere%s, hogy visszatérjen a Csoport Jogosultságait';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'Kattintson %sHere%s vissza Fórum Jogosultságok';
