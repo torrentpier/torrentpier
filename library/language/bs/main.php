47ad2b33095e30449414cab3dc3a345fd23f1078
Yury Pikhtarev <exileum@icloud.com>
2025-10-17 11:15:51 +0300

    New translations main.php (Bosnian)

--- library/language/bs/main.php
+++ library/language/bs/main.php
@@ -2097,6 +2097,7 @@ $lang['GROUP_AUTH_UPDATED'] = 'Grupa dozvole ažurirani';
 $lang['AUTH_UPDATED'] = 'Dozvole ažurirani';
 $lang['AUTH_GENERAL_ERROR'] = 'Could not update admin status';
 $lang['AUTH_SELF_ERROR'] = 'Could not change yourself from an admin to user';
+$lang['NO_FORUMS_AVAILABLE'] = 'No forums available. Create forums first to manage permissions.';
 $lang['CLICK_RETURN_USERAUTH'] = 'Klik %sHere%s da se vrati u Korisnik Dozvole';
 $lang['CLICK_RETURN_GROUPAUTH'] = 'Klik %sHere%s da se vrati u Grupi Dozvole';
 $lang['CLICK_RETURN_FORUMAUTH'] = 'Klik %sHere%s da se vrati u Forum Dozvole';
