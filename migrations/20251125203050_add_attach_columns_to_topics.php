94ce3f0e141db7c686a4c1f8a73f4a0c66f28cc1
Yury Pikhtarev <exileum@icloud.com>
2025-11-29 21:34:52 +0400

    feat: add up/down migration for topic attachment columns
    - Introduced `up()` method to add `attach_ext_id` and `attach_filesize` columns to `bb_topics` table.
    - Added `down()` method to safely remove the newly added columns.

--- migrations/20251125203050_add_attach_columns_to_topics.php
+++ migrations/20251125203050_add_attach_columns_to_topics.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * TorrentPier – Bull-powered BitTorrent tracker engine
+ *
+ * @copyright Copyright (c) 2005-2025 TorrentPier (https://torrentpier.com)
+ * @link      https://github.com/torrentpier/torrentpier for the canonical source repository
+ * @license   https://github.com/torrentpier/torrentpier/blob/master/LICENSE MIT License
+ */
 
 declare(strict_types=1);
 
@@ -6,18 +13,7 @@ use Phinx\Migration\AbstractMigration;
 
 final class AddAttachColumnsToTopics extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change(): void
+    public function up(): void
     {
         $this->table('bb_topics')
             ->addColumn('attach_ext_id', 'integer', [
@@ -33,6 +29,14 @@ final class AddAttachColumnsToTopics extends AbstractMigration
                 'null' => false,
                 'after' => 'attach_ext_id'
             ])
+            ->update();
+    }
+
+    public function down(): void
+    {
+        $this->table('bb_topics')
+            ->removeColumn('attach_ext_id')
+            ->removeColumn('attach_filesize')
             ->update();
     }
 }
