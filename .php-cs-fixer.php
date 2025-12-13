56bab61fd54569ecd76d9c6e18f10942aa2a2e8b
Yury Pikhtarev <exileum@icloud.com>
2025-12-13 04:18:07 +0400

    refactor: update `class_attributes_separation` rule in PHP CS Fixer config to set properties to 'none'

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -55,7 +55,7 @@ return new Config()
         'cast_spaces' => ['space' => 'none'], // (int) $x → (int)$x
 
         // Class notation
-        'class_attributes_separation' => ['elements' => ['method' => 'one', 'property' => 'one']],
+        'class_attributes_separation' => ['elements' => ['method' => 'one', 'property' => 'none']],
         'no_blank_lines_after_class_opening' => true,
         'ordered_class_elements' => ['order' => ['use_trait', 'constant_public', 'constant_protected', 'constant_private', 'property_public_static', 'property_protected_static', 'property_private_static', 'property_public', 'property_protected', 'property_private', 'construct', 'destruct', 'magic', 'phpunit', 'method_public_static', 'method_protected_static', 'method_private_static', 'method_public', 'method_protected', 'method_private']],
         'self_accessor' => true, // ClassName::method() → self::method() (risky)
